8b433dca456afeef710e35c25df30300515b5c3c
Matt Mazzola <mattmazzola@live.com>
2014-08-02 22:41:54 -0500

    Add Core.SharePointProxyForSpaApps

---
+++ Samples/Core.SharePointProxyForSpaApps/SharePointProxyForSpaAppsWeb/App/common/directives/prettyjson.directive.js
@@ -0,0 +1,30 @@
+﻿(function (angular) {
+    "use strict";
+
+    angular
+        .module('prettyjson.directive', [])
+        .directive('prettyJson', PrettyJson);
+
+    function PrettyJson() {
+
+        function formatJson(json, spaces) {
+            spaces = spaces || '  ';
+
+            return JSON.stringify(json, null, spaces);
+        }
+
+        return {
+            restrict: 'E',
+            scope: {
+                json: '='
+            },
+            template: '{{prettyJson}}',
+            link: function ($scope, element, attrs, ngModelCtrl) {
+                $scope.$watch('json', function () {
+                    $scope.prettyJson = formatJson($scope.json);
+                }, true);
+            }
+        };
+    }
+
+})(angular);
---
+++ Samples/Core.SharePointProxyForSpaApps/SharePointProxyForSpaAppsWeb/Models/Request.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+
+namespace SharePointProxyForSpaAppsWeb.Models
+{
+    public class Request
+    {
+        public string Url { get; set; }
+        public string Method { get; set; }
+        public JObject Headers { get; set; }
+        public string Data { get; set; }
+    }
+}
