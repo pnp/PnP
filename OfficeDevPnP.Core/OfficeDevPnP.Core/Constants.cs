38a8f7b2c1b1ff12d29f86756f086ecc47cb0d15
frank_marasco <frank.marasco@microsoft.com>
2014-08-16 08:22:44 -0500

    intial

---
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core
+{
+    /// <summary>
+    /// Constants
+    /// </summary>
+    public static class Constants
+    {
+        public static readonly string EXCEPTION_MSG_INVALID_ARG = "{The arguement {0}, is invalid or not supplied.";
+    }
+}
