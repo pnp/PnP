357d20f6027a1059a12991f2be740118aaa77546
erwinvanhunen <erwin.van.hunen@outlook.com>
2014-08-27 17:38:19 +0200

    Added cmdlet help, updated to use OfficeDevPnP.Core

--- Solutions/PowerShell.Commands/Commands/Web/GetFile.cs
+++ Solutions/PowerShell.Commands/Commands/Web/GetFile.cs
@@ -10,7 +10,11 @@ namespace OfficeDevPnP.PowerShell.Commands
     [Cmdlet(VerbsCommon.Get, "SPOFile")]
     [CmdletHelp("Downloads a file.")]
     [CmdletExample(Code = @"
-PS:> Get-SPOFile ")]
+PS:> Get-SPOFile -ServerRelativeUrl /sites/project/_catalogs/themes/15/company.spcolor",Remarks="Downloads the file and saves it to the current folder", SortOrder = 1)]
+    [CmdletExample(Code = @"
+PS:> Get-SPOFile -ServerRelativeUrl /sites/project/_catalogs/themes/15/company.spcolor -Path c:\temp -FileName company.spcolor", Remarks="Downloads the file and saves it to c:\\temp\\company.spcolor",SortOrder = 2)]
+    [CmdletExample(Code = @"
+PS:> Get-SPOFile -ServerRelativeUrl /sites/project/_catalogs/themes/15/company.spcolor -AsString", Remarks = "Downloads the file and outputs its contents to the console", SortOrder = 3)]
     public class GetFile : SPOWebCmdlet
     {
         [Parameter(Mandatory = true, ParameterSetName = "FILE")]
@@ -35,11 +39,11 @@ PS:> Get-SPOFile ")]
                 {
                     Path = Directory.GetCurrentDirectory();
                 }
-                PowerShell.Core.SPOWeb.GetFile(ServerRelativeUrl, Path, Filename, this.SelectedWeb, ClientContext);
+                this.SelectedWeb.SaveFileToLocal(ServerRelativeUrl, Path, Filename);
             }
             else
             {
-                WriteObject(PowerShell.Core.SPOWeb.GetFile(ServerRelativeUrl, this.SelectedWeb, ClientContext));
+                WriteObject(this.SelectedWeb.GetFileAsString(ServerRelativeUrl));
             }
 
         }
