eb0756f3f77e2a89d18af20dc791718fdd27543c
erwinvanhunen <erwin.van.hunen@outlook.com>
2014-08-27 18:16:05 +0200

    Added

---
+++ Solutions/PowerShell.Commands/Commands/Web/RemoveNavigationNode.cs
@@ -0,0 +1,33 @@
+﻿using OfficeDevPnP.PowerShell.Commands.Base;
+using OfficeDevPnP.PowerShell.Commands.Base.PipeBinds;
+using Microsoft.SharePoint.Client;
+using System;
+using System.Linq;
+using System.Management.Automation;
+using OfficeDevPnP.PowerShell.Core;
+
+namespace OfficeDevPnP.PowerShell.Commands
+{
+    [Cmdlet(VerbsCommon.Remove, "SPONavigationNode")]
+    public class RemoveNavigationNode : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = true, HelpMessage="Either 'Top' or 'Quicklaunch'")]
+        public SPOWeb.NavigationNodeType Location;
+
+        [Parameter(Mandatory = true)]
+        public string Title;
+
+        [Parameter(Mandatory = false)]
+        public string Header;
+
+        [Parameter(Mandatory = false)]
+        public SwitchParameter QuickLaunch = true;
+
+        protected override void ExecuteCmdlet()
+        {
+            this.SelectedWeb.DeleteNavigationNode(Title, Header, Location == SPOWeb.NavigationNodeType.QuickLaunch ? true : false);
+        }
+    }
+
+    
+}
