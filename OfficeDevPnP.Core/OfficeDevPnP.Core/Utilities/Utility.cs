af903fe7c752799b2a43b4b1bdb31b6aa14380ec
erwinvanhunen <erwin.van.hunen@outlook.com>
2014-08-28 18:19:59 +0200

    Added GetHealthScore method

--- OfficeDevPnP.Core/OfficeDevPnP.Core/Utilities/Utility.cs
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Utilities/Utility.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -43,6 +44,33 @@ namespace OfficeDevPnP.Core.Utilities
                 cc.ExecuteQuery();
             }
             return web;
+        }
+
+        /// <summary>
+        /// Returns the healthscore for a SharePoint Server
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static int GetHealthScore(string url)
+        {
+            int value = 0;
+            Uri baseUri = new Uri(url);
+            Uri checkUri = new Uri(baseUri, "_layouts/15/blank.htm");
+            WebRequest webRequest = HttpWebRequest.Create(checkUri);
+            webRequest.Method = "HEAD";
+            webRequest.UseDefaultCredentials = true;
+            using (WebResponse webResponse = webRequest.GetResponse())
+            {
+                foreach (string header in webResponse.Headers)
+                {
+                    if (header == "X-SharePointHealthScore")
+                    {
+                        value = Int32.Parse(webResponse.Headers[header]);
+                        break;
+                    }
+                }
+            }
+            return value;
         }
 
         //public static string URLCombine(string baseUrl, string relativeUrl)
