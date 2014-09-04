dcc82f9db7b7fbd9809163d4b769813f5fa5027f
erwinvanhunen <erwin.van.hunen@outlook.com>
2014-09-05 00:53:25 +0200

    New Set-SPOWeb cmdlet

---
+++ Solutions/PowerShell.Commands/Commands/Web/SetWeb.cs
@@ -0,0 +1,35 @@
+﻿using OfficeDevPnP.PowerShell.CmdletHelpAttributes;
+using OfficeDevPnP.PowerShell.Commands.Base;
+using Microsoft.SharePoint.Client;
+using System.Management.Automation;
+using System;
+
+namespace OfficeDevPnP.PowerShell.Commands
+{
+    [Cmdlet(VerbsCommon.Set, "SPOWeb")]
+    [CmdletHelp("Sets properties on a web")]
+    public class SetWeb : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = false)]
+        public string SiteLogoUrl;
+
+        [Parameter(Mandatory = false)]
+        public string AlternateCssUrl;
+
+        protected override void ExecuteCmdlet()
+        {
+            if (!string.IsNullOrEmpty(SiteLogoUrl))
+            {
+                this.SelectedWeb.SiteLogoUrl = SiteLogoUrl;
+                this.SelectedWeb.Update();
+            }
+            if (!string.IsNullOrEmpty(AlternateCssUrl))
+            {
+                this.SelectedWeb.AlternateCssUrl = AlternateCssUrl;
+                this.SelectedWeb.Update();
+            }
+            ClientContext.ExecuteQuery();
+        }
+    }
+
+}
