495bc5ab8eeb7c91f8560ae4e6ce37f29cd9b8d4
Suman Chakrabarti <suman.chakrabarti@hotmail.com>
2014-09-25 14:20:33 -0400

    Adding Core.RibbonCommands sample

---
+++ Samples/Core.RibbonCommands/Core.RibbonCommandsWeb/Pages/Default.aspx.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace Core.RibbonCommandsWeb {
+    public partial class Default : System.Web.UI.Page {
+        XNamespace ns = "http://schemas.microsoft.com/sharepoint/";
+
+        protected void Page_PreInit(object sender, EventArgs e) {
+            Uri redirectUrl;
+            switch (SharePointContextProvider.CheckRedirectionStatus(Context, out redirectUrl)) {
+                case RedirectionStatus.Ok:
+                    return;
+                case RedirectionStatus.ShouldRedirect:
+                    Response.Redirect(redirectUrl.AbsoluteUri, endResponse: true);
+                    break;
+                case RedirectionStatus.CanNotRedirect:
+                    Response.Write("An error occurred while processing your request.");
+                    Response.End();
+                    break;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e) {
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(Context);
+            LoadChrome();
+            DocumentsLink.NavigateUrl = spContext.SPHostUrl + "Shared Documents";
+        }
+
+        XElement GetCustomActionXmlNode() {
+            var filePath = Server.MapPath("~/Models/RibbonCommands.xml");
+            var xdoc = XDocument.Load(filePath);
+            var customActionNode = xdoc.Element(ns + "Elements").Element(ns + "CustomAction");
+            return customActionNode;
+        }
+
+        protected void InitializeButton_Click(object sender, EventArgs e) {
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(Context);
+
+            using (var clientContext = spContext.CreateUserClientContextForSPHost()) {
+                clientContext.Load(clientContext.Web, web => web.UserCustomActions);
+                clientContext.ExecuteQuery();
+
+                // get the xml elements file and get the CommandUIExtension node
+                var customActionNode = GetCustomActionXmlNode();
+                var customActionName = customActionNode.Attribute("Id").Value;
+                var commandUIExtensionNode = customActionNode.Element(ns + "CommandUIExtension");
+                var xmlContent = commandUIExtensionNode.ToString();
+                var location = customActionNode.Attribute("Location").Value;
+                var registrationId = customActionNode.Attribute("RegistrationId").Value;
+                var registrationTypeString = customActionNode.Attribute("RegistrationType").Value;
+                var registrationType = (UserCustomActionRegistrationType)Enum.Parse(typeof(UserCustomActionRegistrationType), registrationTypeString);
+
+                var sequence = 1000;
+                if (customActionNode.Attribute(ns + "Sequence") != null) {
+                    sequence = Convert.ToInt32(customActionNode.Attribute(ns + "Sequence").Value);
+                }
+
+                // see of the custom action already exists
+                var customAction = clientContext.Web.UserCustomActions.FirstOrDefault(uca => uca.Name == customActionName);
+
+                // if it does not exist, create it
+                if (customAction == null) {
+                    // create the ribbon
+                    customAction = clientContext.Web.UserCustomActions.Add();
+                    customAction.Name = customActionName;
+                }
+
+                // set custom action properties
+                customAction.Location = location;
+                customAction.CommandUIExtension = xmlContent; // CommandUIExtension xml
+                customAction.RegistrationId = registrationId;
+                customAction.RegistrationType = registrationType;
+                customAction.Sequence = sequence;
+
+                customAction.Update();
+                clientContext.Load(customAction);
+                clientContext.ExecuteQuery();
+            }
+        }
+
+        protected void RemoveButton_Click(object sender, EventArgs e) {
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(Context);
+
+            using (var clientContext = spContext.CreateUserClientContextForSPHost()) {
+                var customActionNode = GetCustomActionXmlNode();
+                var customActionName = customActionNode.Attribute("Id").Value;
+
+                clientContext.Load(clientContext.Web, web => web.UserCustomActions);
+                clientContext.ExecuteQuery();
+
+                var customAction = clientContext.Web.UserCustomActions.FirstOrDefault(uca => uca.Name == customActionName);
+
+                if (customAction != null) {
+                    customAction.DeleteObject();
+                    clientContext.ExecuteQuery();
+                }
+            }
+        }
+
+        void LoadChrome() {
+
+            // define initial script, needed to render the chrome control
+            string script = @"
+            function chromeLoaded() {
+                $('body').show();
+            }
+
+            //function callback to render chrome after SP.UI.Controls.js loads
+            function renderSPChrome() {
+                //Set the chrome options for launching Help, Account, and Contact pages
+                var options = {
+                    'appTitle': document.title,
+                    'onCssLoaded': 'chromeLoaded()'
+                };
+
+                //Load the Chrome Control in the divSPChrome element of the page
+                var chromeNavigation = new SP.UI.Controls.Navigation('divSPChrome', options);
+                chromeNavigation.setVisible(true);
+            }";
+
+            //register script in page
+            Page.ClientScript.RegisterClientScriptBlock(typeof(Default), "BasePageScript", script, true);
+        }
+    }
+}
---
+++ Samples/Core.RibbonCommands/Core.RibbonCommandsWeb/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Core.RibbonCommandsWeb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Core.RibbonCommandsWeb")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("978fabcb-c3dc-491c-9f48-be5019d15187")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
