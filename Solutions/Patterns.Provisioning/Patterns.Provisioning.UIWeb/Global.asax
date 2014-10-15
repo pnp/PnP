19c7042dc35c4b216a51f1035b9d6a03990047f8
jansenbe <bjansen@microsoft.com>
2014-10-15 08:33:35 +0200

    removed wrongly checked in temp solutions

--- Solutions/Patterns.Provisioning/Patterns.Provisioning.UIWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Patterns.Provisioning.UIWeb.Global" Language="C#" %>
--- Solutions/Patterns.Provisioning/Patterns.Provisioning.UIWeb/Scripts/_references.js
+++
@@ -1,7 +0,0 @@
-﻿/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="jquery-1.10.2.min.js" />
-/// <reference path="jquery-ui-1.11.0.min.js" />
-/// <reference path="jquery-steps.min.js" />
-/// <reference path="jquery-validate.min.js" />
-/// <reference path="knockout-3.0.0.js" />
-/// <reference path="bootstrap.min.js" />
