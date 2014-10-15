f63bed938da3677252b520165d627e6a1938079e
Erwin van Hunen <erwin.van.hunen@outlook.com>
2014-10-15 21:18:24 +0200

    Added workflow extensions and cmdlets

---
+++ Solutions/PowerShell.Commands/Commands/Workflows/GetWorkflowSubscription.cs
@@ -0,0 +1,59 @@
+﻿using System.Management.Automation;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.WorkflowServices;
+using OfficeDevPnP.PowerShell.Commands.Base.PipeBinds;
+
+namespace OfficeDevPnP.PowerShell.Commands.Workflows
+{
+    [Cmdlet(VerbsCommon.Get, "SPOWorkflowSubscription")]
+    public class GetWorkflowSubscription : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = false, HelpMessage = "The name of the workflow", Position = 0)]
+        public string Name;
+
+        [Parameter(Mandatory = false, HelpMessage = "A list to search the association for", Position = 1)]
+        public ListPipeBind List;
+        protected override void ExecuteCmdlet()
+        {
+            if (List != null)
+            {
+                var list = this.SelectedWeb.GetList(List);
+
+                if (string.IsNullOrEmpty(Name))
+                {
+                    var servicesManager = new WorkflowServicesManager(ClientContext, this.SelectedWeb);
+                    var subscriptionService = servicesManager.GetWorkflowSubscriptionService();
+                    var subscriptions = subscriptionService.EnumerateSubscriptionsByList(list.Id);
+
+                    ClientContext.Load(subscriptions);
+
+                    ClientContext.ExecuteQuery();
+                    WriteObject(subscriptions, true);
+                }
+                else
+                {
+                    WriteObject(list.GetWorkflowSubscription(Name));
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(Name))
+                {
+                    var servicesManager = new WorkflowServicesManager(ClientContext, this.SelectedWeb);
+                    var subscriptionService = servicesManager.GetWorkflowSubscriptionService();
+                    var subscriptions = subscriptionService.EnumerateSubscriptions();
+
+                    ClientContext.Load(subscriptions);
+
+                    ClientContext.ExecuteQuery();
+                    WriteObject(subscriptions, true);
+                }
+                else
+                {
+                    WriteObject(this.SelectedWeb.GetWorkflowSubscription(Name));
+                }
+            }
+        }
+    }
+
+}
---
+++ Solutions/PowerShell.Commands/Commands/Workflows/RemoveWorkflowSubscription.cs
@@ -0,0 +1,37 @@
+﻿using System.Management.Automation;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.WorkflowServices;
+using OfficeDevPnP.PowerShell.Commands.Base.PipeBinds;
+using System.Collections.Generic;
+using System;
+
+namespace OfficeDevPnP.PowerShell.Commands.Workflows
+{
+    [Cmdlet(VerbsCommon.Remove, "SPOWorkflowSubscription")]
+    public class RemoveWorkflowSubscription : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = true, HelpMessage = "The subscription to remove", Position = 0)]
+        public WorkflowSubscriptionPipeBind Identity;
+
+        protected override void ExecuteCmdlet()
+        {
+            if (Identity.Subscription != null)
+            {
+                Identity.Subscription.Delete();
+            }
+            else if (Identity.Id != Guid.Empty)
+            {
+                var subscription = this.SelectedWeb.GetWorkflowSubscription(Identity.Id);
+                if (subscription != null)
+                    subscription.Delete();
+            }
+            else if (!string.IsNullOrEmpty(Identity.Name))
+            {
+                var subscription = this.SelectedWeb.GetWorkflowSubscription(Identity.Name);
+                if (subscription != null)
+                    subscription.Delete();
+            }
+        }
+    }
+
+}
