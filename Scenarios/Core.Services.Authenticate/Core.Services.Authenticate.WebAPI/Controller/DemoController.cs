983ca151d6badcd5ae609512ce989a317f1953d6
jansenbe <bjansen@microsoft.com>
2014-10-16 17:39:14 +0200

    New webapi

---
+++ Scenarios/Core.Services.Authenticate/Core.Services.Authenticate.WebAPI/Controller/DemoController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Core.Services.Authenticate.WebAPI.Controller
+{
+    public class DemoController : ApiController
+    {
+    }
+}
