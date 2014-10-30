5df2b8f19b7f67e6a64cd9cc279fe179e78f8b9e
Erwin van Hunen <erwin.van.hunen@outlook.com>
2014-10-30 18:39:28 +0100

    Added TenantExtensions Tests

---
+++ OfficeDevPnP.Core/OfficeDevPnP.Core.Tests/AppModelExtensions/TenantExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using Microsoft.Online.SharePoint.TenantAdministration;
+
+namespace OfficeDevPnP.Core.Tests.AppModelExtensions
+{
+    [TestClass()]
+    public class TenantExtensionsTests
+    {
+        [TestMethod()]
+        public void GetSiteCollectionsTest()
+        {
+            using (var tenantContext = TestCommon.CreateTenantClientContext())
+            {
+                var tenant = new Tenant(tenantContext);
+                var siteCols = tenant.GetSiteCollections();
+
+                Assert.IsTrue(siteCols.Any());
+
+            }
+        }
+
+        [TestMethod()]
+        public void GetOneDriveSiteCollections()
+        {
+            using (var tenantContext = TestCommon.CreateTenantClientContext())
+            {
+                var tenant = new Tenant(tenantContext);
+                var siteCols = tenant.GetOneDriveSiteCollections();
+
+                Assert.IsTrue(siteCols.Any());
+
+            }
+        }
+    }
+}
