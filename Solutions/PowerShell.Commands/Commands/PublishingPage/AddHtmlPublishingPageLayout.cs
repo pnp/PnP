a376363aa3a174b8bf78cc7050e67e8adcba6052
HappySolutions <henrik@happysolutions.se>
2014-11-06 16:52:01 +0100

    Cmdlet support for adding html publishing page layouts

---
+++ Solutions/PowerShell.Commands/Commands/PublishingPage/AddHtmlPublishingPageLayout.cs
@@ -0,0 +1,26 @@
+﻿using System.Management.Automation;
+using Microsoft.SharePoint.Client;
+
+namespace OfficeDevPnP.PowerShell.Commands.PublishingPage
+{
+ [Cmdlet(VerbsCommon.Add, "SPOHtmlPublishingPageLayoutt")]
+    public class AddHtmlPublishingPageLayout : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = true, HelpMessage = "Full path to the file which will be uploaded")]
+         public string SourceFilePath = string.Empty;
+
+        [Parameter(Mandatory = true)]
+        public string Title = string.Empty;
+
+        [Parameter(Mandatory = true)]
+        public string Description = string.Empty;
+
+        [Parameter(Mandatory = true)]
+        public string AssociatedContentTypeID;
+     
+        protected override void ExecuteCmdlet()
+        {
+            this.SelectedWeb.DeployHtmlPageLayout(SourceFilePath, Title, Description, AssociatedContentTypeID);
+        }
+    }
+}
