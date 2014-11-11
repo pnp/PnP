afa0d6ef92f99f64476143a2ef50be68099c2e02
sgryphon <sgryphon@computer.org>
2014-11-11 17:36:27 +1000

    Additional tests for composed looks.

--- OfficeDevPnP.Core/OfficeDevPnP.Core.Tests/AppModelExtensions/BrandingExtensionsTests.cs
+++ OfficeDevPnP.Core/OfficeDevPnP.Core.Tests/AppModelExtensions/BrandingExtensionsTests.cs
@@ -12,11 +12,12 @@ namespace OfficeDevPnP.Core.Tests.AppModelExtensions
     [TestClass()]
     public class BrandingExtensionsTests
     {
-        private string builtInLook1 = "Sea Monster"; // oslo, palette005, image_bg005, fontscheme003
-        private string builtInLook2 = "Blossom"; // seattle, palette002,image_bg002
-        private string builtInMaster1 = "oslo.master";
-        private string builtInPalette1 = "palette003.spcolor";
-        private string builtInFont1 = "fontscheme002.spfont";
+        private string builtInLookSeaMonster = "Sea Monster"; // oslo, palette005, image_bg005, fontscheme003
+        private string builtInLookBlossom = "Blossom"; // seattle, palette002,image_bg002
+        private string builtInMasterOslo = "oslo.master";
+        private string builtInMasterSeattle = "seattle.master";
+        private string builtInPalette003 = "palette003.spcolor";
+        private string builtInFont002 = "fontscheme002.spfont";
 
         private string customColorFilePath = string.Empty;
         private string customBackgroundFilePath = string.Empty;
@@ -34,6 +35,8 @@ namespace OfficeDevPnP.Core.Tests.AppModelExtensions
                      </Query>
                 </View>";
 
+        private string testWebName;
+
 
         [TestInitialize()]
         public void Initialize()
@@ -43,6 +46,32 @@ namespace OfficeDevPnP.Core.Tests.AppModelExtensions
             System.IO.File.WriteAllBytes(customColorFilePath, OfficeDevPnP.Core.Tests.Properties.Resources.custom);
             customBackgroundFilePath = Path.Combine(Path.GetTempPath(), "custombg.jpg");
             Properties.Resources.custombg.Save(customBackgroundFilePath);
+
+            testWebName = string.Format("Test_CL{0:yyyyMMddTHHmmss}", DateTimeOffset.Now);
+            using (var context = TestCommon.CreateClientContext())
+            {
+                var wci1 = new WebCreationInformation();
+                wci1.Url = testWebName;
+                wci1.Title = testWebName;
+                wci1.WebTemplate = "CMSPUBLISHING#0";
+                var web1 = context.Web.Webs.Add(wci1);
+                context.ExecuteQuery();
+
+                var wci2 = new WebCreationInformation();
+                wci2.Url = "a";
+                wci2.Title = "A";
+                wci2.WebTemplate = "CMSPUBLISHING#0";
+                var webA = web1.Webs.Add(wci2);
+                context.ExecuteQuery();
+
+                var wci3 = new WebCreationInformation();
+                wci3.Url = "b";
+                wci3.Title = "B";
+                wci3.WebTemplate = "CMSPUBLISHING#0";
+                var webB = web1.Webs.Add(wci3);
+                context.ExecuteQuery();
+            }
+
         }
 
         [TestCleanup()]
@@ -105,6 +134,42 @@ namespace OfficeDevPnP.Core.Tests.AppModelExtensions
                     customBackgroundFile.DeleteObject();
                     context.ExecuteQuery();
                 }
+
+                var webCollection1 = web.Webs;
+                context.Load(webCollection1, wc => wc.Include(w => w.Title, w => w.ServerRelativeUrl));
+                context.ExecuteQuery();
+                var websToDelete = new List<Web>();
+                foreach (var web1 in webCollection1)
+                {
+                    if (web1.Title.StartsWith("Test_"))
+                    {
+                        var webCollection2 = web1.Webs;
+                        context.Load(webCollection2, wc => wc.Include(w => w.Title, w => w.ServerRelativeUrl));
+                        context.ExecuteQuery();
+                        var childrenToDelete = new List<Web>(webCollection2);
+                        foreach (var web2 in childrenToDelete)
+                        {
+                            Console.WriteLine("Deleting site {0}", web2.ServerRelativeUrl);
+                            web2.DeleteObject();
+                            context.ExecuteQuery();
+                        }
+                        websToDelete.Add(web1);
+                    }
+                }
+
+                foreach (var web1 in websToDelete)
+                {
+                    Console.WriteLine("Deleting site {0}", web1.ServerRelativeUrl);
+                    web1.DeleteObject();
+                    try
+                    {
+                        context.ExecuteQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Exception cleaning up: {0}", ex);
+                    }
+                }
             }
         }
 
@@ -156,14 +221,14 @@ namespace OfficeDevPnP.Core.Tests.AppModelExtensions
         [TestMethod()]
         public void CreateComposedLookShouldWork()
         {
-            var testLookName = string.Format("Test_{0:s}", DateTimeOffset.Now);
+            var testLookName = string.Format("Test_CL{0:yyyyMMddTHHmmss}", DateTimeOffset.Now);
 
             using (var context = TestCommon.CreateClientContext())
             {
                 context.Load(context.Web, w => w.ServerRelativeUrl);
                 context.ExecuteQuery();
-                var paletteServerRelativeUrl = context.Web.ServerRelativeUrl + "/_catalog/theme/15" + builtInPalette1;
-                var masterServerRelativeUrl = context.Web.ServerRelativeUrl + "/_catalog/masterpage" + builtInMaster1;
+                var paletteServerRelativeUrl = context.Web.ServerRelativeUrl + "/_catalog/theme/15" + builtInPalette003;
+                var masterServerRelativeUrl = context.Web.ServerRelativeUrl + "/_catalog/masterpage" + builtInMasterOslo;
                 
                 context.Web.CreateComposedLookByUrl(testLookName, paletteServerRelativeUrl, null, null, masterServerRelativeUrl, 5);
             }
@@ -179,9 +244,108 @@ namespace OfficeDevPnP.Core.Tests.AppModelExtensions
                 var item = existingCollection.FirstOrDefault();
 
                 var lookPaletteUrl = item["ThemeUrl"] as FieldUrlValue;
-                Assert.IsTrue(lookPaletteUrl.Url.Contains(builtInPalette1));
+                Assert.IsTrue(lookPaletteUrl.Url.Contains(builtInPalette003));
                 var lookMasterUrl = item["MasterPageUrl"] as FieldUrlValue;
-                Assert.IsTrue(lookMasterUrl.Url.Contains(builtInMaster1));
+                Assert.IsTrue(lookMasterUrl.Url.Contains(builtInMasterOslo));
+            }
+        }
+
+        [TestMethod()]
+        public void SetComposedLookInherits()
+        {
+            using (var context = TestCommon.CreateClientContext())
+            {
+                var webCollection = context.Web.Webs;
+                context.Load(webCollection, wc => wc.Include(w => w.Title));
+                context.ExecuteQuery();
+                var webToChange1 = webCollection.First(w => w.Title == testWebName);
+
+                var webCollection2 = webToChange1.Webs;
+                context.Load(webCollection2, wc => wc.Include(w => w.Title));
+                context.ExecuteQuery();
+                var webToChangeA = webCollection2.First(w => w.Title == "A");
+
+                webToChangeA.SetComposedLookByUrl(builtInLookBlossom);
+                webToChange1.SetComposedLookByUrl(builtInLookSeaMonster);
+            }
+
+            using (var context = TestCommon.CreateClientContext())
+            {
+                var webCollection = context.Web.Webs;
+                context.Load(webCollection, wc => wc.Include(w => w.Title));
+                context.ExecuteQuery();
+                var webToCheck1 = webCollection.First(w => w.Title == testWebName);
+
+                var webCollection2 = webToCheck1.Webs;
+                //context.Load(webCollection2, wc => wc.Include(w => w.Title, w => w.MasterUrl, w => w.CustomMasterUrl, w => w.ThemeInfo));
+                context.Load(webCollection2, wc => wc.Include(w => w.Title, w => w.MasterUrl, w => w.CustomMasterUrl));
+                context.ExecuteQuery();
+
+                var webToCheckB = webCollection2.First(w => w.Title == "B");
+                var webToCheckA = webCollection2.First(w => w.Title == "A");
+
+                // Sea Monster oslo, palette005, image_bg005, fontscheme003
+                Assert.IsTrue(webToCheckB.MasterUrl.Contains(builtInMasterOslo));
+                // Blossom seattle, palette002, image_bg002
+                Assert.IsTrue(webToCheckA.MasterUrl.Contains(builtInMasterSeattle));
+                
+                // TODO: A way to check the theme has been applied. web.ThemeInfo isn't working
+
+                //var themeInfoB = webToCheckB.ThemeInfo;
+                //Console.WriteLine("Load themeInfoB");
+                //context.Load(themeInfoB);
+                //context.ExecuteQuery(); // Fails with "The object id "ThemeInfo-..." is invalid"
+                //Console.WriteLine("Use themeInfoB");
+                //var accentTextB = themeInfoB.GetThemeShadeByName("AccentText");
+                //context.ExecuteQuery();
+                //var themeInfoA = webToCheckA.ThemeInfo;
+                //context.Load(themeInfoA);
+                //context.ExecuteQuery();
+                //var accentTextA = themeInfoA.GetThemeShadeByName("AccentText");
+                //context.ExecuteQuery();
+
+                //Assert.AreEqual("F07200", accentTextB.Value);
+                //Assert.AreEqual("D55881", accentTextA.Value);
+            }
+        }
+
+        [TestMethod()]
+        public void SetComposedLookResetInheritance()
+        {
+            using (var context = TestCommon.CreateClientContext())
+            {
+                var webCollection = context.Web.Webs;
+                context.Load(webCollection, wc => wc.Include(w => w.Title));
+                context.ExecuteQuery();
+                var webToChange1 = webCollection.First(w => w.Title == testWebName);
+
+                var webCollection2 = webToChange1.Webs;
+                context.Load(webCollection2, wc => wc.Include(w => w.Title));
+                context.ExecuteQuery();
+                var webToChangeA = webCollection2.First(w => w.Title == "A");
+
+                webToChangeA.SetComposedLookByUrl(builtInLookBlossom);
+                webToChange1.SetComposedLookByUrl(builtInLookSeaMonster, resetSubsitesToInherit:true);
+            }
+
+            using (var context = TestCommon.CreateClientContext())
+            {
+                var webCollection = context.Web.Webs;
+                context.Load(webCollection, wc => wc.Include(w => w.Title));
+                context.ExecuteQuery();
+                var webToCheck1 = webCollection.First(w => w.Title == testWebName);
+
+                var webCollection2 = webToCheck1.Webs;
+                context.Load(webCollection2, wc => wc.Include(w => w.Title, w => w.MasterUrl, w => w.CustomMasterUrl, w => w.ThemeInfo));
+                context.ExecuteQuery();
+                var webToCheckA = webCollection2.First(w => w.Title == "A");
+
+                // Sea Monster oslo, palette005, image_bg005, fontscheme003
+                Assert.IsTrue(webToCheckA.MasterUrl.Contains(builtInMasterOslo));
+
+                //var themeInfoA = webToCheckA.ThemeInfo;
+                
+                //Assert.AreEqual("F07200", themeInfoA.GetThemeShadeByName("AccentText"));
             }
         }
 
