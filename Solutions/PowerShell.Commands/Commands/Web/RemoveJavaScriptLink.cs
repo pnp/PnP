227ea57663979a236bc0499290ff5c73f731593f
Erwin van Hunen <erwin.van.hunen@outlook.com>
2014-11-14 13:35:50 +0100

    New cmdlet

---
+++ Solutions/PowerShell.Commands/Commands/Web/RemoveJavaScriptLink.cs
@@ -0,0 +1,22 @@
+﻿using OfficeDevPnP.PowerShell.CmdletHelpAttributes;
+using OfficeDevPnP.PowerShell.Commands.Base;
+using Microsoft.SharePoint.Client;
+using System.Collections.Generic;
+using System.Management.Automation;
+using OfficeDevPnP.Core.Entities;
+
+namespace OfficeDevPnP.PowerShell.Commands
+{
+    [Cmdlet(VerbsCommon.Remove, "SPOJavaScriptLink")]
+    [CmdletHelp("Removes a JavaScript link or block from a web")]
+    public class RemoveJavaScriptLink : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string Key = string.Empty;
+
+        protected override void ExecuteCmdlet()
+        {
+            this.SelectedWeb.DeleteJsLink(Key);
+        }
+    }
+}
