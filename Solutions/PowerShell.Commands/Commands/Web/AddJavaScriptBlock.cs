df74bac8aa38ffde27f966c08366e77938e35aa5
Erwin van Hunen <erwin.van.hunen@outlook.com>
2014-11-14 13:30:12 +0100

    New JavaScript cmdlets

---
+++ Solutions/PowerShell.Commands/Commands/Web/AddJavaScriptBlock.cs
@@ -0,0 +1,25 @@
+﻿using OfficeDevPnP.PowerShell.CmdletHelpAttributes;
+using OfficeDevPnP.PowerShell.Commands.Base;
+using Microsoft.SharePoint.Client;
+using System.Collections.Generic;
+using System.Management.Automation;
+using OfficeDevPnP.Core.Entities;
+
+namespace OfficeDevPnP.PowerShell.Commands
+{
+    [Cmdlet(VerbsCommon.Add, "SPOJavascriptBlock")]
+    [CmdletHelp("Adds a link to a JavaScript snippet/block to a web")]
+    public class AddJavaScriptBlock : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string Key = string.Empty;
+
+        [Parameter(Mandatory = true)]
+        public string Script = null;
+       
+        protected override void ExecuteCmdlet()
+        {
+            this.SelectedWeb.AddJsBlock(Key,Script);
+        }
+    }
+}
