7e76dd621733510f2cd98a89ccb64a5783e1a12a
vman <vardhaman.rd@gmail.com>
2014-11-16 18:09:51 +0000

    initial checkin of unit testing apps project

---
+++ Samples/UnitTesting.SharePoint.Apps/UnitTesting.SharePoint.AppsWeb/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace UnitTesting.SharePoint.AppsWeb.Controllers
+{
+    public class TestController : Controller
+    {
+
+        [SharePointContextFilter]
+        public ActionResult Index()
+        {
+            var spContext = HttpContext.Session["SPContext"] as SharePointAcsContext;
+
+            ViewBag.ContextToken = spContext.ContextToken;
+
+            ViewBag.ClientId = string.IsNullOrEmpty(WebConfigurationManager.AppSettings.Get("ClientId")) ? WebConfigurationManager.AppSettings.Get("HostedAppName") : WebConfigurationManager.AppSettings.Get("ClientId");
+
+            ViewBag.ClientSecret = string.IsNullOrEmpty(WebConfigurationManager.AppSettings.Get("ClientSecret")) ? WebConfigurationManager.AppSettings.Get("HostedAppSigningKey") : WebConfigurationManager.AppSettings.Get("ClientSecret");
+
+            return View();
+
+        }
+    }
+
+
+}
