e4269fb60c239ed10c8d317d43cad6aad9a9c812
Erwin van Hunen <erwin.van.hunen@outlook.com>
2015-01-02 23:50:17 +0100

    Enum used by Set-SPOGroup

---
+++ Solutions/PowerShell.Commands/Commands/Enums/AssociatedGroupTypeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.PowerShell.Commands.Enums
+{
+    public enum AssociatedGroupTypeEnum
+    {
+        None,
+        Visitors,
+        Members,
+        Owners
+    }
+}
+
