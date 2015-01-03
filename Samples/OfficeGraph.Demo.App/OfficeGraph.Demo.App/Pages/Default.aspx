7487997f9fdd151346f02a69e245e74e521646a7
vman <vardhaman.rd@gmail.com>
2015-01-03 14:47:25 +0000

    Adding office graph demo solution

---
+++ Samples/OfficeGraph.Demo.App/OfficeGraph.Demo.App/Pages/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%-- The following 4 lines are ASP.NET directives needed when using SharePoint components --%>
+
+<%@ Page Inherits="Microsoft.SharePoint.WebPartPages.WebPartPage, Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" MasterPageFile="~masterurl/default.master" Language="C#" %>
+
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+
+<%-- The markup and script in the following Content element will be placed in the <head> of the page --%>
+<asp:Content ContentPlaceHolderID="PlaceHolderAdditionalPageHead" runat="server">
+    <script type="text/javascript" src="../Scripts/jquery-1.9.1.min.js"></script>
+    <meta name="WebPartPageExpansion" content="full" />
+
+    <!-- Add your CSS styles to the following file -->
+    <link rel="Stylesheet" type="text/css" href="../Content/App.css" />
+
+    <!-- Add your JavaScript to the following file -->
+    <script type="text/javascript" src="../Scripts/App.js"></script>
+</asp:Content>
+
+<%-- The markup in the following Content element will be placed in the TitleArea of the page --%>
+<asp:Content ContentPlaceHolderID="PlaceHolderPageTitleInTitleArea" runat="server">
+    Page Title
+</asp:Content>
+
+<%-- The markup and script in the following Content element will be placed in the <body> of the page --%>
+<asp:Content ContentPlaceHolderID="PlaceHolderMain" runat="server">
+
+
+    <div id="OGPreviews">
+    </div>
+
+    <WebPartPages:AllowFraming runat="server" />
+</asp:Content>
---
+++ Samples/OfficeGraph.Demo.App/OfficeGraph.Demo.App/Scripts/App.js
@@ -0,0 +1,72 @@
+﻿'use strict';
+
+(function ($) {
+
+    $(document).ready(loadEverything);
+
+    function loadEverything() {
+
+        var isDialog = getQueryStringParameterByName('IsDlg');
+        if (isDialog == '1') {
+            $("#globalNavBox").hide();
+        }
+
+        var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/search/query?Querytext='*'" +
+					"&Properties='GraphQuery:ACTOR(ME\\, action\\:1019),GraphRankingModel:{\"features\"\\:[{\"function\"\\:\"EdgeWeight\"}]}'" +
+					"&RankingModelId='0c77ded8-c3ef-466d-929d-905670ea1d72'" +
+					"&SelectProperties='Title,UserName,Path'" +
+					"&RowLimit=10";
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET",
+            headers: { "Accept": "application/json; odata=nometadata" },
+            success: function (data) {
+                var results = [];
+
+                $(data.PrimaryQueryResult.RelevantResults.Table.Rows).each(function (i, e) {
+                    var o = {};
+                    $(e.Cells).each(function (ii, ee) {
+
+                        if (ee.Key == 'Title')
+                            o.title = ee.Value;
+                        else if (ee.Key == 'UserName')
+                            o.username = ee.Value;
+                        else if (ee.Key == 'Path')
+                            o.path = ee.Value;
+
+                    });
+
+                    //build an image
+                    o.pic = _spPageContextInfo.webAbsoluteUrl + '/_layouts/15/userphoto.aspx?size=m&accountname=' + o.username;
+
+                    results.push(o);
+                })
+                RenderEverything(results);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus + " : " + errorThrown);
+            }
+        });
+
+    }
+
+    function RenderEverything(results) {
+
+        var previewsContainer = $("#OGPreviews");
+
+        $(results).each(function (i, e) {
+
+            var theImg = $("<a target='_blank' href='" + e.path + "'><img src=" + e.pic + " title='" + e.title + "'></img></a>");
+
+            previewsContainer.append(theImg);
+
+        })
+    }
+
+    function getQueryStringParameterByName(name) {
+        var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+        return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+    }
+
+})(jQuery);
