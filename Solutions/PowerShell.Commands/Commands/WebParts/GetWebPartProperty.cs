e026548eb72683459aef73388aec6bbfbf047e8f
Erwin van Hunen <erwin.van.hunen@outlook.com>
2015-01-07 11:27:15 +0100

    New cmdlet to return webpart properties

---
+++ Solutions/PowerShell.Commands/Commands/WebParts/GetWebPartProperty.cs
@@ -0,0 +1,26 @@
+﻿using OfficeDevPnP.PowerShell.Commands.Base;
+using OfficeDevPnP.PowerShell.Commands.Base.PipeBinds;
+using Microsoft.SharePoint.Client;
+using System.Management.Automation;
+
+namespace OfficeDevPnP.PowerShell.Commands
+{
+    [Cmdlet(VerbsCommon.Get, "SPOWebPartProperty")]
+    public class GetWebPartProperty : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string PageUrl = string.Empty;
+
+        [Parameter(Mandatory = true)]
+        public GuidPipeBind Identity;
+
+        protected override void ExecuteCmdlet()
+        {
+            var properties = this.SelectedWeb.GetWebPartProperties(Identity.Id, PageUrl);
+            WriteObject(properties.FieldValues);
+        }
+
+
+
+    }
+}
