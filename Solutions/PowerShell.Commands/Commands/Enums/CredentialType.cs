9e6555bee81880c51daa632807298bda9bec21d3
Erwin van Hunen <erwin.van.hunen@outlook.com>
2015-01-22 17:08:59 +0100

    Updated cmdlet to use Core.Utilities.CredentialManager

---
+++ Solutions/PowerShell.Commands/Commands/Enums/CredentialType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.PowerShell.Commands.Enums
+{
+    public enum CredentialType
+    {
+        OnPrem = 0,
+        O365 = 1,
+        PSCredential = 2
+    }
+}
