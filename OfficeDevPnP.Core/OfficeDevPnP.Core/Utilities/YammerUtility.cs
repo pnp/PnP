7bd9051a05060de7253ad6d477bd656fed4e1284
Nanddeep Nachan <nanddeepnachan@gmail.com>
2015-01-22 16:10:54 +0200

    Made GetYammerJson and PostYammerJson requests thread safe

--- OfficeDevPnP.Core/OfficeDevPnP.Core/Utilities/YammerUtility.cs
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Utilities/YammerUtility.cs
@@ -5,18 +5,33 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace OfficeDevPnP.Core.Utilities
 {
     public static class YammerUtility
     {
         /// <summary>
+        /// Lock object to make GetYammerJson requests thread safe
+        /// </summary>
+        private static readonly object lockObjGetJson = new object();
+
+        /// <summary>
+        /// Lock object to make PostYammerJson requests thread safe
+        /// </summary>
+        private static readonly object lockObjPostJson = new object();
+
+        /// <summary>
+        /// Cache to store yammer json response.
+        /// This will help minimize extra GetYammerJson requests.
+        /// </summary>
+        private static List<KeyValuePair<string, string>> cacheYammerJsonResponse = new List<KeyValuePair<string, string>>();
+
+        /// <summary>
         /// Returns Yammer Group if group exists. If the group does not exist, returns null.
         /// </summary>
         /// <param name="groupName">Group name to search for</param>
-        /// <param name="accessToken"></param>
-        /// <returns>Returns Yammer Group is group exists. If group does not exists, returns null.</returns>
+        /// <param name="accessToken">Access token to the Yammer network</param>
+        /// <returns>Yammer Group, if exists. otherwise null.</returns>
         public static YammerGroup GetYammerGroupByName(string groupName, string accessToken)
         {
             YammerGroup yamGroup = null;
@@ -29,20 +44,21 @@ namespace OfficeDevPnP.Core.Utilities
                     yamGroup = item;
                 }
             }
+
             return yamGroup;
         }
 
         /// <summary>
         /// Returns Yammer Group if group exists. If the group does not exist, returns null.
         /// </summary>
-        /// <param name="groupName">Group name to search for</param>
+        /// <param name="groupId">Group id to search for</param>
         /// <param name="accessToken"></param>
-        /// <returns>Returns Yammer Group is group exists. If group does not exists, returns null.</returns>
+        /// <returns>Yammer Group, if exists. otherwise null.</returns>
         public static YammerGroup GetYammerGroupById(int groupId, string accessToken)
         {
             YammerGroup yamGroup = null;
             var groups = GetYammerGroups(accessToken);
-           
+
             foreach (var item in groups)
             {
                 if (item.id == groupId)
@@ -50,6 +66,7 @@ namespace OfficeDevPnP.Core.Utilities
                     yamGroup = item;
                 }
             }
+
             return yamGroup;
         }
 
@@ -77,73 +94,84 @@ namespace OfficeDevPnP.Core.Utilities
             return groups;
         }
 
+        /// <summary>
+        /// Returns Yammer user based on the access token.
+        /// </summary>
+        /// <param name="accessToken">Access token to the Yammer network</param>
+        /// <returns>Yammer user in the network</returns>
         public static YammerUser GetYammerUser(string accessToken)
         {
-            //get service account token
+            // Get service account token
             var response = GetYammerJson(String.Format("https://www.yammer.com/api/v1/users/current.json?access_token={0}", accessToken), accessToken);
             return JsonUtility.Deserialize<YammerUser>(response);
         }
 
         /// <summary>
-        /// Can be used to create Yammer group to the Yammer network
+        /// Creates Yammer group to the Yammer network
         /// </summary>
-        /// <param name="groupName"></param>
-        /// <param name="isPrivate"></param>
-        /// <param name="accessToken"></param>
-        /// <returns></returns>
+        /// <param name="groupName">Name of the Yammer group to create</param>
+        /// <param name="isPrivate">Visibility of the Yammer group</param>
+        /// <param name="accessToken">Access token to the Yammer network</param>
+        /// <returns>Yammer group</returns>
         public static YammerGroup CreateYammerGroup(string groupName, bool isPrivate, string accessToken)
         {
-            //Get reference existing group if exists
+            // Get reference of existing group, if exists
             YammerGroup yamGroup = GetYammerGroupByName(groupName, accessToken);
             if (yamGroup == null)
             {
-                //Create yammer group
+                // Create yammer group
                 string url = String.Format("https://www.yammer.com/api/v1/groups.json?name={0}&private={1}", groupName, isPrivate.ToString().ToLower());
                 PostYammerJson(url, accessToken);
                 yamGroup = GetYammerGroupByName(groupName, accessToken);
             }
+
             return yamGroup;
         }
 
         /// <summary>
         /// Creates web part entity with the Yammer group structure on it
         /// </summary>
-        /// <param name="yammerNetworkName"></param>
-        /// <param name="url"></param>
-        /// <param name="showHeader"></param>
-        /// <param name="showFooter"></param>
-        /// <returns></returns>
+        /// <param name="yammerNetworkName">Name of the Yammer network</param>
+        /// <param name="yammerGroupId">Id of the Yammer group</param>
+        /// <param name="showHeader">Show header in web part</param>
+        /// <param name="showFooter">Show footer in web part</param>
+        /// <returns>Web part entity with the Yammer group</returns>
         public static WebPartEntity GetYammerGroupDiscussionPart(string yammerNetworkName, int yammerGroupId, bool showHeader, bool showFooter)
         {
             WebPartEntity wpYammer = new WebPartEntity();
             wpYammer.WebPartXml = CreateYammerGroupDiscussionPartXml(yammerNetworkName, yammerGroupId, showHeader, showFooter);
             wpYammer.WebPartIndex = 0;
             wpYammer.WebPartTitle = "Yammer";
+
             return wpYammer;
         }
 
         /// <summary>
         /// Creates web part entity with the Yammer OpenGraph structure on it for specific URL
         /// </summary>
-        /// <param name="yammerNetworkName"></param>
-        /// <param name="url"></param>
-        /// <param name="showHeader"></param>
-        /// <param name="showFooter"></param>
-        /// <returns></returns>
+        /// <param name="yammerNetworkName">Name of the Yammer network</param>
+        /// <param name="url">Url of Yammer OpenGraph</param>
+        /// <param name="showHeader">Show header in web part</param>
+        /// <param name="showFooter">Show footer in web part</param>
+        /// <returns>Web part entity with the Yammer OpenGraph</returns>
         public static WebPartEntity GetYammerOpenGraphDiscussionPart(string yammerNetworkName, string url, bool showHeader, bool showFooter, string postTitle = "", string postImageUrl = "", string defaultGroupId = "")
         {
             WebPartEntity wpYammer = new WebPartEntity();
             wpYammer.WebPartXml = CreateYammerOpenGraphDiscussionPartXml(yammerNetworkName, url, showHeader, showFooter, postTitle, postImageUrl, true, defaultGroupId);
             wpYammer.WebPartIndex = 0;
             wpYammer.WebPartTitle = "Yammer";
+
             return wpYammer;
         }
 
         /// <summary>
         /// Constructs the webpart XML for yammer group needed to inject as Yammer web part to SharePoint page
         /// </summary>
-        /// <param name="yammerNetwork">ID of the network</param>
-        /// <param name="yammerGroupId">Group ID</param>
+        /// <param name="yammerNetwork">Name of the Yammer network</param>
+        /// <param name="yammerGroupId">ID of the Yammer group</param>
+        /// <param name="showHeader">Show header in web part</param>
+        /// <param name="showFooter">Show footer in web part</param>
+        /// <param name="useSSO">Use SSO</param>
         /// <returns>The constructed web part XML</returns>
         public static string CreateYammerDiscussionPartXml(string yammerNetworkName, int yammerGroupId, bool showHeader, bool showFooter, bool useSSO = true)
         {
@@ -153,8 +181,11 @@ namespace OfficeDevPnP.Core.Utilities
         /// <summary>
         /// Constructs the webpart XML for yammer group needed to inject as Yammer web part to SharePoint page
         /// </summary>
-        /// <param name="yammerNetwork">ID of the network</param>
-        /// <param name="yammerGroupId">Group ID</param>
+        /// <param name="yammerNetwork">Name of the Yammer network</param>
+        /// <param name="yammerGroupId">ID of the Yammer group</param>
+        /// <param name="showHeader">Show header in web part</param>
+        /// <param name="showFooter">Show footer in web part</param>
+        /// <param name="useSSO">Use SSO</param>
         /// <returns>The constructed web part XML</returns>
         public static string CreateYammerGroupDiscussionPartXml(string yammerNetworkName, int yammerGroupId, bool showHeader, bool showFooter, bool useSSO = true)
         {
@@ -194,14 +225,17 @@ namespace OfficeDevPnP.Core.Utilities
         /// <summary>
         /// Constructs web part definition for Open Graph discussion web part definition
         /// </summary>
-        /// <param name="yammerNetworkName"></param>
-        /// <param name="yammerGroupId"></param>
-        /// <param name="showHeader"></param>
-        /// <param name="showFooter"></param>
-        /// <param name="useSSO"></param>
-        /// <returns></returns>
-        public static string CreateYammerOpenGraphDiscussionPartXml(string yammerNetworkName, string url, bool showHeader, 
-                                                                    bool showFooter, string postTitle="", string postImageUrl="", 
+        /// <param name="yammerNetworkName">Name of the Yammer network</param>
+        /// <param name="url">Url of the Yammer Open Graph Discussion</param>
+        /// <param name="showHeader">Show header in web part</param>
+        /// <param name="showFooter">Show footer in web part</param>
+        /// <param name="postTitle">Title of the post</param>
+        /// <param name="postImageUrl">Image url of the post</param>
+        /// <param name="useSSO">Use SSO</param>
+        /// <param name="groupId">Default Group Id</param>
+        /// <returns>Web part definition for Open Graph discussion web part</returns>
+        public static string CreateYammerOpenGraphDiscussionPartXml(string yammerNetworkName, string url, bool showHeader,
+                                                                    bool showFooter, string postTitle = "", string postImageUrl = "",
                                                                     bool useSSO = true, string groupId = "")
         {
             StringBuilder wp = new StringBuilder(100);
@@ -236,10 +270,10 @@ namespace OfficeDevPnP.Core.Utilities
             wp.Append("				                     , promptText: 'Start a conversation'");
             if (!string.IsNullOrEmpty(groupId))
             {
-                wp.Append("				                 , defaultGroupId: '" + groupId + "'"); 
+                wp.Append("				                 , defaultGroupId: '" + groupId + "'");
             }
             wp.Append("				                }");
-            wp.Append("				                , objectProperties: {"); 
+            wp.Append("				                , objectProperties: {");
             wp.Append("				                  url: '" + url + "'");
             wp.Append("				                  , type: 'page'");
             wp.Append("				                  , title: '" + postTitle + "'");
@@ -257,35 +291,79 @@ namespace OfficeDevPnP.Core.Utilities
             return wp.ToString();
         }
 
-        private static string GetYammerJson(string url, string accessToken)
+        /// <summary>
+        /// Get Yammer Json to create yammer response
+        /// </summary>
+        /// <param name="url">Yammer Json url</param>
+        /// <param name="yammerAccessToken">Yammer access token</param>
+        /// <returns>Yammer Json</returns>
+        private static string GetYammerJson(string url, string yammerAccessToken)
         {
-            //make the request
-            string json = null;
-            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-            request.Headers.Add("Authorization", "Bearer" + " " + accessToken);
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            // Yammer throws (429) too many requests exception for consecutive requests.
+            // The "Rate Limits" are defined here: https://developer.yammer.com/restapi/
+            // Thread safe to allow only one request at a time.
+            lock (lockObjGetJson)
             {
-                Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
-                StreamReader reader = new StreamReader(response.GetResponseStream(), encode);
-                json = reader.ReadToEnd();
+                // Check if information is present in cache
+                KeyValuePair<string, string> hashInformation = cacheYammerJsonResponse.FirstOrDefault(x => x.Key == yammerAccessToken + "_" + url);
+                if (hashInformation.Key != null)
+                {
+                    return hashInformation.Value;
+                }
+
+                string json = null;
+
+                // Make the request
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Headers.Add("Authorization", "Bearer" + " " + yammerAccessToken);
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream(), encode))
+                    {
+                        json = reader.ReadToEnd();
+                    }
+                }
+
+                // Store information in cache
+                cacheYammerJsonResponse.Add(new KeyValuePair<string, string>(yammerAccessToken + "_" + url, json));
+
+                return json;
             }
-            return json;
         }
 
-        private static string PostYammerJson(string url, string accessToken)
+        /// <summary>
+        /// Post Yammer json to get the result
+        /// </summary>
+        /// <param name="url">Yammer Json url</param>
+        /// <param name="yammerAccessToken">Yammer access token</param>
+        /// <returns>Yammer Json</returns>
+        private static string PostYammerJson(string url, string yammerAccessToken)
         {
-            //make the request
-            string json = null;
-            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-            request.Method = "POST";
-            request.Headers.Add("Authorization", "Bearer" + " " + accessToken);
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            // Yammer throws (429) too many requests exception for consecutive requests. 
+            // The "Rate Limits" are defined here: https://developer.yammer.com/restapi/
+            // Thread safe to allow only one request at a time.
+            lock (lockObjPostJson)
             {
-                Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
-                StreamReader reader = new StreamReader(response.GetResponseStream(), encode);
-                json = reader.ReadToEnd();
+                string json = null;
+
+                // Make the request
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "POST";
+                request.Headers.Add("Authorization", "Bearer" + " " + yammerAccessToken);
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream(), encode))
+                    {
+                        json = reader.ReadToEnd();
+                    }
+                }
+
+                return json;
             }
-            return json;
         }
     }
 }
