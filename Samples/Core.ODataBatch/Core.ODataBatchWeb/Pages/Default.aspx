f2d27fdc204be0f60964e19f5ddfbc7c214c60d0
Rick-Kirkham <rickeeee@msn.com>
2015-01-31 16:51:15 -0800

    Moving two samples from OfficeDev to the PnP umbrella

---
+++ Samples/Core.ODataBatch/Core.ODataBatchWeb/Pages/Default.aspx
@@ -0,0 +1,119 @@
+﻿
+<%--
+ Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
+--%>
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Core.ODataBatchWeb.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body bgcolor="#000000">
+    <form id="form1" runat="server">
+    <div>
+        <h1 style="font-family: 'Segoe UI'; border-style: none; text-wrap: normal; font-weight: normal; color:white">
+            Using the $batch query option with the SharePoint REST APIs
+        </h1>  
+        <p style="font-family: 'Segoe UI'; border-style: none; text-wrap: normal; font-weight: normal; color:white">
+            This sample is most useful if you are using the <a href="http://www.telerik.com/fiddler">Fiddler tool</a> to examine the HTTP requests and responses.
+          </p>      
+        <h2 style="font-family: 'Segoe UI'; border-style: none; text-wrap: normal; font-weight: normal; color:white">Batch Job 1: Two GETs</h2>
+
+        <p style="font-family: 'Segoe UI'; border-style: none; text-wrap: normal; font-weight: normal; color:white">
+            Click the button to get the <b>Users</b> list and the <b>Composed Looks</b> list in a single call to SharePoint.<br />
+            Then scroll down to try batch job #2.
+         <asp:Literal ID="Literal1" runat="server"><br /></asp:Literal>
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" 
+        Text="BATCH REQUEST 1" BackColor="#00FFFF" ForeColor="Black" Font-Size="Large" 
+        Style="font-family: 'Segoe UI'; border-style: none; text-wrap: normal; font-weight: normal" 
+        Height="50px" Width="239px" />&nbsp;&nbsp;&nbsp;
+        </p>
+
+        <asp:Table ID="TwoLists" runat="server">
+            <asp:TableRow VerticalAlign="Top" >
+                <asp:TableCell>   
+                    <asp:GridView ID="GridView1" runat="server"  BackColor="#808080" ForeColor="White"
+                        BorderColor="#0033CC" BorderStyle="Solid" Caption="Users" 
+                        CaptionAlign="Left" CellPadding="5" Style="font-family: 'Segoe UI'" GridLines="None" 
+                        HorizontalAlign="Left">
+                        <AlternatingRowStyle BackColor="White" ForeColor="Black" />
+                    </asp:GridView>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:GridView ID="GridView2" runat="server" BackColor="#808080" ForeColor="White"
+                        BorderColor="#0033CC" BorderStyle="Solid" Caption="Composed Looks" 
+                        CaptionAlign="Left" CellPadding="5" Style="font-family: 'Segoe UI'" GridLines="None" 
+                        HorizontalAlign="Left" Width="200">
+                        <AlternatingRowStyle BackColor="White" ForeColor="Black" />
+                    </asp:GridView>
+               </asp:TableCell>
+       </asp:TableRow>
+
+        </asp:Table>
+</div>
+        <div>
+          <asp:Table ID="Table1" runat="server">
+            <asp:TableRow VerticalAlign="Top" >
+                <asp:TableCell>  
+        <asp:Literal ID="Literal8" runat="server"><br /><br /></asp:Literal>
+
+        <h2 style="font-family: 'Segoe UI'; border-style: none; text-wrap: normal; font-weight: normal; color:white">Batch job 2: POST and GET</h2>
+        <p style="font-family: 'Segoe UI'; border-style: none; text-wrap: normal; font-weight: normal; color:white">
+            Enter a new list title in the box and then click the button to add a new list and get the <b>List of Lists</b> in a single call to SharePoint.<br />
+            Then scroll down to try batch job #3.</p>
+        <asp:Literal ID="Literal2" runat="server"><br /></asp:Literal>
+        <asp:TextBox ID="NewList" runat="server">Enter new list title.</asp:TextBox>&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" 
+        Text="BATCH REQUEST 2" BackColor="#00FFFF" ForeColor="Black" Font-Size="Large" 
+        Style="font-family: 'Segoe UI'; border-style: none; text-wrap: normal; font-weight: normal" 
+        Height="50px" Width="239px" />&nbsp;&nbsp;&nbsp;
+        
+        <asp:Label ID="AddListResponse" runat="server" Style="font-family: 'Segoe UI'; color:white"></asp:Label>
+
+        <asp:GridView ID="GridView3" runat="server" BackColor="#808080" ForeColor="Black"
+        BorderColor="#0033CC" BorderStyle="Solid" Caption="List of Lists" 
+        CaptionAlign="Left" CellPadding="5" Style="font-family: 'Segoe UI'" GridLines="None" 
+        HorizontalAlign="Left" Width="200">
+        <AlternatingRowStyle BackColor="White" ForeColor="Black" />
+              </asp:GridView>
+               </asp:TableCell>
+           </asp:TableRow>
+         </asp:Table>
+       </div>
+
+        <div>
+
+         <asp:Literal ID="Literal7" runat="server"><br /><br /></asp:Literal>
+
+        <h2 style="font-family: 'Segoe UI'; border-style: none; text-wrap: normal; font-weight: normal; color:white">Batch Job 3: DELETE and GET</h2>
+        <p style="font-family: 'Segoe UI'; border-style: none; text-wrap: normal; font-weight: normal; color:white"> 
+         Enter the name of a list to delete and then click the button delete it and get the updated <b>List of Lists</b> in a single call to SharePoint.<br />
+        <i>Delete only lists that were created with this app. Deleting built-in SharePoint lists or lists created with other apps could have unpredictable effects.</i></p>
+        <asp:Literal ID="Literal5" runat="server"><br /></asp:Literal>
+        <asp:TextBox ID="OldList" runat="server">Enter existing list title.</asp:TextBox>&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" 
+        Text="BATCH REQUEST 3" BackColor="#00FFFF" ForeColor="Black" Font-Size="Large" 
+        Style="font-family: 'Segoe UI'; border-style: none; text-wrap: normal; font-weight: normal" 
+        Height="50px" Width="239px" />&nbsp;&nbsp;&nbsp;
+       
+        <asp:Label ID="DeleteListResponse" runat="server" Style="font-family: 'Segoe UI'; color:white"></asp:Label>
+
+        <asp:Literal ID="Literal6" runat="server"><br /><br /></asp:Literal>
+        <asp:GridView ID="GridView4" runat="server" BackColor="#808080" ForeColor="White"
+        BorderColor="#0033CC" BorderStyle="Solid" Caption="List of Lists" 
+        CaptionAlign="Left" CellPadding="5" Style="font-family: 'Segoe UI'" GridLines="None" 
+        HorizontalAlign="Left" Width="200">
+        <AlternatingRowStyle BackColor="White" ForeColor="Black" />
+              </asp:GridView>
+
+        </div>
+        
+
+    </form>
+</body>
+</html>
+
+
---
+++ Samples/Core.ODataBatch/Core.ODataBatchWeb/Pages/Default.aspx.cs
@@ -0,0 +1,419 @@
+﻿/*
+ *  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Microsoft.SharePoint.Client;
+//using Microsoft.SharePoint.Samples;
+using Microsoft.IdentityModel.S2S.Tokens;
+using System.Net;
+using System.IO;
+using System.Xml;
+
+using System.Data;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Microsoft.Data.OData;
+
+using Core.ODataBatchWeb.ODataHelpers;
+
+namespace Core.ODataBatchWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        SharePointContextToken contextToken;
+        string accessToken;
+        Uri sharepointUrl;
+      
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            Uri redirectUrl;
+            switch (SharePointContextProvider.CheckRedirectionStatus(Context, out redirectUrl))
+            {
+                case RedirectionStatus.Ok:
+                    return;
+                case RedirectionStatus.ShouldRedirect:
+                    Response.Redirect(redirectUrl.AbsoluteUri, endResponse: true);
+                    break;
+                case RedirectionStatus.CanNotRedirect:
+                    Response.Write("An error occurred while processing your request.");
+                    Response.End();
+                    break;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string contextTokenString = TokenHelper.GetContextTokenFromRequest(Request);
+
+            if (contextTokenString != null)
+            {
+                contextToken =
+                    TokenHelper.ReadAndValidateContextToken(contextTokenString, Request.Url.Authority);
+
+                sharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+                accessToken =
+                    TokenHelper.GetAccessToken(contextToken, sharepointUrl.Authority).AccessToken;
+               
+                // Cache the access token in the command argument of the buttons. Don't do this in a production app.
+                Button2.CommandArgument = accessToken;
+                Button3.CommandArgument = accessToken;
+                Button4.CommandArgument = accessToken;
+            }
+            else if (!IsPostBack)
+            {
+                Response.Write("Could not find a context token.");
+            } 
+        }
+
+
+            protected void Button2_Click(object sender, EventArgs e)
+        {
+            string accessToken = ((Button)sender).CommandArgument;
+            Int16 listRetrievalCount = 0;
+
+            if (IsPostBack)
+            {
+                // Get the host web's URL.
+                sharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+            }
+            
+            // Create the parent request
+            var batchRequest = new BatchODataRequest(sharepointUrl + "_api/"); // ctor adds "$batch"
+            batchRequest.SetHeader("Authorization", "Bearer " + accessToken);
+            
+            using (var oDataMessageWriter = new ODataMessageWriter(batchRequest))
+            {
+                var oDataBatchWriter = oDataMessageWriter.CreateODataBatchWriter();
+                oDataBatchWriter.WriteStartBatch();
+
+               // Create the two child query operations.
+               oDataBatchWriter.CreateOperationRequestMessage(
+                    "GET", new Uri(sharepointUrl.ToString() + "_api/Web/lists/getbytitle('Composed Looks')/items?$select=Title"));
+                listRetrievalCount++;
+
+                oDataBatchWriter.CreateOperationRequestMessage(
+                   "GET", new Uri(sharepointUrl.ToString() + "_api/Web/lists/getbytitle('User Information List')/items?$select=Title"));
+                listRetrievalCount++;
+                
+                oDataBatchWriter.WriteEndBatch();
+                oDataBatchWriter.Flush();
+            }
+
+            // Parse the response and bind the data to the UI controls
+            var oDataResponse = batchRequest.GetResponse();
+
+            using (var oDataReader = new ODataMessageReader(oDataResponse))
+            {
+                var oDataBatchReader = oDataReader.CreateODataBatchReader();
+
+                while (oDataBatchReader.Read())
+                {
+                    switch (oDataBatchReader.State)
+                    {
+                        case ODataBatchReaderState.Initial:
+
+                            // Optionally, handle the start of a batch payload.
+                            break;
+                        case ODataBatchReaderState.Operation:
+
+                            // Start of an operation (either top-level or in a changeset)
+                            var operationResponse = oDataBatchReader.CreateOperationResponseMessage();
+
+                            // Response's ATOM markup parsing and presentation section
+                            using (var stream = operationResponse.GetStream())
+                            {
+                                List<XElement> entries = SharePointDataHelpers.ListDataHelper.ExtractListItemsFromATOMResponse(stream);
+
+                                var itemTitles = SharePointDataHelpers.ListDataHelper.GetItemTitles(entries);
+
+                                // Bind data to the grid on the page.
+                                   // In a production app, check operationResponse.StatusCode and handle non-200 statuses.
+                                   // For simplicity, this sample assumes status 200 (the list items are returned).
+                                switch (listRetrievalCount)
+                                {
+                                    case 2:
+                                        GridView2.DataSource = itemTitles;
+                                        GridView2.DataBind();
+                                        listRetrievalCount--;
+                                        break;
+
+                                    case 1:
+                                        GridView1.DataSource = itemTitles;
+                                        GridView1.DataBind();
+                                        listRetrievalCount--;
+                                        break;
+                                }
+                            };
+                            break;
+                        case ODataBatchReaderState.ChangesetStart:
+                            // Optionally, handle the start of a change set.
+                            break;
+
+                        case ODataBatchReaderState.ChangesetEnd:
+                            // When this sample was created, SharePoint did not support "all or nothing" transactions. 
+                            // If that changes in the future this is where you would commit the transaction.
+                            break;
+
+                        case ODataBatchReaderState.Exception:
+                            // In a producition app handle exeception. Omitted for simplicity in this sample app.
+                            break;
+                    }
+                }                
+            }
+            TwoLists.Visible = true;
+        }
+
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            string accessToken = ((Button)sender).CommandArgument;
+            Int16 operationCount = 0;
+
+            if (IsPostBack)
+            {
+                // Get the host web's URL.
+                sharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+            }
+
+            // Create the parent request
+            var batchRequest = new BatchODataRequest(sharepointUrl + "_api/"); // ctor adds "$batch"
+            batchRequest.SetHeader("Authorization", "Bearer " + accessToken);
+
+            using (var oDataMessageWriter = new ODataMessageWriter(batchRequest))
+            {
+                var oDataBatchWriter = oDataMessageWriter.CreateODataBatchWriter();
+                oDataBatchWriter.WriteStartBatch();
+
+                oDataBatchWriter.WriteStartChangeset();
+
+                // Create the list adding operation
+                var addListOperation = oDataBatchWriter.CreateOperationRequestMessage(
+                    "POST", new Uri(sharepointUrl.ToString() + "_api/lists"));
+                addListOperation.SetHeader("Content-Type", "application/json;odata=verbose");
+
+                // Write the body of the operation
+                using (var oDataInsertWriter = new ODataMessageWriter(addListOperation))
+                {
+                    var entryWriter = oDataInsertWriter.CreateODataEntryWriter();
+
+                    var insertionBody = new ODataEntry()
+                    {
+                        Properties = new[]
+                            {
+                                new ODataProperty() {Name = "Title", Value = NewList.Text},
+                                new ODataProperty() {Name = "BaseTemplate", Value = "100"}
+                            }
+                    };
+
+                    // Set the "__metadata" type property
+                    insertionBody.TypeName = "SP.List";
+
+                    entryWriter.WriteStart(insertionBody);
+                    entryWriter.WriteEnd();
+                }
+                oDataBatchWriter.WriteEndChangeset();
+                operationCount++;
+
+                // Create the query operation
+                var queryOperationMessage3 = oDataBatchWriter.CreateOperationRequestMessage(
+                    "GET", new Uri(sharepointUrl.ToString() + "_api/Web/lists"));
+                operationCount++;
+
+                oDataBatchWriter.WriteEndBatch();
+                oDataBatchWriter.Flush();
+            }
+
+            // Parse the response and bind the data to the UI controls
+            var oDataResponse = batchRequest.GetResponse();
+
+            using (var oDataReader = new ODataMessageReader(oDataResponse))
+            {
+                var oDataBatchReader = oDataReader.CreateODataBatchReader();
+
+                while (oDataBatchReader.Read())
+                {
+                    switch (oDataBatchReader.State)
+                    {
+                        case ODataBatchReaderState.Initial:
+                            // Optionally, handle the start of a batch payload.
+                            break;
+
+                        case ODataBatchReaderState.Operation:
+                            // Encountered an operation (either top-level or in a changeset)
+                            var operationResponse = oDataBatchReader.CreateOperationResponseMessage();
+
+                            // Response ATOM markup parsing and presentation
+                            using (var stream = operationResponse.GetStream())
+                            {
+
+                                switch (operationCount)
+                                {
+                                    case 2: // The "add new list" operation
+                                        
+                                        if (operationResponse.StatusCode == 201)
+                                        {
+                                            AddListResponse.Text = "Your list was created!";
+                                        }
+                                        else
+                                        {
+                                            AddListResponse.Text = "Your list was not created. Status returned: " + operationResponse.StatusCode.ToString();
+                                        }
+
+                                        operationCount--;
+                                        break;
+
+                                    case 1: // The "List of Lists" operation
+
+                                        // Bind data to the grid on the page.
+                                           // In a production app, check operationResponse.StatusCode and handle non-200 statuses.
+                                           // For simplicity, this sample assumes status 200 (the list items are returned).
+                                        List<XElement> entries = SharePointDataHelpers.ListDataHelper.ExtractListItemsFromATOMResponse(stream);
+                                        var itemTitles = SharePointDataHelpers.ListDataHelper.GetItemTitles(entries);
+                                        GridView3.DataSource = itemTitles;
+                                        GridView3.DataBind();
+                                        operationCount--;
+                                        break;
+                                }
+                            };
+                            break;
+
+                        case ODataBatchReaderState.ChangesetStart:
+                            // Optionally, handle the start of a change set.
+                            break;
+
+                        case ODataBatchReaderState.ChangesetEnd:
+                            // When this sample was created, SharePoint did not support "all or nothing" transactions. 
+                            // If that changes in the future this is where you would commit the transaction.
+                            break;
+
+                        case ODataBatchReaderState.Exception:
+                            // In a producition app handle exeception. Omitted for simplicity in this sample app.
+                            break;
+                    }
+                }
+            }
+            GridView3.Visible = true;
+            TwoLists.Visible = false;
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            string accessToken = ((Button)sender).CommandArgument;
+            Int16 operationCount = 0;
+
+            if (IsPostBack)
+            {
+                // Get the host web's URL.
+                sharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+            }
+
+            // Create the parent request
+            var batchRequest = new BatchODataRequest(sharepointUrl + "_api/"); // ctor adds "$batch"
+            batchRequest.SetHeader("Authorization", "Bearer " + accessToken);
+
+            using (var oDataMessageWriter = new ODataMessageWriter(batchRequest))
+            {
+                var oDataBatchWriter = oDataMessageWriter.CreateODataBatchWriter();
+                oDataBatchWriter.WriteStartBatch();
+
+                oDataBatchWriter.WriteStartChangeset();
+
+                // Create the list deleting operation
+                var deleteOperation = oDataBatchWriter.CreateOperationRequestMessage(
+                    "DELETE", new Uri(sharepointUrl.ToString() + "_api/Web/lists/getbytitle(\'" +OldList.Text+ "\')"));
+                deleteOperation.SetHeader("If-Match", "\"1\"");
+
+                oDataBatchWriter.WriteEndChangeset();
+                operationCount++;
+
+                // Create the query operation
+                var queryOperationMessage3 = oDataBatchWriter.CreateOperationRequestMessage(
+                    "GET", new Uri(sharepointUrl.ToString() + "_api/Web/lists"));
+                operationCount++;
+
+                oDataBatchWriter.WriteEndBatch();
+                oDataBatchWriter.Flush();
+            }
+
+            // Parse the response and bind the data to the UI controls
+            var oDataResponse = batchRequest.GetResponse();
+
+            using (var oDataReader = new ODataMessageReader(oDataResponse))
+            {
+                var oDataBatchReader = oDataReader.CreateODataBatchReader();
+
+                while (oDataBatchReader.Read())
+                {
+                    switch (oDataBatchReader.State)
+                    {
+                        case ODataBatchReaderState.Initial:
+                            // Optionally, handle the start of a batch payload.
+                            break;
+
+                        case ODataBatchReaderState.Operation:
+                            // Encountered an operation (either top-level or in a changeset)
+                            var operationResponse = oDataBatchReader.CreateOperationResponseMessage();
+
+                            // Response ATOM markup parsing and presentation
+                            using (var stream = operationResponse.GetStream())
+                            {
+
+                                switch (operationCount)
+                                {
+                                    case 2: // The "delete list" operation
+
+                                        if (operationResponse.StatusCode == 200)
+                                        {
+                                            DeleteListResponse.Text = "Your list was deleted!";
+                                        }
+                                        else
+                                        {
+                                            DeleteListResponse.Text = "Your list was not deleted. Status returned: " + operationResponse.StatusCode.ToString();
+                                        }
+
+                                        operationCount--;
+                                        break;
+
+                                    case 1: // The "List of Lists" operation
+
+                                        // Bind data to the grid on the page.
+                                           // In a production app, check operationResponse.StatusCode and handle non-200 statuses.
+                                           // For simplicity, this sample assumes status 200 (the list items are returned).
+                                        List<XElement> entries = SharePointDataHelpers.ListDataHelper.ExtractListItemsFromATOMResponse(stream);
+                                        var itemTitles = SharePointDataHelpers.ListDataHelper.GetItemTitles(entries);
+                                        GridView4.DataSource = itemTitles;
+                                        GridView4.DataBind();
+                                        operationCount--;
+                                        break;
+                                }
+                            };
+                            break;
+
+                        case ODataBatchReaderState.ChangesetStart:
+                            // Optionally, handle the start of a change set.
+                            break;
+
+                        case ODataBatchReaderState.ChangesetEnd:
+                            // When this sample was created, SharePoint did not support "all or nothing" transactions. 
+                            // If that changes in the future this is where you would commit the transaction.
+                            break;
+
+                        case ODataBatchReaderState.Exception:
+                            // In a producition app handle exeception. Omitted for simplicity in this sample app.
+                            break;
+                    }
+                }
+            }
+
+            GridView3.Visible = false;
+        }
+    }
+}
+
