e8903016ebc86caad34e70d4591789b04f39a180
Vesa Juvonen <vesa.juvonen@hotmail.com>
2015-02-06 15:02:18 +0200

    Hybrid provisioning

---
+++ Samples/Provisioning.Hybrid.Simple/Provisioning.Hybrid.Simple.Common/ServiceBusMessageManager.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provisioning.Hybrid.Simple.Common
+{
+    public class ServiceBusMessageManager
+    {
+        /// <summary>
+        /// Simple operation call included in the Interface
+        /// </summary>
+        /// <param name="message">Message to be passed to the other side</param>
+        /// <param name="serviceBusNamespace">Service bus namespace</param>
+        /// <param name="serviceBusSecret">Service bus secret</param>
+        /// <returns></returns>
+        public string SendMessage(
+                    string message, string serviceBusNamespace, string serviceBusSecret)
+        {
+            var cf = new ChannelFactory<ISiteRequest>(
+                        new NetTcpRelayBinding(),
+                        new EndpointAddress(ServiceBusEnvironment.CreateServiceUri("sb", serviceBusNamespace, "solver")));
+
+            cf.Endpoint.Behaviors.Add(
+                new TransportClientEndpointBehavior { 
+                    TokenProvider = TokenProvider.CreateSharedSecretTokenProvider("owner", serviceBusSecret) });
+
+            // Open channel and call the method
+            var ch = cf.CreateChannel();
+            string returnValue = ch.SendMessage(message);
+            cf.Close();
+
+            // return the updated message
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Operation to create new site collections in the service side
+        /// </summary>
+        /// <param name="siteTitle"></param>
+        /// <param name="siteTemplate"></param>
+        /// <param name="ownerIdentifier"></param>
+        /// <param name="serviceBusNameSpace"></param>
+        /// <param name="serviceBusSecret"></param>
+        public string SendSiteRequestMessage(SiteCollectionRequest request, string serviceBusNamespace, string serviceBusSecret)
+        {
+            var cf = new ChannelFactory<ISiteRequest>(
+                        new NetTcpRelayBinding(),
+                        new EndpointAddress(ServiceBusEnvironment.CreateServiceUri("sb", serviceBusNamespace, "solver")));
+
+            cf.Endpoint.Behaviors.Add(
+                new TransportClientEndpointBehavior { 
+                    TokenProvider = TokenProvider.CreateSharedSecretTokenProvider("owner", serviceBusSecret) });
+
+            // Open channel and call the method
+            var ch = cf.CreateChannel();
+            string createdUrl = ch.ProvisionSiteCollection(request);
+            cf.Close();
+
+            return createdUrl;
+        }
+    }
+}
---
+++ Samples/Provisioning.Hybrid.Simple/Provisioning.Hybrid.Simple.Common/SiteCollectionRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Provisioning.Hybrid.Simple.Common
+{
+    /// <summary>
+    /// Data object for the site collection creation details.
+    /// </summary>
+     [DataContract]
+    public class SiteCollectionRequest
+    {
+        /// <summary>
+        ///  Title for the new site being requested.
+        /// </summary> 
+         [DataMember]
+        public string Title { get; set; }
+
+        /// <summary>
+        ///  Which template we should use.
+        /// </summary>
+        [DataMember]
+        public string Template { get; set; }
+
+        /// <summary>
+        /// Which environment site collection should be created.
+        /// </summary>
+        [DataMember]
+        public string TargetEnvironment { get; set; }
+
+        /// <summary>
+        /// Identifier for the site collection owner.
+        /// </summary>
+        [DataMember]
+        public string OwnerIdentifier { get; set; }
+    }
+}
