0e1551a88206b32566b097399bea65ddb432c335
jansenbe <bjansen@microsoft.com>
2015-02-10 14:40:10 +0100

    add copied files to git

---
+++ Solutions/OfficeDevPnP.FrameWork.TimerJob/OfficeDevPnP.Framework.TimerJob.Samples/Jobs/SimpleJob.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Framework.TimerJob.Utilities;
+
+namespace OfficeDevPnP.Framework.TimerJob.Samples.Jobs
+{
+    public class SimpleJob: TimerJob
+    {
+        public SimpleJob() : base("SimpleJob")
+        {
+            TimerJobRun += SimpleJob_TimerJobRun;
+        }
+
+        void SimpleJob_TimerJobRun(object sender, TimerJobRunEventArgs e)
+        {
+            e.webClientContext.Load(e.webClientContext.Web, p => p.Title);
+            e.webClientContext.ExecuteQueryRetry();
+            Console.WriteLine("Site {0} has title {1}", e.url, e.webClientContext.Web.Title);
+        }
+    }
+}
