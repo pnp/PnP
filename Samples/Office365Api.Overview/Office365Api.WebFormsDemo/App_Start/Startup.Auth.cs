536f9196c463d4577cc9db7a77edbde52a1af82e
Paolo Pialorsi <paolo@pialorsi.com>
2015-02-27 18:10:36 +0100

    Skipped /Account/* issues and pages. The focus are Office365 API.

--- Samples/Office365Api.Overview/Office365Api.WebFormsDemo/App_Start/Startup.Auth.cs
+++ Samples/Office365Api.Overview/Office365Api.WebFormsDemo/App_Start/Startup.Auth.cs
@@ -24,6 +24,11 @@ namespace Office365Api.WebFormsDemo
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301883
         public void ConfigureAuth(IAppBuilder app)
         {
+            // Configure the db context, user manager and signin manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
