8276fbcf7c76682958071e723090266fe24053cc
jansenbe <bjansen@microsoft.com>
2015-02-27 19:51:33 +0100

    merge of pr458

--- OfficeDevPnP.Core/OfficeDevPnP.Core/AppModelExtensions/SearchExtensions.cs
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/AppModelExtensions/SearchExtensions.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace Microsoft.SharePoint.Client
 {
     /// <summary>
-    /// Class for Search utility methods
+    /// Class for Search extension methods
     /// </summary>
     public static partial class SearchExtensions
     {
@@ -20,6 +20,11 @@ namespace Microsoft.SharePoint.Client
         /// </param>
         public static void ExportSearchSettings(this ClientContext context, string exportFilePath, SearchObjectLevel searchSettingsExportLevel)
         {
+            if (string.IsNullOrEmpty(exportFilePath))
+            {
+                throw new ArgumentNullException("exportFilePath");
+            }
+
             SearchConfigurationPortability sconfig = new SearchConfigurationPortability(context);
             SearchObjectOwner owner = new SearchObjectOwner(context, searchSettingsExportLevel);
 
@@ -37,81 +42,82 @@ namespace Microsoft.SharePoint.Client
             }
         }
 
-#if !CLIENTSDKV15
-
         /// <summary>
         /// Imports search settings from file.
         /// </summary>
         /// <param name="context">Context for SharePoint objects and operations</param>
-        /// <param name="searchSchemaXMLPath">Search schema xml file path</param>
+        /// <param name="searchSchemaImportFilePath">Search schema xml file path</param>
         /// <param name="searchSettingsImportLevel">Search settings import level
         /// Reference: http://msdn.microsoft.com/en-us/library/microsoft.sharepoint.client.search.administration.searchobjectlevel(v=office.15).aspx
         /// </param>
-        public static void ImportSearchSettings(this ClientContext context, string searchSchemaXMLPath, SearchObjectLevel searchSettingsImportLevel)
+        public static void ImportSearchSettings(this ClientContext context, string searchSchemaImportFilePath, SearchObjectLevel searchSettingsImportLevel)
         {
-            SearchConfigurationPortability searchConfig = new SearchConfigurationPortability(context);
-            SearchObjectOwner owner = new SearchObjectOwner(context, searchSettingsImportLevel);
+            if (string.IsNullOrEmpty(searchSchemaImportFilePath))
+            {
+                throw new ArgumentNullException("searchSchemaImportFilePath");
+            }
 
-            // Import search configuration
-            searchConfig.ImportSearchConfiguration(owner, System.IO.File.ReadAllText(searchSchemaXMLPath));
-            context.Load(searchConfig);
-            context.ExecuteQueryRetry();
-        }
-
-#else
-
-        /// <summary>
-        /// Imports search settings from file.
-        /// </summary>
-        /// <param name="context">Context for SharePoint objects and operations</param>
-        /// <param name="searchSchemaXMLPath">Search schema xml file path</param>
-        /// <param name="searchSettingsImportLevel">Search settings import level
-        /// Reference: http://msdn.microsoft.com/en-us/library/microsoft.sharepoint.client.search.administration.searchobjectlevel(v=office.15).aspx
-        /// </param>
-        public static void ImportSearchSettings(this ClientContext context, string searchSchemaXMLPath, SearchObjectLevel searchSettingsImportLevel)
-        {
+#if CLIENTSDKV15
             if (searchSettingsImportLevel == SearchObjectLevel.Ssa)
             {
                 // Reference: https://msdn.microsoft.com/en-us/library/microsoft.sharepoint.client.search.portability.searchconfigurationportability_members.aspx
                 throw new Exception("You cannot import customized search configuration settings to a Search service application (SSA).");
             }
-
+#endif
             SearchConfigurationPortability searchConfig = new SearchConfigurationPortability(context);
             SearchObjectOwner owner = new SearchObjectOwner(context, searchSettingsImportLevel);
 
             // Import search configuration
-            searchConfig.ImportSearchConfiguration(owner, System.IO.File.ReadAllText(searchSchemaXMLPath));
+            searchConfig.ImportSearchConfiguration(owner, System.IO.File.ReadAllText(searchSchemaImportFilePath));
             context.Load(searchConfig);
             context.ExecuteQueryRetry();
         }
 
         /// <summary>
-        /// Sets the search center url on web
+        /// Sets the search center url on site collection (Site Settings -> Site collection administration --> Search Settings)
         /// </summary>
         /// <param name="web">SharePoint site - root web</param>
         /// <param name="searchCenterUrl">Search center url</param>
-        public static void SetSearchCenterUrl(this Web web, string searchCenterUrl)
+        public static void SetSiteCollectionSearchCenterUrl(this Web web, string searchCenterUrl)
         {
-            // Currently there is no direct API available to set the search center url on web.
-            // Set search setting at web level          
-            web.SetPropertyBagValue("SRCH_SB_SET_SITE", "{'Inherit':false,'ResultsPageAddress':null,'ShowNavigation':true}");
+            if (searchCenterUrl == null)
+            {
+                throw new ArgumentNullException("searchCenterUrl");
+            }
 
-            // Set search center url
-            web.SetPropertyBagValue("SRCH_ENH_FTR_URL_SITE", searchCenterUrl);
+            // Currently there is no direct API available to set the search center url on web.
+            // Set search setting at web level   
+       
+            // if another value was set then respect that
+            if (String.IsNullOrEmpty(web.GetPropertyBagValueString("SRCH_SB_SET_SITE", string.Empty)))
+            {
+                web.SetPropertyBagValue("SRCH_SB_SET_SITE", "{'Inherit':false,'ResultsPageAddress':null,'ShowNavigation':true}");
+            }
+
+            if (!string.IsNullOrEmpty(searchCenterUrl))
+            {
+                // Set search center url
+                web.SetPropertyBagValue("SRCH_ENH_FTR_URL_SITE", searchCenterUrl);
+            }
+            else
+            {
+                // When search center url is blank remove the property (like the SharePoint UI does)
+                web.RemovePropertyBagValue("SRCH_ENH_FTR_URL_SITE");
+            }
         }
 
         /// <summary>
-        /// Get the search center url for web
+        /// Get the search center url for the site collection (Site Settings -> Site collection administration --> Search Settings)
         /// </summary>
         /// <param name="web">SharePoint site - root web</param>
         /// <returns>Search center url for web</returns>
-        public static string GetSearchCenterUrl(this Web web)
+        public static string GetSiteCollectionSearchCenterUrl(this Web web)
         {
             // Currently there is no direct API available to get the search center url on web.
             // Get search center url
             return web.GetPropertyBagValueString("SRCH_ENH_FTR_URL_SITE", string.Empty);
         }
 
-#endif
+
     }
 }
