3dea89ba4e81074b1b9bd7c2c1430f0c1e96a3a9
Vesa Juvonen <vesa.juvonen@hotmail.com>
2015-03-04 15:49:45 +0200

    Adjusted sample for better resuse and fixed small bugs

--- Samples/Provisioning.Cloud.Async.WebJob/Provision.Cloud.Async.WebJob.Console.Message/Program.cs
+++ Samples/Provisioning.Cloud.Async.WebJob/Provision.Cloud.Async.WebJob.Console.Message/Program.cs
@@ -2,6 +2,7 @@
 using Provisioning.Cloud.Async.WebJob.Common;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,14 +17,20 @@ namespace Provision.Cloud.Async.WebJob.Console.Message
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // Update  parameters accordingly based on our environment from app.config
+            // Update these accordingly for your environment
+            string tenantName = ConfigurationManager.AppSettings["TenantName"];
+            string ownwerEmail = ConfigurationManager.AppSettings["SiteColTestOwnerEmail"];
+
+            // Create provisioning message objects for the storage queue
             ProvisioningData data = new ProvisioningData();
-            data.TenantName = "vesaj";
+            data.TenantName = tenantName;
             // Add request data in
             data.RequestData = new SiteRequestData(){
-                 Title = "",
-                 Template = "",
+                 Title = "Sample from queue",
+                 Template = "STS#0",
                  Lcid = 1033,
-                 Owner = "", 
+                 Owner = ownwerEmail, 
                  StorageMaximumLevel = 100,
                  TimeZoneId = 16, 
                  Url = Guid.NewGuid().ToString().Replace("-", "")
