58192bd54b0520e8a424ad749f24790e2a2611ad
frankmarasco <fmarasco@microsoft.com>
2015-03-18 18:59:20 -0500

    Added Provisioning Template XML. Intial test  class to support the domain model for the provisioning template

---
+++ OfficeDevPnP.Core/OfficeDevPnP.Core.Tests/ProvisioningTemplates/DomainModelTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeDevPnP.Core.Framework.Provisioning;
+using System.Xml.Linq;
+using OfficeDevPnP.Core.Utilities;
+using System.IO;
+
+namespace OfficeDevPnP.Core.Tests.ProvisioningTemplates
+{
+    [TestClass]
+    public class DomainModelTests
+    {
+        [TestMethod]
+        [TestCategory("Provisioning Template Domain Model")]
+        public void CanSerializeDomainObject()
+        {
+            ProvisioningTemplate _st;
+            var path = string.Format(@"{0}\Resources\Templates\{1}", AppDomain.CurrentDomain.BaseDirectory, "ProvisioningTemplate.xml");
+            XDocument _doc = XDocument.Load(path);
+            _st = XMLUtility.Deserialize<ProvisioningTemplate>(_doc);
+
+            Assert.IsNotNull(_st);
+
+        }
+    }
+}
