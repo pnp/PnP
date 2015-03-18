3f65bcd1284afa952e07d4e68740d3827828ac20
frankmarasco <fmarasco@microsoft.com>
2015-03-18 18:13:41 -0500

    intial structure for provisioning

---
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Field.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+
+namespace OfficeDevPnP.Core.Framework.Provisioning
+{
+    /// <summary>
+    /// Represents a Field XML
+    /// </summary>
+    public class Field : IXmlSerializable
+    {
+        private string _schemaXml = string.Empty;
+
+        /// <summary>
+        /// Gets a value that specifies the XML Schema representing the Field type.
+        /// </summary>
+        public string SchemaXml
+        {
+            get { return this._schemaXml; }
+            set { this._schemaXml = value; }
+        }
+
+        /// <summary>
+        /// No Imp will return null
+        /// </summary>
+        /// <returns></returns>
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            if (reader.MoveToContent() == XmlNodeType.Element && reader.LocalName == "Field")
+            {
+                this._schemaXml = reader.ReadOuterXml();
+            }
+            //    reader.MoveToContent();
+
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            if (string.IsNullOrEmpty(this._schemaXml))
+            {
+                return;
+            }
+            XElement _fieldXML = XElement.Parse(this._schemaXml);
+            foreach (var attrib in _fieldXML.Attributes())
+            {
+                writer.WriteAttributeString(attrib.Name.ToString(), attrib.Value);
+            }
+        }
+    }
+}
---
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/ProvisioningTemplate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning
+{
+    [XmlRoot(ElementName = "SharePointProvisioningTemplate")]
+    [Serializable]
+    public class ProvisioningTemplate
+    {
+        #region private members
+        private List<Field> _siteFields = new List<Field>();
+        #endregion
+
+        #region Properties
+        [XmlAttribute]
+        public string ID { get; set; }
+
+        [XmlIgnore]
+        public int? Version { get; set; }
+
+        [XmlAttribute("Version")]
+        public object VersionValue
+        {
+            get { return this.Version; }
+            set
+            {
+                if (value == null)
+                {
+                    this.Version = null;
+                }
+                else if (value is int || value is int?)
+                {
+                    this.Version = (int)value;
+                }
+                else
+                {
+                    this.Version = int.Parse(value.ToString());
+                }
+            }
+        }
+        [XmlElement]
+        public string DefaultSitePolicy { get; set; }
+
+        /// <summary>
+        /// Fields to Provision
+        /// </summary>
+        [XmlArray(ElementName = "SiteFields")]
+        [XmlArrayItem("Field", typeof(Field))]
+        public List<Field> SiteFields
+        {
+            get { return this._siteFields; }
+            set { this._siteFields = value; }
+        }
+        #endregion
+    }
+}
