1664072a446fc249275ee60d94768ae0408cac7c
frankmarasco <fmarasco@microsoft.com>
2015-03-20 17:29:20 -0500

    Extensibiity structure for Provider Call out for the provisioning component

---
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Extensibility/ExtensibilityManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.Extensibility
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ExtensibilityManager
+    {
+        public void ExecuteCallout();
+    }
+}
