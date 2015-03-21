c17202eceda824113ca2472b40a9cb327950f180
Bert Jansen <bjansen@microsoft.com>
2015-03-21 11:48:33 +0100

    Merge of pr555. -) Renamed to ECM.DocumentSets -) Uses low trust

---
+++ Samples/ECM.DocumentSets/ECM.DocumentSetsWeb/Models/AddAllowedContentTypeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECM.DocumentSetsWeb.Models
+{
+    public class AddAllowedContentTypeViewModel
+    {
+        //public SelectList ContentTypeList { get; set; }
+        public string SelectedStringId { get; set; }
+
+        public AddAllowedContentTypeViewModel(string selectedStringId)
+        {
+            SelectedStringId = selectedStringId;
+        }
+        public AddAllowedContentTypeViewModel()
+        {
+        }
+    }
+}
