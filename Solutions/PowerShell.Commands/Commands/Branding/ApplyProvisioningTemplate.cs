97cebf3c2e758941c064000dc75744d6875189d2
Erwin van Hunen <erwin.van.hunen@outlook.com>
2015-03-25 23:03:11 +0100

    Added provisioning cmdlets

---
+++ Solutions/PowerShell.Commands/Commands/Branding/ApplyProvisioningTemplate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.PowerShell.CmdletHelpAttributes;
+using OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml;
+using OfficeDevPnP.Core.Utilities;
+using System.Xml.Linq;
+
+namespace OfficeDevPnP.PowerShell.Commands.Branding
+{
+    [Cmdlet("Apply", "SPOProvisioningTemplate")]
+    [CmdletHelp("Generates a provisioning template from a web", Category = "Branding")]
+    public class ApplyProvisioningTemplate : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string Path;
+
+        
+        protected override void ExecuteCmdlet()
+        {
+            XDocument doc = XDocument.Load(Path);
+
+            ProvisioningTemplate provisioningTemplate = XMLSerializer.Deserialize<SharePointProvisioningTemplate>(doc).ToProvisioningTemplate();
+
+            if (provisioningTemplate != null)
+            {
+                SelectedWeb.ApplyProvisioningTemplate(provisioningTemplate);
+            }
+        }
+    }
+}
