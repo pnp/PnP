0065670e3fb39d6f947e8dc8c17860c698354aad
Erwin van Hunen <erwin.van.hunen@outlook.com>
2015-03-28 14:13:54 +0100

    Added ~keywordstermstoreid token support

---
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/SiteCollectionTermStoreId - Copy.cs
@@ -0,0 +1,26 @@
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.Taxonomy;
+
+namespace OfficeDevPnP.Core.Framework.ObjectHandlers.TokenDefinitions
+{
+    public class KeywordsTermStoreIdToken : TokenDefinition
+    {
+        public KeywordsTermStoreIdToken(Web web)
+            : base(web, "~keywordstermstoreidtoken", "{keywordstermstoreidtoken}")
+        {
+        }
+
+        public override string GetReplaceValue()
+        {
+            if (CacheValue == null)
+            {
+                TaxonomySession session = TaxonomySession.GetTaxonomySession(Web.Context);
+                var termStore = session.GetDefaultKeywordsTermStore();
+                Web.Context.Load(termStore, t => t.Id);
+                Web.Context.ExecuteQueryRetry();
+                CacheValue = termStore.Id.ToString();
+            }
+            return CacheValue;
+        }
+    }
+}
