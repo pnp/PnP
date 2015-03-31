f5c5bef27e488683827840336a8188189430d0ec
Bert Jansen <bjansen@microsoft.com>
2015-03-31 16:11:41 +0200

    Implemented CreateEntities for the ComposedLook object

--- OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectComposedLook.cs
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectComposedLook.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.SharePoint.Client;
 using OfficeDevPnP.Core.Framework.ObjectHandlers;
 using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using System;
 
 namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
 {
-    class ObjectComposedLook : ObjectHandlerBase
+    public class ObjectComposedLook : ObjectHandlerBase
     {
         public override void ProvisionObjects(Web web, ProvisioningTemplate template)
         {
@@ -57,39 +58,120 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
         public override ProvisioningTemplate CreateEntities(Web web, ProvisioningTemplate template, ProvisioningTemplate baseTemplate)
         {
             // Load object if not there
+            bool executeQueryNeeded = false;
             if (!web.IsObjectPropertyInstantiated("AlternateCssUrl"))
             {
                 web.Context.Load(web);
+                executeQueryNeeded = true;
+            }
+            if (!web.IsObjectPropertyInstantiated("Url"))
+            {
+                web.Context.Load(web);
+                executeQueryNeeded = true;
+            }
+
+            if (executeQueryNeeded)
+            {
                 web.Context.ExecuteQuery();
             }
 
-            // TODO: review
-            template.ComposedLook = null;
+            // Information coming from the site
+            template.ComposedLook.AlternateCSS = Tokenize(web.AlternateCssUrl, web.Url);
+            template.ComposedLook.MasterPage = Tokenize(web.MasterUrl, web.Url);
+            template.ComposedLook.SiteLogo = Tokenize(web.SiteLogoUrl, web.Url);
 
-            //var theme = web.GetComposedLook("Current");
+            var theme = web.GetCurrentComposedLook();
 
-            //// Get needed data from the site
-            //// TODO: Access currently set theme for details
-            //template.ComposedLook.AlternateCSS = web.AlternateCssUrl;
-            //template.ComposedLook.BackgroundFile = theme.BackgroundImage;
-            //template.ComposedLook.ColorFile = theme.Theme;
-            //template.ComposedLook.FontFile = theme.Font;
-            //template.ComposedLook.MasterPage = web.MasterUrl;
-            //template.ComposedLook.Name = "";
-            //template.ComposedLook.SiteLogo = web.SiteLogoUrl;
-
-            // If a base template is specified then use that one to "cleanup" the generated template model
-            if (baseTemplate != null)
+            if (theme != null)
             {
-                template = CleanupEntities(template, baseTemplate);
+                template.ComposedLook.Name = theme.Name;
+
+                if (theme.IsCustomComposedLook)
+                {
+                    template.ComposedLook.BackgroundFile = Tokenize(theme.BackgroundImage, web.Url);
+                    template.ComposedLook.ColorFile = Tokenize(theme.Theme, web.Url);
+                    template.ComposedLook.FontFile = Tokenize(theme.Font, web.Url);
+
+                    // Create file entries for the custom theme files  
+                    if (template.ComposedLook.BackgroundFile != null)
+                    {
+                        template.Files.Add(GetComposedLookFile(template.ComposedLook.BackgroundFile));
+                    }
+                    if (template.ComposedLook.ColorFile != null)
+                    {
+                        template.Files.Add(GetComposedLookFile(template.ComposedLook.ColorFile));
+                    }
+                    if (template.ComposedLook.FontFile != null)
+                    {
+                        template.Files.Add(GetComposedLookFile(template.ComposedLook.FontFile));
+                    }
+                    if (template.ComposedLook.SiteLogo != null)
+                    {
+                        template.Files.Add(GetComposedLookFile(template.ComposedLook.SiteLogo));
+                    }
+
+                    // If a base template is specified then use that one to "cleanup" the generated template model
+                    if (baseTemplate != null)
+                    {
+                        template = CleanupEntities(template, baseTemplate);
+                    }
+                }
+            }
+            else
+            {
+                template.ComposedLook = null;
             }
 
             return template;
         }
 
+        private Model.File GetComposedLookFile(string asset)
+        {
+            int index = asset.LastIndexOf("/");
+            Model.File file = new Model.File();
+            file.Src = asset.Substring(index + 1);
+            file.Folder = asset.Substring(0, index);
+            file.Overwrite = true;
+
+            return file;
+        }
+
+        private string Tokenize(string url, string webUrl)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return url;
+            }
+            else
+            {
+                if (url.IndexOf("/_catalogs/theme", StringComparison.InvariantCultureIgnoreCase) > -1)
+                {
+                    return url.Substring(url.IndexOf("/_catalogs/theme", StringComparison.InvariantCultureIgnoreCase)).Replace("/_catalogs/theme", "~themecatalog");
+                }
+                if (url.IndexOf("/_catalogs/masterpage", StringComparison.InvariantCultureIgnoreCase) > -1)
+                {
+                    return url.Substring(url.IndexOf("/_catalogs/masterpage", StringComparison.InvariantCultureIgnoreCase)).Replace("/_catalogs/masterpage", "~masterpagecatalog");
+                }
+                if (url.IndexOf(webUrl, StringComparison.InvariantCultureIgnoreCase) > -1)
+                {
+                    return url.Replace(webUrl, "~site");
+                }
+                else
+                {
+                    Uri r = new Uri(webUrl);
+                    if (url.IndexOf(r.PathAndQuery, StringComparison.InvariantCultureIgnoreCase) > -1)
+                    {
+                        return url.Replace(r.PathAndQuery, "~site");
+                    }
+                }
+
+                // nothing to tokenize...
+                return url;
+            }
+        }
+
         private ProvisioningTemplate CleanupEntities(ProvisioningTemplate template, ProvisioningTemplate baseTemplate)
         {
-
             return template;
         }
     }
