c86429ee1e9f3e81e987af0edd71f1b0599d3ead
Paolo Pialorsi <paolo@pialorsi.com>
2015-04-02 11:18:52 +0200

    Updated code to support ID, Version and SitePolicy in provisioning template export.

--- OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Model/ProvisioningTemplateInfo.cs
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Model/ProvisioningTemplateInfo.cs
@@ -5,6 +5,8 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.Model
     public class ProvisioningTemplateInfo
     {
         public string TemplateID { get; set; }
+        public Double TemplateVersion { get; set; }
+        public string TemplateSitePolicy { get; set; }
         public DateTime ProvisioningTime { get; set; }
         public bool Result { get; set; }
     }
