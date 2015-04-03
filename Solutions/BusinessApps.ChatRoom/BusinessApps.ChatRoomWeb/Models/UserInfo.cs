f84dabb036dedf54c4a3dd04d7cd9c4ca718c2e1
Jonathan Huss <jonathan@jonathanhuss.com>
2015-04-03 11:05:45 -0500

    BusinessApps.ChatRoom - Initial Release

---
+++ Solutions/BusinessApps.ChatRoom/BusinessApps.ChatRoomWeb/Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessApps.ChatRoomWeb.Models
+{
+    public class UserInfo
+    {
+        public string DisplayName { get; set; }
+        public string PhotoUrl { get; set; }
+        public DateTime LastPing { get; set; }
+    }
+}
