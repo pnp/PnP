21346f769343c7418d7dbfbc12c6a660c346e327
Patrik Björklund <p.bjorklund@gmail.com>
2015-04-14 17:46:32 +0200

    Extract interface and generic base model class

--- OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Model/PropertyBagEntry.cs
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Model/PropertyBagEntry.cs
@@ -2,40 +2,67 @@
 
 namespace OfficeDevPnP.Core.Framework.Provisioning.Model
 {
-    public class PropertyBagEntry : IEquatable<PropertyBagEntry>
+    public class PropertyBagEntry : ModelBase<PropertyBagEntry>, IModel
     {
         #region Properties
-
-        public string Key { get; set; }
-            
-        public string Value { get; set; }
 
         #endregion
 
         #region Comparison code
 
+        #endregion
+    }
+
+    public class ModelBase<T> : IModel, IEquatable<ModelBase<T>>
+    {
+        Func<int> hashFormatter;
+        public ModelBase(Func<int> hashFormatter)
+        {
+            this.hashFormatter = hashFormatter;
+        }
+
+        //Default hashformatter. Other classes can just override their GetHashCode or inject a Func 
+        public ModelBase()
+        {
+            this.hashFormatter = () =>
+            {
+                return (String.Format("{0}|{1}",
+                    this.Key,
+                    this.Value).GetHashCode());
+            };
+        }
         public override int GetHashCode()
         {
-            return (String.Format("{0}|{1}",
-                this.Key,
-                this.Value).GetHashCode());
+            return hashFormatter();
         }
-
-        public override bool Equals(object obj)
-        {
-            if (!(obj is PropertyBagEntry))
-            {
-                return(false);
-            }
-            return (Equals((PropertyBagEntry)obj));
-        }
-
-        public bool Equals(PropertyBagEntry other)
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public bool Equals(IModel other)
         {
             return (this.Key == other.Key &&
                 this.Value == other.Value);
         }
 
-        #endregion
+
+        public bool Equals(ModelBase<T> other)
+        {
+            return (this.Key == other.Key && this.Value == other.Value);
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is T))
+            {
+                return(false);
+            }
+            return (Equals((ModelBase<T>)obj));
+        }
+
     }
+
+    public interface IModel
+    {
+        string Key { get; set; }
+        string Value { get; set; }
+    }
+
 }
