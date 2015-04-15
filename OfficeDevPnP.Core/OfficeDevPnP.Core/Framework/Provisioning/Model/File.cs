7f6b78a1b081a6feef9e2a2ca01f88c3abe21be4
Erwin van Hunen <erwin.van.hunen@outlook.com>
2015-04-15 23:06:13 +0200

    Moved webpart page webpart provisioning to files

--- OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Model/File.cs
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Model/File.cs
@@ -1,16 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OfficeDevPnP.Core.Framework.Provisioning.Model
 {
     public class File : IEquatable<File>
     {
-        #region Properties
+        #region Private Members
+        private List<WebPart> _webParts = new List<WebPart>();
+        #endregion
 
+        #region Properties
         public string Src { get; set; }
-        
+
         public string Folder { get; set; }
 
         public bool Overwrite { get; set; }
+
+        public bool Create { get; set; }
+
+        public List<WebPart> WebParts
+        {
+            get { return _webParts; }
+            private set { _webParts = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+        public File() { }
+
+        public File(string src, string folder, bool overwrite, bool create, IEnumerable<WebPart> webParts)
+        {
+            this.Src = src;
+            this.Overwrite = overwrite;
+            this.Folder = folder;
+            this.Create = create;
+            if (webParts != null)
+            {
+                this.WebParts.AddRange(webParts);
+            }
+        }
+
 
         #endregion
 
