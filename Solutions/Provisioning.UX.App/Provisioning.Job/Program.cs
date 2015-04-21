c06237afd1f4cd867d020ae55358a4ced598a29f
frankmarasco <fmarasco@microsoft.com>
2015-04-21 08:58:43 -0500

    Added Provisioning.Job

--- Solutions/Provisioning.UX.App/Provisioning.Job/Program.cs
+++ Solutions/Provisioning.UX.App/Provisioning.Job/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Provisioning.TimerJob
+namespace Provisioning.Job
 {
     class Program
     {
--- Solutions/Provisioning.UX.App/Provisioning.Job/Properties/AssemblyInfo.cs
+++ Solutions/Provisioning.UX.App/Provisioning.Job/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Provisioning.TimerJob")]
+[assembly: AssemblyTitle("Provisioning.Job")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Provisioning.TimerJob")]
+[assembly: AssemblyProduct("Provisioning.Job")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a2d7fafe-d526-425a-8ab0-9334d03d9744")]
+[assembly: Guid("3f846749-f49e-4d68-841f-e0efac06f8be")]
 
 // Version information for an assembly consists of the following four values:
 //
