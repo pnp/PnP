15c714c38c38f5d2c8c56816fb537695c74da9eb
MartinHatch <martin.hatch@hatchsolutions.co.uk>
2015-04-22 10:59:19 +0100

    Missing Files

---
+++ Samples/Branding.JSLink/Branding.JSLink.NCSS/Style Library/OfficeDevPnP/Branding.JSLink/Generics/Colours.js
@@ -0,0 +1,85 @@
+﻿var jslinkTemplates = window.jslinkTemplates || {};
+
+jslinkTemplates.Colours = function () {
+
+    var selectIdPrefix = "COL_";
+    var errorIdPrefix = "ERR_";
+
+    var _display = function (ctx) {
+
+        var returnHtml = "";
+        returnHtml += "<div style='background: " + ctx.CurrentItem["Colour"] + "; width: 20px; float: left'>&nbsp;</div>";
+        returnHtml += "<div style='margin-left: 30px;'>" + ctx.CurrentItem["Colour"] + "</div>";
+
+        // return our string
+        return returnHtml;
+    };
+    var _edit = function (ctx) {
+
+        // create form context object
+        var formCtx = SPClientTemplates.Utility.GetFormContextForCurrentField(ctx);
+
+        // register our custom validator
+        var validator = new SPClientForms.ClientValidation.ValidatorSet();
+        validator.RegisterValidator(new jslinkTemplates.Colours.validator());
+        formCtx.registerClientValidator(formCtx.fieldName, validator);
+
+        // register our callbacks
+        formCtx.registerGetValueCallback(formCtx.fieldName, jslinkTemplates.Colours.getValue.bind(null, formCtx.fieldName));
+        formCtx.registerValidationErrorCallback(formCtx.fieldName, jslinkTemplates.Colours.onError.bind(null, formCtx.fieldName));
+        
+        var returnHtml = "";
+
+        // hard coding the colours
+        var colours = new Array("Red", "Green", "Blue");
+
+        returnHtml += "<select id='" + selectIdPrefix + formCtx.fieldName + "'>";
+        for (var i = 0; i < colours.length; i++) {
+            // create a drop-down option
+            returnHtml += "<option value='" + colours[i] + "'";
+
+            if (ctx.CurrentItem["Colour"] == colours[i]) {
+                // make sure the current field value is selected
+                returnHtml += " selected='true' ";
+
+            }
+            returnHtml += ">" + colours[i] + "</option>";
+        }
+        returnHtml += "</select>";
+
+        // add an error span for our validator to use
+        returnHtml += "<br/><span id='" + errorIdPrefix + formCtx.fieldName + "' class='ms-formvalidation ms-csrformvalidation'></span><br/>";
+
+        return returnHtml;
+    };
+
+    var _getValue = function (fieldName) {
+        var selector = '#' + selectIdPrefix + fieldName;
+        return $(selector).val();
+    };
+    var _validator = function () {
+        jslinkTemplates.Colours.validator.prototype.Validate = function (value) {
+            var isError = false;
+            var errorMessage = "";
+
+            if (value == "Blue") {
+                isError = true;
+                errorMessage = "Sorry, we don't like the colour blue";
+            }
+
+            return new SPClientForms.ClientValidation.ValidationResult(isError, errorMessage);
+        };
+    };
+    var _onError = function (fieldName, error) {
+        var selector = '#' + errorIdPrefix + fieldName;
+        $(selector).html("<span role='alert'>" + error.errorMessage + "</span>");
+    };
+
+    return {
+        "display": _display,
+        "edit": _edit,
+        "getValue": _getValue,
+        "validator": _validator,
+        "onError": _onError
+    }
+}();
---
+++ Samples/Branding.JSLink/Branding.JSLink.NCSS/Style Library/OfficeDevPnP/Branding.JSLink/Generics/ManagedMetadata.js
@@ -0,0 +1,121 @@
+﻿var jslinkTemplates = window.jslinkTemplates || {};
+jslinkTemplates.Taxonomy = function() {
+
+    function _editMode(ctx) {
+        // register our "get value callback" method
+        var formCtx = SPClientTemplates.Utility.GetFormContextForCurrentField(ctx);
+        formCtx.registerGetValueCallback(formCtx.fieldName, jslinkTemplates.Taxonomy.getValue.bind(null, formCtx.fieldName));
+
+        _loadField(formCtx.fieldName, ctx.CurrentFieldSchema.SspId, ctx.CurrentFieldSchema.TermSetId, ctx.CurrentFieldValue);
+
+        return "<select id='TAX_" + formCtx.fieldName + "' style='display:block; width:100%;'><option>Loading...</option></select>" + 
+            "<input type='hidden' id='TAXVALUE_" + formCtx.fieldName + "' />";
+    };
+
+    function _loadField(fieldName, sspId, termSetId, currentValue) {
+        var elementId = "#TAX_" + fieldName;
+
+        // wait for the page to render
+        jQuery(document).ready(function () {
+
+            var context = new SP.ClientContext.get_current();
+            var taxonomySession = SP.Taxonomy.TaxonomySession.getTaxonomySession(context);
+
+            var termStore = taxonomySession.get_termStores().getById(sspId);
+            var termSet = termStore.getTermSet(termSetId);
+
+            var terms = termSet.get_terms();
+            context.load(terms);
+            context.executeQueryAsync(
+                function () {
+                    jQuery(elementId).empty(); // clear out the existing options
+                    jQuery(elementId).append("<option></option>"); // and add a blank one
+
+                    var termEnumerator = terms.getEnumerator();
+                    while (termEnumerator.moveNext()) {
+                        var currentTerm = termEnumerator.get_current();
+                        jQuery(elementId).append("<option value='" + currentTerm.get_id() + "'>" + currentTerm.get_name() + "</option>")
+                    }
+
+                    jQuery(elementId + " option").click(jslinkTemplates.Taxonomy.selectTerm);
+
+
+                },
+                function (sender, args) {
+                    alert("Call failed. Error: " + args.get_message());
+                }
+            );
+        });
+    };
+    function _selectTerm(sender) {
+        var termId = jQuery(sender.target).val();
+        var termLabel = jQuery(sender.target).text();
+
+        if (termId == "") {
+            return;
+        }
+
+        // update the hidden input field with the currently selected value
+        // this should be in the value LABEL|GUID
+        jQuery(sender.target).parent().siblings("input").val(termLabel + "|" + termId);
+
+        // kill off any existing child-drop-downs (recursively)
+        // before we re-render them below
+        var selectElement = jQuery(sender.target).parent()[0];
+        removeChildDropDowns(selectElement);
+
+        var context = new SP.ClientContext.get_current();
+        var taxonomySession = SP.Taxonomy.TaxonomySession.getTaxonomySession(context);
+
+        var term = taxonomySession.getTerm(termId);
+        var childTerms = term.get_terms();
+
+        context.load(term);
+        context.load(childTerms);
+        context.executeQueryAsync(
+            function () {
+                // only execute if it has child terms
+                if (term.get_termsCount() > 0) {
+                    // start off with a select containing a blank option
+                    var html = "<select style='display:block; margin-top: 5px; width:100%;' id='TERM_" + termId + "'><option></option>";
+
+                    var termEnumerator = childTerms.getEnumerator();
+                    while (termEnumerator.moveNext()) {
+                        var currentTerm = termEnumerator.get_current();
+                        html += "<option value='" + currentTerm.get_id() + "'>" + currentTerm.get_name() + "</option>";
+                    }
+
+                    // add the new drop-down
+                    jQuery(sender.target).parent().after(html);
+
+                    // and add a click event
+                    jQuery("#TERM_" + termId + " option").click(jslinkTemplates.Taxonomy.selectTerm);
+                }
+            },
+            function (sender, args) {
+                alert("Call failed. Error: " + args.get_message());
+            }
+        );
+    };
+   
+    function removeChildDropDowns(selectElement) {
+
+        // the ~ means "any following siblings" and ignores any siblings
+        // further up the chain
+        jQuery("#" + selectElement.id + " ~ select").remove();
+    };
+
+    function _getValue(fieldName) {
+        // retrieve value from the relevant hidden input
+        var hiddenValue = jQuery("#TAXVALUE_" + fieldName).val();
+        
+        // return it
+        return hiddenValue;
+    };
+
+    return {
+        editMode: _editMode,
+        selectTerm: _selectTerm,
+        getValue: _getValue
+    }
+}();
