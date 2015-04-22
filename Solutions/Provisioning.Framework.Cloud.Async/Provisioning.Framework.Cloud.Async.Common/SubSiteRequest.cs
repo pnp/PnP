e263797365461f6ff5f65036029cf0a920de3b8e
Vesa Juvonen <vesa.juvonen@hotmail.com>
2015-04-22 16:38:34 +0300

    Async site collection provisioning with PnP engine. First check in, documenttion updates still needed.

---
+++ Solutions/Provisioning.Framework.Cloud.Async/Provisioning.Framework.Cloud.Async.Common/SubSiteRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provisioning.Framework.Cloud.Async.Common
+{
+    class SubSiteRequest
+    {
+        public string Url { get; set; }
+        public string Template { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public uint Lcid { get; set; }
+    }
+}
---
+++ Solutions/Provisioning.Framework.Cloud.Async/Provisioning.Framework.Cloud.Async.Job/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Online.SharePoint.TenantAdministration;
+
+namespace Provisioning.Framework.Cloud.Async.Job
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var host = new JobHost();
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+
+        }
+    }
+}
