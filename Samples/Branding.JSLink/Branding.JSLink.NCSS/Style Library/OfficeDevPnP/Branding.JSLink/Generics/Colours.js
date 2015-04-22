34b9613d68861aa70e9308ae1c5758f6a53a66ec
MartinHatch <martin.hatch@hatchsolutions.co.uk>
2015-04-22 14:22:36 +0100

    Updated Colours to add a full selection instead of the demo Red/Green/Blue
    Updated ManagedMetadata to properly handle editing mode (and loading the drop-downs for the current field value)

--- Samples/Branding.JSLink/Branding.JSLink.NCSS/Style Library/OfficeDevPnP/Branding.JSLink/Generics/Colours.js
+++ Samples/Branding.JSLink/Branding.JSLink.NCSS/Style Library/OfficeDevPnP/Branding.JSLink/Generics/Colours.js
@@ -7,12 +7,8 @@ jslinkTemplates.Colours = function () {
 
     var _display = function (ctx) {
 
-        var returnHtml = "";
-        returnHtml += "<div style='background: " + ctx.CurrentItem["Colour"] + "; width: 20px; float: left'>&nbsp;</div>";
-        returnHtml += "<div style='margin-left: 30px;'>" + ctx.CurrentItem["Colour"] + "</div>";
-
         // return our string
-        return returnHtml;
+        return _getColourSpan(ctx.CurrentItem["Colour"]);
     };
     var _edit = function (ctx) {
 
@@ -30,8 +26,149 @@ jslinkTemplates.Colours = function () {
         
         var returnHtml = "";
 
-        // hard coding the colours
-        var colours = new Array("Red", "Green", "Blue");
+        //#region array of available colours
+        var colours = new Array("AliceBlue",
+                                "AntiqueWhite",
+                                "Aqua",
+                                "Aquamarine",
+                                "Azure",
+                                "Beige",
+                                "Bisque",
+                                "Black",
+                                "BlanchedAlmond",
+                                "Blue",
+                                "BlueViolet",
+                                "Brown",
+                                "BurlyWood",
+                                "CadetBlue",
+                                "Chartreuse",
+                                "Chocolate",
+                                "Coral",
+                                "CornflowerBlue",
+                                "Cornsilk",
+                                "Crimson",
+                                "Cyan",
+                                "DarkBlue",
+                                "DarkCyan",
+                                "DarkGoldenRod",
+                                "DarkGray",
+                                "DarkGreen",
+                                "DarkKhaki",
+                                "DarkMagenta",
+                                "DarkOliveGreen",
+                                "DarkOrange",
+                                "DarkOrchid",
+                                "DarkRed",
+                                "DarkSalmon",
+                                "DarkSeaGreen",
+                                "DarkSlateBlue",
+                                "DarkSlateGray",
+                                "DarkTurquoise",
+                                "DarkViolet",
+                                "DeepPink",
+                                "DeepSkyBlue",
+                                "DimGray",
+                                "DodgerBlue",
+                                "FireBrick",
+                                "FloralWhite",
+                                "ForestGreen",
+                                "Fuchsia",
+                                "Gainsboro",
+                                "GhostWhite",
+                                "Gold",
+                                "GoldenRod",
+                                "Gray",
+                                "Green",
+                                "GreenYellow",
+                                "HoneyDew",
+                                "HotPink",
+                                "IndianRed",
+                                "Indigo",
+                                "Ivory",
+                                "Khaki",
+                                "Lavender",
+                                "LavenderBlush",
+                                "LawnGreen",
+                                "LemonChiffon",
+                                "LightBlue",
+                                "LightCoral",
+                                "LightCyan",
+                                "LightGoldenRodYellow",
+                                "LightGray",
+                                "LightGreen",
+                                "LightPink",
+                                "LightSalmon",
+                                "LightSeaGreen",
+                                "LightSkyBlue",
+                                "LightSlateGray",
+                                "LightSteelBlue",
+                                "LightYellow",
+                                "Lime",
+                                "LimeGreen",
+                                "Linen",
+                                "Magenta",
+                                "Maroon",
+                                "MediumAquaMarine",
+                                "MediumBlue",
+                                "MediumOrchid",
+                                "MediumPurple",
+                                "MediumSeaGreen",
+                                "MediumSlateBlue",
+                                "MediumSpringGreen",
+                                "MediumTurquoise",
+                                "MediumVioletRed",
+                                "MidnightBlue",
+                                "MintCream",
+                                "MistyRose",
+                                "Moccasin",
+                                "NavajoWhite",
+                                "Navy",
+                                "OldLace",
+                                "Olive",
+                                "OliveDrab",
+                                "Orange",
+                                "OrangeRed",
+                                "Orchid",
+                                "PaleGoldenRod",
+                                "PaleGreen",
+                                "PaleTurquoise",
+                                "PaleVioletRed",
+                                "PapayaWhip",
+                                "PeachPuff",
+                                "Peru",
+                                "Pink",
+                                "Plum",
+                                "PowderBlue",
+                                "Purple",
+                                "RebeccaPurple",
+                                "Red",
+                                "RosyBrown",
+                                "RoyalBlue",
+                                "SaddleBrown",
+                                "Salmon",
+                                "SandyBrown",
+                                "SeaGreen",
+                                "SeaShell",
+                                "Sienna",
+                                "Silver",
+                                "SkyBlue",
+                                "SlateBlue",
+                                "SlateGray",
+                                "Snow",
+                                "SpringGreen",
+                                "SteelBlue",
+                                "Tan",
+                                "Teal",
+                                "Thistle",
+                                "Tomato",
+                                "Turquoise",
+                                "Violet",
+                                "Wheat",
+                                "White",
+                                "WhiteSmoke",
+                                "Yellow",
+                                "YellowGreen");
+        //#endregion
 
         returnHtml += "<select id='" + selectIdPrefix + formCtx.fieldName + "'>";
         for (var i = 0; i < colours.length; i++) {
@@ -43,12 +180,20 @@ jslinkTemplates.Colours = function () {
                 returnHtml += " selected='true' ";
 
             }
-            returnHtml += ">" + colours[i] + "</option>";
+            returnHtml += ">" + _getColourSpan(colours[i]) + "</option>";
         }
         returnHtml += "</select>";
 
         // add an error span for our validator to use
         returnHtml += "<br/><span id='" + errorIdPrefix + formCtx.fieldName + "' class='ms-formvalidation ms-csrformvalidation'></span><br/>";
+
+        return returnHtml;
+    };
+
+    var _getColourSpan = function(colour) {
+        var returnHtml = "";
+        returnHtml += "<div style='background: " + colour + "; width: 20px; float: left'>&nbsp;</div>";
+        returnHtml += "<div style='margin-left: 30px;'>" + colour + "</div>";
 
         return returnHtml;
     };
--- Samples/Branding.JSLink/Branding.JSLink.NCSS/Style Library/OfficeDevPnP/Branding.JSLink/Generics/ManagedMetadata.js
+++ Samples/Branding.JSLink/Branding.JSLink.NCSS/Style Library/OfficeDevPnP/Branding.JSLink/Generics/ManagedMetadata.js
@@ -1,6 +1,9 @@
 ﻿var jslinkTemplates = window.jslinkTemplates || {};
 jslinkTemplates.Taxonomy = function() {
 
+
+    var currentlySelectedTerms;
+
     function _editMode(ctx) {
         // register our "get value callback" method
         var formCtx = SPClientTemplates.Utility.GetFormContextForCurrentField(ctx);
@@ -13,7 +16,10 @@ jslinkTemplates.Taxonomy = function() {
     };
 
     function _loadField(fieldName, sspId, termSetId, currentValue) {
-        var elementId = "#TAX_" + fieldName;
+        
+
+        // reset the current terms array
+        currentlySelectedTerms = [];
 
         // wait for the page to render
         jQuery(document).ready(function () {
@@ -24,29 +30,104 @@ jslinkTemplates.Taxonomy = function() {
             var termStore = taxonomySession.get_termStores().getById(sspId);
             var termSet = termStore.getTermSet(termSetId);
 
-            var terms = termSet.get_terms();
-            context.load(terms);
-            context.executeQueryAsync(
-                function () {
-                    jQuery(elementId).empty(); // clear out the existing options
-                    jQuery(elementId).append("<option></option>"); // and add a blank one
+            if (currentValue.length > 0) {
+                // pull the Id from the current value
+                var currentTermId = currentValue.split("|")[1];
+                var currentTerm = termSet.getTerm(currentTermId);
 
-                    var termEnumerator = terms.getEnumerator();
-                    while (termEnumerator.moveNext()) {
-                        var currentTerm = termEnumerator.get_current();
-                        jQuery(elementId).append("<option value='" + currentTerm.get_id() + "'>" + currentTerm.get_name() + "</option>")
-                    }
+                context.load(currentTerm);
+                context.executeQueryAsync(
+                    function () {
 
-                    jQuery(elementId + " option").click(jslinkTemplates.Taxonomy.selectTerm);
+                        // retrieve the full path .. this will include all of the parent terms too 
+                        var fullPath = currentTerm.get_pathOfTerm();
 
-
-                },
-                function (sender, args) {
-                    alert("Call failed. Error: " + args.get_message());
-                }
-            );
+                        // they are semi-colon delimited, so if we perform a split we end up
+                        // with an array containing all of the currently selected values
+                        currentlySelectedTerms = fullPath.split(";");
+                        
+                        // now we can proceed with rendering...
+                        _renderDropDowns(fieldName, sspId, termSetId);
+                    },
+                    function () {
+                        alert("Call failed. Error: " + args.get_message());
+                    });
+            }
+            else {
+                // there is no current value, so just go
+                _renderDropDowns(fieldName, sspId, termSetId);
+            }
         });
     };
+
+    function _renderDropDowns(fieldName, sspId, termSetId) {
+        var elementId = "#TAX_" + fieldName;
+
+        var context = new SP.ClientContext.get_current();
+        var taxonomySession = SP.Taxonomy.TaxonomySession.getTaxonomySession(context);
+
+        var termStore = taxonomySession.get_termStores().getById(sspId);
+        var termSet = termStore.getTermSet(termSetId);
+
+        var terms = termSet.get_terms();
+        context.load(terms);
+        context.executeQueryAsync(
+            function () {
+                jQuery(elementId).empty(); // clear out the existing options
+
+                var html = "<option></option>";
+
+                var termEnumerator = terms.getEnumerator();
+                while (termEnumerator.moveNext()) {
+                    var currentTerm = termEnumerator.get_current();
+
+                    html += "<option value='" + currentTerm.get_id() + "'";
+                    
+                    // if the current option being rendered is in our array
+                    // then make sure the option is selected
+                    if (contains(currentlySelectedTerms, currentTerm.get_name())) {
+                        html += " selected='selected'";
+                    }
+
+                    html += ">" + currentTerm.get_name() + "</option>";
+
+                    
+                }
+
+                // add the options
+                jQuery(elementId).append(html);
+
+                // check if we selected one of them
+                var selectedOption = jQuery(elementId + " option[selected='selected']");
+                if (selectedOption.length > 0)
+                {
+                    // simulate the same "sender.target" property used in a click event
+                    var newSender = {
+                        'target': selectedOption[0]
+                    };
+                    // recursively render the child drop downs
+                    _selectTerm(newSender);
+                }
+
+                jQuery(elementId + " option").click(jslinkTemplates.Taxonomy.selectTerm);
+
+
+            },
+            function (x, args) {
+                alert("Call failed. Error: " + args.get_message());
+            }
+        );
+    };
+
+    function contains(a, obj) {
+        for (var i = 0; i < a.length; i++) {
+            if (a[i] === obj) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     function _selectTerm(sender) {
         var termId = jQuery(sender.target).val();
         var termLabel = jQuery(sender.target).text();
@@ -82,17 +163,37 @@ jslinkTemplates.Taxonomy = function() {
                     var termEnumerator = childTerms.getEnumerator();
                     while (termEnumerator.moveNext()) {
                         var currentTerm = termEnumerator.get_current();
-                        html += "<option value='" + currentTerm.get_id() + "'>" + currentTerm.get_name() + "</option>";
+
+                        html += "<option value='" + currentTerm.get_id() + "'";
+
+                        // if the current option being rendered is in our array
+                        // then make sure the option is selected
+                        if (contains(currentlySelectedTerms, currentTerm.get_name())) {
+                            html += " selected='selected'";
+                        }
+
+                        html += ">" + currentTerm.get_name() + "</option>";
                     }
 
-                    // add the new drop-down
+                    // add the new drop-downs
                     jQuery(sender.target).parent().after(html);
+
+                    // check if we selected one of them when we added them
+                    var selectedOption = jQuery('#TERM_' + termId + " option[selected='selected']");
+                    if (selectedOption.length > 0) {
+                        // simulate the same "sender.target" property used in a click event
+                        var newSender = {
+                            'target': selectedOption[0]
+                        };
+                        // recursively render the child drop downs
+                        _selectTerm(newSender);
+                    }
 
                     // and add a click event
                     jQuery("#TERM_" + termId + " option").click(jslinkTemplates.Taxonomy.selectTerm);
                 }
             },
-            function (sender, args) {
+            function (x, args) {
                 alert("Call failed. Error: " + args.get_message());
             }
         );
