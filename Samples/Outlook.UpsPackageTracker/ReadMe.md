4bd3ba030e31dcf4a31ff79f8ba763888915de5e
Doug Perkes <dougperkes@msn.com>
2015-04-23 17:06:29 -0700

    Initial commit of the UPSPackageTracker solution

---
+++ Samples/Outlook.UpsPackageTracker/ReadMe.md
@@ -0,0 +1,56 @@
+# UPS Package Tracker for Outlook 2013 #
+
+### Summary ###
+This sample demonstrates how to create an App for Outlook which extracts data from a mail message, retrieves data from an external service and formats the data for display.
+
+![](http://i.imgur.com/BXXM3sF.png)
+
+### Applies to ###
+-  Outlook 2013
+
+### Prerequisites ###
+In order to run the sample you must create a developer account on the [https://www.ups.com/upsdeveloperkit](https://www.ups.com/upsdeveloperkit "UPS Developer Kit") site. The username, password and access license number from UPS must be placed into the web.config file.
+
+### Solution ###
+Solution | Author(s)
+---------|----------
+UPSPackageTracker.sln | Doug Perkes
+
+### Version history ###
+Version  | Date | Comments
+---------| -----| --------
+1.0  | Apr 23rd 2015 | Initial release
+
+### Disclaimer ###
+**THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.**
+
+
+----------
+
+# Doc scenario 1 #
+Description
+Image
+
+
+## Sub level 1.1 ##
+Description:
+Code snippet:
+```C#
+string scenario1Page = String.Format("scenario1-{0}.aspx", DateTime.Now.Ticks);
+string scenario1PageUrl = csomService.AddWikiPage("Site Pages", scenario1Page);
+```
+
+## Sub level 1.2 ##
+
+# Doc scenario 2 #
+
+## Sub level 2.1 ##
+
+## Sub level 2.2 ##
+
+### Note: ###
+
+## Sub level 2.3 ##
+
+# Doc scenario 3#
+
---
+++ Samples/Outlook.UpsPackageTracker/UPSPackageTrackerWeb/Scripts/Office/1.1/ca-es/outlook_strings.debug.js
@@ -0,0 +1,33 @@
+﻿
+Type.registerNamespace("_u");
+_u.ExtensibilityStrings = function()
+{
+};
+_u.ExtensibilityStrings.registerClass("_u.ExtensibilityStrings");
+_u.ExtensibilityStrings.l_DeleteAttachmentDoesNotExist_Text = "No es pot suprimir el fitxer adjunt perquè no es pot trobar el fitxer adjunt amb l’índex del fitxer adjunt.";
+_u.ExtensibilityStrings.l_EwsRequestOversized_Text = "La sol·licitud supera la mida límit d'1 MB. Modifiqueu la sol·licitud de l'EWS.";
+_u.ExtensibilityStrings.l_ElevatedPermissionNeededForMethod_Text = "Cal un permís elevat per cridar el mètode: '{0}'.";
+_u.ExtensibilityStrings.l_AttachmentErrorName_Text = "Error de fitxer adjunt";
+_u.ExtensibilityStrings.l_InvalidEventDates_Text = "La data de finalització és anterior a la data d'inici.";
+_u.ExtensibilityStrings.l_DisplayNameTooLong_Text = "Un o més dels noms de visualització indicats és massa llarg.";
+_u.ExtensibilityStrings.l_NumberOfRecipientsExceeded_Text = "El nombre total de destinataris al camp no pot superar els {0}.";
+_u.ExtensibilityStrings.l_HtmlSanitizationFailure_Text = "El sanejament d'HTML ha fallat.";
+_u.ExtensibilityStrings.l_DataWriteErrorName_Text = "Error d'escriptura de dades";
+_u.ExtensibilityStrings.l_ElevatedPermissionNeeded_Text = "Per accedir als membres protegits de l'API Javascript per a Office cal un permís elevat.";
+_u.ExtensibilityStrings.l_ExceededMaxNumberOfAttachments_Text = "No s’han pogut afegir els fitxers adjunts perquè el missatge ja conté el nombre màxim de fitxers adjunts";
+_u.ExtensibilityStrings.l_InternalProtocolError_Text = "Error de protocol intern: '{0}'.";
+_u.ExtensibilityStrings.l_AttachmentDeletedBeforeUploadCompletes_Text = "L’usuari ha suprimit el fitxer adjunt abans que acabés la càrrega.";
+_u.ExtensibilityStrings.l_OffsetNotfound_Text = "No s'ha pogut trobar cap desplaçament per a aquesta marca d'hora.";
+_u.ExtensibilityStrings.l_AttachmentExceededSize_Text = "No es pot afegir el fitxer adjunt perquè és massa gran.";
+_u.ExtensibilityStrings.l_InvalidEndTime_Text = "L'hora de finalització no pot ser anterior a l'hora d'inici.";
+_u.ExtensibilityStrings.l_ParametersNotAsExpected_Text = "Els paràmetres indicats no coincideixen amb el format esperat.";
+_u.ExtensibilityStrings.l_AttachmentUploadGeneralFailure_Text = "No es pot afegir el fitxer adjunt a l'element.";
+_u.ExtensibilityStrings.l_NoValidRecipientsProvided_Text = "No s'han subministrat destinataris vàlids.";
+_u.ExtensibilityStrings.l_InvalidAttachmentId_Text = "L'identificador del fitxer adjunt no era vàlid.";
+_u.ExtensibilityStrings.l_CannotAddAttachmentBeforeUpgrade_Text = "Els fitxers adjunts no es poden afegir mentre s’està recuperant la resposta completa o el reenviament del servidor.";
+_u.ExtensibilityStrings.l_EmailAddressTooLong_Text = "Una o més de les adreces electròniques indicades és massa llarga.";
+_u.ExtensibilityStrings.l_InvalidAttachmentPath_Text = "El camí del fitxer adjunt no era vàlid.";
+_u.ExtensibilityStrings.l_AttachmentDeleteGeneralFailure_Text = "No es pot suprimir el fitxer adjunt de l'element.";
+_u.ExtensibilityStrings.l_InternalFormatError_Text = "S'ha produït un error de format intern.";
+_u.ExtensibilityStrings.l_InvalidDate_Text = "L'entrada no es resol en una data vàlida.";
+_u.ExtensibilityStrings.l_CursorPositionChanged_Text = "L’usuari ha canviat la posició del cursor mentre s’estaven inserint les dades."
