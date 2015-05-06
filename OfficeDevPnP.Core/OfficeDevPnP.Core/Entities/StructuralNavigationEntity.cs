b7c895af16061d816c42cd564c4588476a94f1e9
Bert Jansen <bjansen@microsoft.com>
2015-05-06 09:05:45 +0200

    Structural navigation API, first commit

---
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Entities/StructuralNavigationEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Entities
+{
+    public class StructuralNavigationEntity
+    {
+        public StructuralNavigationEntity()
+        {
+            MaxDynamicItems = 20;
+            ShowSubsites = true;
+            ShowPages = false;
+        }
+
+        public bool ManagedNavigation { get; internal set; }
+        public bool ShowSubsites { get; set; }
+        public bool ShowPages { get; set; }
+        public uint MaxDynamicItems { get; set; }
+        public bool ShowSiblings { get; set; }
+
+    }
+}
