d1d3b2bb8765b5e6d9322daae91170f9cc6cbb16
Patrik Björklund <p.bjorklund@gmail.com>
2015-05-12 16:51:46 +0200

    Readd taxonomypicker sample code
    When the sample and component got merged the code showing how do to dependant/filterable taxpicker selections was deleted. This PR reintroduces it

--- Components/Core.TaxonomyPicker/Core.TaxonomyPickerWeb/Pages/Default.aspx
+++ Components/Core.TaxonomyPicker/Core.TaxonomyPickerWeb/Pages/Default.aspx
@@ -24,6 +24,31 @@
                         </div>
                     </td>
                 </tr>
+
+                <tr>
+                    <td class="ms-formlabel" valign="top"><h3 class="ms-standardheader">Continent:</h3></td>
+                    <td class="ms-formbody" valign="top">
+                        <div class="ms-core-form-line" style="margin-bottom: 0px;">
+                            <asp:HiddenField runat="server" ID="taxPickerContinent" />
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="ms-formlabel" valign="top"><h3 class="ms-standardheader">Country:</h3></td>
+                    <td class="ms-formbody" valign="top">
+                        <div class="ms-core-form-line" style="margin-bottom: 0px;">
+                            <asp:HiddenField runat="server" ID="taxPickerCountry" />
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="ms-formlabel" valign="top"><h3 class="ms-standardheader">Region:</h3></td>
+                    <td class="ms-formbody" valign="top">
+                        <div class="ms-core-form-line" style="margin-bottom: 0px;">
+                            <asp:HiddenField runat="server" ID="taxPickerRegion" />
+                        </div>
+                    </td>
+                </tr>
             </table>
 
             <asp:Button runat="server" OnClick="SubmitButton_Click" Text="Submit" />
--- Components/Core.TaxonomyPicker/Core.TaxonomyPickerWeb/Scripts/app.js
+++ Components/Core.TaxonomyPicker/Core.TaxonomyPickerWeb/Scripts/app.js
@@ -34,6 +34,17 @@ $(document).ready(function () {
                                 function () {
                                     //bind the taxonomy picker to the default keywords termset
                                     $('#taxPickerKeywords').taxpicker({ isMulti: true, allowFillIn: true, useKeywords: true }, context);
+
+                                    //bind taxpickers that depend on eachothers choices
+                                    $('#taxPickerContinent').taxpicker({ isMulti: false, allowFillIn: false, useKeywords: false, termSetId: "9df7c69b-267c-4b8b-ab3c-ac5c15cbbfae", levelToShowTerms: 1 }, context, function () {
+                                        $('#taxPickerCountry').taxpicker({ isMulti: false, allowFillIn: false, useKeywords: false, termSetId: "9df7c69b-267c-4b8b-ab3c-ac5c15cbbfae", filterTermId: this._selectedTerms[0].Id, levelToShowTerms: 2 }, context, function () {
+                                            $('#taxPickerRegion').taxpicker({ isMulti: false, allowFillIn: false, useKeywords: false, termSetId: "9df7c69b-267c-4b8b-ab3c-ac5c15cbbfae", filterTermId: this._selectedTerms[0].Id, levelToShowTerms: 3 }, context);
+                                        });
+                                    });
+
+                                    taxPickerIndex["#taxPickerContinent"] = 0;
+                                    taxPickerIndex["#taxPickerCountry"] = 4;
+                                    taxPickerIndex["#taxPickerRegion"] = 5;
                                 });
                         });
                 });
