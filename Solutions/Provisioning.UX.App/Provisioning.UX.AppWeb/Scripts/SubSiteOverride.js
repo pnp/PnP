397fc3d0ad078550703816a4c4ad9c49a86dc878
frankmarasco <fmarasco@microsoft.com>
2015-05-13 20:37:00 -0500

    added new subsite page

--- Solutions/Provisioning.UX.App/Provisioning.UX.AppWeb/Scripts/SubSiteOverride.js
+++ Solutions/Provisioning.UX.App/Provisioning.UX.AppWeb/Scripts/SubSiteOverride.js
@@ -1,1 +1,47 @@
-﻿
+﻿// Register script for MDS if possible
+// RegisterModuleInit("OverrideNewSubSiteLink.js", SubSiteOverride_Inject); //MDS registration
+
+if (document.readyState === "complete") {
+    //Already loaded!
+    SubSiteOverride_Inject(); //non MDS run
+}
+else {
+    //Add onload or DOMContentLoaded event listeners Mozilla, Opera and webkit nightlies currently support this event
+    if (document.addEventListener) {
+        // Use the handy event callback
+        document.addEventListener("DOMContentLoaded", function () { SubSiteOverride_Inject(); }, false);
+        // If IE event model is used
+    } else if (document.attachEvent) {
+        // ensure firing before onload,
+        // maybe late but safe also for iframes
+        document.attachEvent("onreadystatechange", function () { SubSiteOverride_Inject(); });
+    }
+}
+
+// Actual execution
+function SubSiteOverride_Inject() {
+
+    // Run injection only for site content
+    if ((window.location.href.toLowerCase().indexOf("viewlsts.aspx") > -1 && window.location.href.toLowerCase().indexOf("_layouts/15") > -1)) {
+        SubSiteOverride_OverrideLinkToAppUrl();
+    }
+}
+
+// Actual link override. Checking the right URL from root site collection of the tenant/web application
+function SubSiteOverride_OverrideLinkToAppUrl() {
+
+    //Update create new site link point to our custom page.
+    var link = document.getElementById('createnewsite');
+    var url = SubSiteSettings_Web_Url + "/pages/subsite/newsbweb.aspx?SPHostUrl=" + encodeURIComponent(_spPageContextInfo.webAbsoluteUrl);
+    if (link != undefined) {
+        // Could be get from SPSite root web property bag - now hard coded for demo purposes
+        link.href = url;
+    }
+}
+
+if (typeof (Sys) != "undefined" && Boolean(Sys) && Boolean(Sys.Application)) {
+    Sys.Application.notifyScriptLoaded();
+}
+if (typeof (NotifyScriptLoadedAndExecuteWaitingJobs) == "function") {
+    NotifyScriptLoadedAndExecuteWaitingJobs("SubSiteOverride.js");
+}
