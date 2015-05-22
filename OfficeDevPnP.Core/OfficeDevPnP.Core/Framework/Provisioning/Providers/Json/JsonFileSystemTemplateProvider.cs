30b5850eb4b8b540545b0989513e921e641fbaad
Paolo Pialorsi <paolo@pialorsi.com>
2015-05-22 18:59:41 +0200

    Implemented JSON providers and tests. JSON Formatter implementation complete.

---
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Providers/Json/JsonFileSystemTemplateProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeDevPnP.Core.Framework.Provisioning.Connectors;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.Providers.Json
+{
+    public class JsonFileSystemTemplateProvider : JsonTemplateProvider
+    {
+        public JsonFileSystemTemplateProvider(string connectionString, string container) :
+            base(new FileSystemConnector(connectionString, container))
+        {
+        }
+    }
+}
---
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Providers/Json/JsonSharePointTemplateProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Framework.Provisioning.Connectors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.Providers.Json
+{
+    public class JsonSharePointTemplateProvider : JsonTemplateProvider
+    {
+        public JsonSharePointTemplateProvider(ClientRuntimeContext cc, string connectionString, string container) :
+            base(new SharePointConnector(cc, connectionString, container))
+        {
+        }
+    }
+}
