200d4270c23439ed26a5857a53d7b8cf4bb39675
Paolo Pialorsi <paolo@pialorsi.com>
2015-05-22 19:23:40 +0200

    Removed Provisioning.UX.App

--- Solutions/Provisioning.UX.App/Provisioning.UX.App/Provisioning.UX.AppWeb/Scripts/data/data.module.js
+++
@@ -1,6 +0,0 @@
-﻿(function () {
-    'use strict';
-
-    angular.module('app.data', []);
-
-})();
--- Solutions/Provisioning.UX.App/Provisioning.UX.AppWeb/Scripts/wizard/modules/wizard.module.js
+++
@@ -1,8 +0,0 @@
-﻿(function () {
-    'use strict';
-
-    angular.module('app.wizard', [           
-
-    ]);
-
-})();
