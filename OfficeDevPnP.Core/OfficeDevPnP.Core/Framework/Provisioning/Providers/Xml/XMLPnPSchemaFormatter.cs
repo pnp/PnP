51be8c05b5a2c90520c5b32016423e869cc8f674
Chirag Bhatia <chiragbhatia2006@gmail.com>
2015-05-25 19:16:34 +0530

    Fixed Stream copy bug & spell correction

--- OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Providers/Xml/XMLPnPSchemaFormatter.cs
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Framework/Provisioning/Providers/Xml/XMLPnPSchemaFormatter.cs
@@ -111,9 +111,10 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml
                 throw new ArgumentNullException("template");
             }
 
-            // Crate a copy of the source stream
+            // Create a copy of the source stream
             MemoryStream sourceStream = new MemoryStream();
-            template.CopyTo(sourceStream);
+            template.Position = 0;
+            template.CopyTo(sourceStream);            
             sourceStream.Position = 0;
             template = sourceStream;
 
