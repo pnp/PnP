fda7c9a546c09934282d84a839ee6c29f7618c4d
André Lage <ponto_portuca@hotmail.com>
2015-05-26 10:35:00 +0200

    Sample solution to Manage UserCustomActions

---
+++ Samples/Core.ManageUserCustomAction/CoreManageUserCustomAction/Scripts/App.js
@@ -0,0 +1,234 @@
+﻿Core.ManageUserCustomAction.OfficeApp.SP.App = {
+	
+	HostWebUrl: PnPCommon.getQueryStringParameter("SPHostUrl").replace("#", ""),
+	AppWebUrl: PnPCommon.getQueryStringParameter("SPAppWebUrl").replace("#", ""),
+    waitForm: null,
+    GetInit: function () {
+        PnPData.GetTokens();
+        PnPData.GetPermissions();
+        PnPData.GetListTemplates();
+        PnPData.GetListContentTypePicker(PnPApp.AppWebUrl, PnPApp.HostWebUrl, 'CT', PnPApp.GetListContentTypePickerSuccess);
+        PnPData.GetListContentTypePicker(PnPApp.AppWebUrl, PnPApp.HostWebUrl, 'Lists', PnPApp.GetListContentTypePickerSuccess);
+        PnPData.GetProgId();
+        PnPData.GetFileType();
+
+        PnPCommon.CallDataOnFocusAfterInsert("#dlxCustomActionImageUrl", ".PnPTokensImageUrl");
+        PnPCommon.CallDataOnFocusAfterInsert("#dlxCustomActionScriptBlock", ".PnPTokensScriptBlock");
+        PnPCommon.CallDataOnFocusAfterInsert("#dlxCustomActionScriptSrc", ".PnPTokensScriptSrc");
+        PnPCommon.CallDataOnFocusAfterInsert("#dlxCustomActionUrl", ".PnPTokensUrl");
+        PnPCommon.CallDataOnFocusAfterInsert("#dlxCustomActionCommandUIExtension", ".PnPTokensUIExtension");
+        PnPCommon.CallDataOnFocus("#dlxCustomActionRights", "#PnPRights");
+        
+        PnPCommon.CleanForm();
+        PnPApp.GetCustomActions();
+        PnPApp.EditCustomActions();
+        $('#dlxUpdateUserCustomAction').prop('disabled', true);
+        $('#dlxDeleteUserCustomAction').prop('disabled', true);
+        $('#dlxCustomActionLocation').change(function () {
+            if ($('#dlxCustomActionLocation').find(':selected').text().trim() === "ScriptLink")
+            {
+                $("#ZoneScriptBlock").css('display', 'block');
+                $("#ZoneScriptSrc").css('display', 'block');
+            }
+            else {
+                $("#ZoneScriptBlock").css('display', 'none');
+                $("#ZoneScriptSrc").css('display', 'none');
+            }
+        });
+        $('#dlxCustomActionRegistrationType').change(function () {
+            if ($(this).val() === '0') {
+                $(".dropdown").css("display", "none");
+                $("#BtnBrowseListFolder").css('display', 'none');
+                $("#PnPContentTypePicker").css('display', 'none');
+                $(".dropdown").css("display", "none");
+                $("#dlxCustomActionRegistrationId").off();
+                
+            }
+            if ($(this).val() === '1') {
+                $("#BtnBrowseListFolder").css('display', 'inline');
+                $("#PnPContentTypePicker").css('display', 'none');
+                $(".dropdown").css("display", "none");
+                $("#dlxCustomActionRegistrationId").off();
+                PnPCommon.CallDataOnFocus("#dlxCustomActionRegistrationId", "#PnPListTemplates");
+            }
+            else if ($(this).val() === '2') {
+                $(".dropdown").css("display", "none");
+                $("#BtnBrowseListFolder").css('display', 'none');
+                $("#PnPContentTypePicker").css('display', 'inline-table');
+                $("#dlxCustomActionRegistrationId").off();
+                PnPCommon.CallDataOnFocus("#dlxCustomActionRegistrationId", "#PnPContentTypes");
+            }
+            else if ($(this).val() === '3') {
+                $(".dropdown").css("display", "none");
+                $("#BtnBrowseListFolder").css('display', 'none');
+                $("#PnPContentTypePicker").css('display', 'none');
+                $("#dlxCustomActionRegistrationId").off();
+                PnPCommon.CallDataOnFocus("#dlxCustomActionRegistrationId", "#PnPProgId");
+            }
+            else if ($(this).val() === '4') {
+                $(".dropdown").css("display", "none");
+                $("#BtnBrowseListFolder").css('display', 'none');
+                $("#PnPContentTypePicker").css('display', 'none');
+                $("#dlxCustomActionRegistrationId").off();
+                PnPCommon.CallDataOnFocus("#dlxCustomActionRegistrationId", "#PnPFileTypes");
+            }
+            else {
+                $("#BtnBrowseListFolder").css('display', 'none');
+                $("#PnPContentTypePicker").css('display', 'none');
+            }
+        });
+    },
+	GetCustomActions: function() {
+		 PnPCommon.ShowWaitMessage();
+		 PnPData.GetCustomActions(this.AppWebUrl, this.HostWebUrl, PnPApp.GetCustomActionsSuccess);
+    },
+    GetCustomActionsSuccess: function(data) {
+        var results = JSON.parse(data.body).d.results;
+        $("#dlxUserCustomActionsOptions").empty();
+		for (var i = 0; i < results.length; i++) {
+		    $("#dlxUserCustomActionsOptions").append('<option value="' + results[i].Id + '" title="' + results[i].Description + '">' + results[i].Title + '</option>');
+		}
+		PnPCommon.CloseWaitMessage();
+    },
+    GetListContentTypePickerSuccess: function (data) {
+       
+        var jsonObject = data[0];
+
+        for (var i = 0; i < jsonObject.length; i++) {
+            if (data[1] === 'Lists') {
+                $("#PnPLists").append('<li id="' + jsonObject[i].Id + '" onclick=\"document.getElementById(\'dlxCustomActionRegistrationId\').value=\'{\'+this.id+\'}\'\" ><img  src="' + jsonObject[i].ImageUrl + '" >' + jsonObject[i].Title + '</li>');
+            } else if (data[1] === 'CT') {
+                $("#PnPContentType").append('<li style="padding-bottom:5px;" id="' + jsonObject[i].Id.StringValue + '" onclick=\"document.getElementById(\'dlxCustomActionRegistrationId\').value=this.id\"><img src="../_layouts/15/images/pageLayoutHS.png" >' + jsonObject[i].Name + "(" + jsonObject[i].Group + ")" + '</li>');
+            }
+            else {
+                $("#" + data[1]).append('<ul style="padding-left:20px;padding-bottom:5px;" id="' + jsonObject[i].Id.StringValue + '" onclick=\"document.getElementById(\'dlxCustomActionRegistrationId\').value=this.id\"><img src="../_layouts/15/images/pageLayoutHS.png" >' + jsonObject[i].Name + "(" + jsonObject[i].Group + ")" + '</li></ul>');
+            }
+            if (data[1] == 'Lists') {
+                PnPData.GetListContentTypePicker(PnPApp.AppWebUrl, PnPApp.HostWebUrl, jsonObject[i].Id, PnPApp.GetListContentTypePickerSuccess);
+            }
+        }
+    },
+    NewCustomAction: function () {
+        var NewAnswer = confirm("Do you want to create the UserCustomAction:\n \"" + $('#dlxCustomActionTitle').val());
+        if (NewAnswer === true) {
+            PnPCommon.ShowWaitMessage();
+            PnPData.AddUserCustomAction();
+        }
+    },
+    UpdateCustomAction: function () {
+        var UpdateAnswer = confirm("Do you want to update the UserCustomAction:\n \"" + $('#dlxUserCustomActionsOptions').find(':selected').text() + "\"(" + $('#dlxUserCustomActionsOptions').find(':selected').val() + ")");
+        if (UpdateAnswer === true) {
+            PnPCommon.ShowWaitMessage();
+            PnPData.UpdateUserCustomAction();
+        }
+    },
+    EditCustomActions : function(){
+        $('#dlxUserCustomActionsOptions').change(function() {
+            $('#dlxUpdateUserCustomAction').prop('disabled', false);
+            $('#dlxDeleteUserCustomAction').prop('disabled', false);
+            PnPCommon.ShowWaitMessage();
+            PnPData.GetWebUserCustomAction(PnPApp.AppWebUrl, PnPApp.HostWebUrl, $('#dlxUserCustomActionsOptions').find(':selected').val(), PnPApp.GetCustomActionIdSuccess);
+        });
+    },
+    GetCustomActionIdSuccess : function(data) {
+        var result = JSON.parse(data.body).d;
+
+        $('#dlxCustomActionID').val(result.Id);
+        $('#dlxCustomActionName').val(result.Name);
+        $('#dlxCustomActionTitle').val(result.Title);
+        $('#dlxCustomActionDescription').val(result.Description);
+        $('#dlxCustomActionImageUrl').val(result.ImageUrl);
+
+        if (result.Location === "ScriptLink") {
+            $("#ZoneScriptBlock").css('display', 'block');
+            $("#ZoneScriptSrc").css('display', 'block');
+        } else {
+            $("#ZoneScriptBlock").css('display', 'none');
+            $("#ZoneScriptSrc").css('display', 'none');
+        }
+        if ((result.Group === null) || (result.Group === "")) {
+            $('#dlxCustomActionLocation optgroup option').each(function () {
+                if ($(this).text().trim() === result.Location) {
+                    $(this).prop('selected', true);
+                }
+            });
+        }
+        else {
+            $('#dlxCustomActionLocation option:contains("' + result.Group + '")').each(function () {
+                if ($(this).val().trim() === result.Location) {
+                    $(this).prop('selected', true);
+                }
+            });
+        }
+        
+        $('#dlxCustomActionRegistrationType option[value="' + result.RegistrationType + '"]').prop('selected', true);
+        $('#dlxCustomActionRegistrationType').change();
+        $('#dlxCustomActionRegistrationId').val(result.RegistrationId);
+      
+        PnPCommon.GetRights(result.Rights);
+        $('#dlxCustomActionScriptBlock').val(result.ScriptBlock);
+        $('#dlxCustomActionScriptSrc').val(result.ScriptSrc);
+        $('#dlxCustomActionSequence').val(result.Sequence);
+        $('#dlxCustomActionUrl').val(result.Url);
+        $('#dlxCustomActionCommandUIExtension').val(result.CommandUIExtension);
+        $('#dlxCustomActionVersionOfUserCustomAction').val(result.VersionOfUserCustomAction);
+    
+        PnPCommon.CloseWaitMessage();
+    },
+
+    DeleteUserCustomAction: function () {
+        var deleteAnswer = confirm("Do you want to delete the UserCustomAction:\n \"" + $('#dlxUserCustomActionsOptions').find(':selected').text() + "\"(" + $('#dlxUserCustomActionsOptions').find(':selected').val() + ")");
+        if (deleteAnswer == true) {
+            PnPCommon.ShowWaitMessage();
+            PnPData.DeleteUserCustomAction(PnPApp.AppWebUrl, PnPApp.HostWebUrl, $('#dlxUserCustomActionsOptions').find(':selected').val(), PnPApp.GetdeleteCustomActionIdSuccess);
+        }
+        
+    },
+    GetdeleteCustomActionIdSuccess: function (data) {
+        PnPCommon.CloseWaitMessage();
+        PnPCommon.NotifyMessage('UserCustomAction deleted with success!');
+        PnPApp.GetInit();
+    },
+    OpenAssetPortalBrowserDialog: function (type, targetinput, urlToken) {
+        var context = PnPApp.GetContext();
+        var appContext = PnPApp.GetAppContext(context);
+        var dialogCallback = function (result, target) {
+            if (result == SP.UI.DialogResult.OK) {
+                if ((type == "Image" && PnPCommon.isImage(target.AssetText)) || (type == "JS" && PnPCommon.isJS(target.AssetText))) {
+                    var targetUrl = target.AssetUrl.replace(PnPApp.GetLocation(PnPApp.HostWebUrl).pathname, '');
+                    if (targetUrl.charAt(0) == "/" && targetUrl.charAt(1) == "/") {
+                        if (urlToken == 'true') { document.getElementById(targetinput).value = '~Site' + targetUrl.substring(1, targetUrl.length); }
+                        else { document.getElementById(targetinput).value = target.AssetUrl; }
+                    } else {
+                        if (urlToken == 'true') {
+                            if (target.AssetUrl.replace(PnPApp.GetLocation(PnPApp.HostWebUrl).pathname, '').charAt(0) == "/") {
+                                document.getElementById(targetinput).value = '~Site' + target.AssetUrl.replace(PnPApp.GetLocation(PnPApp.HostWebUrl).pathname, '');
+                            } else {
+                                document.getElementById(targetinput).value = '~Site/' + target.AssetUrl.replace(PnPApp.GetLocation(PnPApp.HostWebUrl).pathname, '');
+                            }
+                        }
+                        else { document.getElementById(targetinput).value = target.AssetUrl; }
+                    }
+                } else if (type === "Image") { PnPCommon.NotifyMessage("Select Image Type 'jpg,gif,bmp,png'"); }
+                else if (type === "JS") { PnPCommon.NotifyMessage("Select JavaScript Type 'js'"); }
+            }
+        };
+        PnPCommon.OpenAssetPortalBrowserDialog(context, appContext, dialogCallback);
+    },
+    GetContext: function () {
+        return PnPCommon.GetContext(PnPApp.AppWebUrl);
+    },
+    GetAppContext: function (context) {
+        return PnPCommon.GetAppContext(context, PnPApp.HostWebUrl);
+    },
+    GetLocation: function (href) {
+        var l = document.createElement("a");
+        l.href = href;
+        return l;
+    }
+}
+window.PnPApp = window.Core.ManageUserCustomAction.OfficeApp.SP.App;
+
+$(document).ready(function () {
+    PnPApp.GetInit();
+});
---
+++ Samples/Core.ManageUserCustomAction/CoreManageUserCustomAction/Scripts/Data.js
@@ -0,0 +1,218 @@
+﻿Core.ManageUserCustomAction.OfficeApp.SP.Data = {
+	
+	GetTokensUserAction: [{ value: "~SiteCollection", desc: "The URL of the parent site collection of the current website." }, { value: "~Site", desc: "The URL of the current website." }, { value: "{Site}", desc: "The URL of the current website." }, { value: "{SiteCollection}", desc: "The URL of the parent site of the current website." }, { value: "{ItemId}", desc: "ID (GUID) taken from the list view" }, { value: "{ItemUrl}", desc: "Web-relative URL of the list item (Url)" }, { value: "{RecurrenceId}", desc: "ID of a recurrent item (RecurrenceID)" }, { value: "{SiteUrl}", desc: "The fully qualified URL to the site (Url)" }, { value: "{ListId}", desc: "ID (GUID) of the list (ID)." }, { value: "{ListUrlDir}", desc: "Server-relative URL of the site plus the list's folder." }, { value: "{Source}", desc: "Fully qualified request URL." }, { value: "{SelectedListId}", desc: "ID (GUID) of the list that is currently selected from a list view." }, { value: "{SelectedItemId}", desc: "ID of the item that is currently selected from the list view." }, { value: "{Layouts}", desc: "The URL of the Layouts virtual folder for the current website." }, { value: "{ControlTemplates}", desc: "The URL of the ControlTemplates virtual folder for the current website." }],
+	GetFileTypes: ["accdb", "accdt", "accdc", "accde", "accdr", "asax", "ascx", "asmx", "asp", "aspx", "bmp", "cat", "chm", "cmp", "config", "css", "db", "dib", "disc", "doc", "docm", "docx", "dot", "dotm", "dotx", "dvd", "dwp", "dwt", "eml", "est", "fwp", "gif", "hdp", "hlp", "hta", "htm", "html", "htt", "inf", "ini", "jfif", "jpe", "jpeg", "jpg", "js", "jse", "log", "master", "mht", "mhtml", "mpd", "mpp", "mps", "mpt", "mpw", "mpx", "msg", "msi", "msp", "ocx", "odc", "odp", "odt", "ods", "one", "onepkg", "onetoc2", "pdf", "png", "pot", "potm", "potx", "ppa", "ppam", "ppt", "pptm", "pptx", "pps", "ppsdc", "ppsm", "ppsx", "psp", "psd", "ptm", "ptt", "pub", "rdl", "rsapplication", "rsc", "rsd", "rsds", "rtf", "smdl", "stp", "stt", "thmx", "tif", "tiff", "txt", "vbe", "vbs", "vdw", "vdx", "vsd", "vsl", "vss", "vst", "vsu", "vsw", "vsx", "vtx", "vsdx", "vsdm", "vssm", "vssx", "vstm", "vstx", "wdp", "webpart", "wm", "wma", "wmd", "wmp", "wms", "wmv", "wmx", "wmz", "wsf", "xla", "xlam", "xls", "xlsb", "xlsm", "xlsx", "xlt", "xltb", "xltm", "xltx", "xml", "xps", "xsd", "xsl", "xsn", "xslt", "zip"],
+	GetProgIds: ["Excel.Sheet", "Word.Document", "Word.Document.6", "Word.Document.12", "PowerPoint.Show.8", "PowerPoint.Show.12", "InfoPath.Document.2", "OneNote.Notebook"],
+	
+	GetCustomActions: function(appWebUrl, hostWebUrl, success) {
+        var executor = new SP.RequestExecutor(appWebUrl);
+        executor.executeAsync({
+            url: this.GetCustomActionsRestUrl(appWebUrl, hostWebUrl),
+            method: "GET",
+            headers: { "Accept": "application/json; odata=verbose" },
+            success: success,
+            error: PnPCommon.NotifyError
+        });
+    },
+	GetCustomActionsRestUrl: function(appWebUrl, hostWebUrl) {
+        return appWebUrl + "/_api/SP.AppContextSite(@target)/web/UserCustomActions?$orderby=title asc&$select=Id,Name,Title,Description,Location,RegistrationType,RegistrationId&@target='" + hostWebUrl + "'";
+    },
+	GetWebUserCustomAction: function (appWebUrl, hostWebUrl, id, success) {
+	    var executor = new SP.RequestExecutor(appWebUrl);
+	    executor.executeAsync({
+	        url: this.GetWebUserCustomActionIdRestUrl(appWebUrl, hostWebUrl, id),
+	        method: "GET",
+	        headers: { "Accept": "application/json; odata=verbose" },
+	        success: success,
+	        error: PnPCommon.NotifyError
+	    });
+	},
+	GetWebUserCustomActionIdRestUrl: function (appWebUrl, hostWebUrl, userCustomActionId) {
+	    return appWebUrl + "/_api/SP.AppContextSite(@target)/web/UserCustomActions('" + userCustomActionId + "')?@target='" + hostWebUrl + "'";
+	},
+	GetListContentTypePicker: function (appWebUrl, hostWebUrl, idct, success) {
+	    var executor = new SP.RequestExecutor(appWebUrl);
+	    executor.executeAsync(
+        {
+            url: PnPData.GetListContentTypePickerRestUrl(appWebUrl, hostWebUrl, idct),
+            method: "GET",
+            headers: { "Accept": "application/json; odata=verbose" },
+            success: function (data) {
+                success([JSON.parse(data.body).d.results, idct]);
+            },
+            error: PnPCommon.NotifyError
+        });
+	},
+	GetListContentTypePickerRestUrl: function (appWebUrl, hostWebUrl, idct) {
+	    if (idct == 'CT') {
+	        return appWebUrl + "/_api/SP.AppContextSite(@target)/web/ContentTypes?$select=Name,Id,Group&$orderby=Name&$filter=Group ne '_Hidden'&@target='" + hostWebUrl + "'";
+	    } else if (idct === 'Lists') {
+	        return appWebUrl + "/_api/SP.AppContextSite(@target)/web/Lists?$select=Title,Id,ImageUrl&@target='" + hostWebUrl + "'";
+	    } else {
+	        return appWebUrl + "/_api/SP.AppContextSite(@target)/web/Lists('" + idct + "')/ContentTypes?$select=Id,Name,Group&$filter=Group ne '_Hidden'&@target='" + hostWebUrl + "'";
+	    }
+	},
+	AddUserCustomAction: function () {
+	    var context = new SP.ClientContext(PnPApp.AppWebUrl);
+	    var factory =new SP.ProxyWebRequestExecutorFactory(PnPApp.AppWebUrl);
+	    context.set_webRequestExecutorFactory(factory);
+	    var app = new SP.AppContextSite(context, PnPApp.HostWebUrl);
+	    var web= app.get_web();
+	    UserCustomActions = web.get_userCustomActions();
+	
+	    newUserCustomAction = UserCustomActions.add();
+	    newUserCustomAction.set_name($.trim($('#dlxCustomActionName').val()));
+	    newUserCustomAction.set_title($.trim($('#dlxCustomActionTitle').val()));
+	    newUserCustomAction.set_description($.trim($('#dlxCustomActionDescription').val()));
+	    newUserCustomAction.set_imageUrl($.trim($('#dlxCustomActionImageUrl').val()));
+	    if ($('#dlxCustomActionLocation').find(':selected').val()==="")
+	    {
+	        newUserCustomAction.set_location($.trim($('#dlxCustomActionLocation').find(':selected').text()));
+	    }
+	    else{
+	        newUserCustomAction.set_location($.trim($('#dlxCustomActionLocation').find(':selected').val()));
+	        newUserCustomAction.set_group($.trim($('#dlxCustomActionLocation').find(':selected').text()));
+	    }
+	    newUserCustomAction.set_registrationType(parseInt($('#dlxCustomActionRegistrationType').find(':selected').val()));
+	    newUserCustomAction.set_registrationId($.trim($('#dlxCustomActionRegistrationId').val()));
+	    if ($.trim($('#dlxCustomActionLocation').find(':selected').val()).toLowerCase() === "scriptlink") {
+	        if ($.trim($('#dlxCustomActionScriptSrc').val()).toLowerCase().indexOf('~site', 0) == 0) {
+	            newUserCustomAction.set_scriptSrc($.trim($('#dlxCustomActionScriptSrc').val()));
+	        } else {
+	            newUserCustomAction.set_scriptSrc('~site' + $('#dlxCustomActionScriptSrc').val().trim());
+	        }
+	        newUserCustomAction.set_scriptBlock($('#dlxCustomActionScriptBlock').val());
+	    }
+	    newUserCustomAction.set_url($.trim($('#dlxCustomActionUrl').val()));
+	    newUserCustomAction.set_sequence(parseInt($('#dlxCustomActionSequence').val()));
+		
+	    if($.trim($('#dlxCustomActionRights').val())!=="")
+	    {
+	        newUserCustomAction.set_rights(PnPCommon.GetRightsID());
+	    }   
+	    if($.trim($('#dlxCustomActionCommandUIExtension').val())!=="")
+	    {
+	        newUserCustomAction.set_commandUIExtension($('#dlxCustomActionCommandUIExtension').val());
+	    }
+	    newUserCustomAction.update();
+    
+	    context.executeQueryAsync(
+             function () {
+                 PnPCommon.NotifyMessage("Custom Action created with success");
+                 PnPApp.GetInit();
+             }, PnPCommon.NotifyErrorSPJSOM
+        );
+	},
+	UpdateUserCustomAction: function () {
+	    var context = new SP.ClientContext(PnPApp.AppWebUrl);
+	    var factory = new SP.ProxyWebRequestExecutorFactory(PnPApp.AppWebUrl);
+	    context.set_webRequestExecutorFactory(factory);
+	    var app = new SP.AppContextSite(context, PnPApp.HostWebUrl);
+	    var Web= app.get_web();
+	    var collUserCustomAction = Web.get_userCustomActions();
+
+	    context.load(Web, 'UserCustomActions');
+	    context.executeQueryAsync(
+            function () {
+                var id = $('#dlxUserCustomActionsOptions').find(':selected').val();
+                var customActionEnumerator = collUserCustomAction.getEnumerator();
+
+                while (customActionEnumerator.moveNext()) {
+                    var oUserCustomAction = customActionEnumerator.get_current();
+
+                    if (oUserCustomAction.get_id() == id) {
+
+                        oUserCustomAction.set_name($.trim($('#dlxCustomActionName').val()));
+                        oUserCustomAction.set_title($.trim($('#dlxCustomActionTitle').val()));
+                        oUserCustomAction.set_description($.trim($('#dlxCustomActionDescription').val()));
+                        oUserCustomAction.set_imageUrl($.trim($('#dlxCustomActionImageUrl').val()));
+                        if ($('#dlxCustomActionLocation').find(':selected').val() === "") {
+                            oUserCustomAction.set_location($.trim($('#dlxCustomActionLocation').find(':selected').text()));
+                        }
+                        else {
+                            oUserCustomAction.set_location($.trim($('#dlxCustomActionLocation').find(':selected').val()));
+                            oUserCustomAction.set_group($.trim($('#dlxCustomActionLocation').find(':selected').text()));
+                        }
+
+                        oUserCustomAction.set_registrationType(parseInt($('#dlxCustomActionRegistrationType').find(':selected').val()));
+                        oUserCustomAction.set_registrationId($.trim($('#dlxCustomActionRegistrationId').val()));
+
+                        if ($.trim($('#dlxCustomActionLocation').find(':selected').val()).toLowerCase() === "scriptlink") {
+                            if ($.trim($('#dlxCustomActionScriptSrc').val()).toLowerCase().indexOf('~site', 0) == 0) {
+                                oUserCustomAction.set_scriptSrc($.trim($('#dlxCustomActionScriptSrc').val()));
+                            } else {
+                                oUserCustomAction.set_scriptSrc('~site' + $('#dlxCustomActionScriptSrc').val().trim());
+                            }
+                            oUserCustomAction.set_scriptBlock($('#dlxCustomActionScriptBlock').val());
+                        }
+
+                        oUserCustomAction.set_url($.trim($('#dlxCustomActionUrl').val()));
+                        oUserCustomAction.set_sequence(parseInt($('#dlxCustomActionSequence').val()));
+
+                        if ($.trim($('#dlxCustomActionRights').val()) !== "") {
+                            oUserCustomAction.set_rights(PnPCommon.GetRightsID());
+                        }
+
+                        if ($.trim($('#dlxCustomActionCommandUIExtension').val()) !== "") {
+                            oUserCustomAction.set_commandUIExtension($('#dlxCustomActionCommandUIExtension').val());
+                        }
+
+                        oUserCustomAction.update();
+                        context.executeQueryAsync(
+                         function () {
+                             PnPCommon.CloseWaitMessage();
+                             PnPCommon.NotifyMessage("Custom Action updated with success!");
+                             PnPApp.GetInit();
+                         }, PnPCommon.NotifyErrorSPJSOM);
+                    }
+                }
+            }, PnPCommon.NotifyErrorSPJSOM);
+	    
+	},
+	DeleteUserCustomAction: function (appWebUrl, hostWebUrl, userCustomActionId, success) {
+	    var executor = new SP.RequestExecutor(appWebUrl);
+	    executor.executeAsync(
+        {
+            url: PnPData.GetWebUserCustomActionIdRestUrl(appWebUrl, hostWebUrl, userCustomActionId),
+            method: "POST",
+            headers: {
+                "IF-MATCH": "*",
+                "X-HTTP-Method": "DELETE",
+                "Accept": "application/json; odata=verbose"
+            },
+            success: success,
+            error: PnPCommon.NotifyError
+        });
+	},
+	GetPermissions: function () {
+	    for (var prop in SP.PermissionKind.prototype) {
+	        if (SP.PermissionKind.prototype.hasOwnProperty(prop)) {
+	            $("#PnPRights").append('<div id="' + prop + '" onclick=\"document.getElementById(\'dlxCustomActionRights\').value=document.getElementById(\'dlxCustomActionRights\').value + this.id+\', \'\">' + prop + '</div>');
+	        }
+	    }
+	},
+	GetListTemplates: function () {
+	    var listTemplates = [];
+	    for (var prop in SP.ListTemplateType.prototype) {
+	        if (SP.ListTemplateType.prototype.hasOwnProperty(prop)) {
+	            $("#PnPListTemplates").append('<div id="' + SP.ListTemplateType.prototype[prop] + '" onclick=\"document.getElementById(\'dlxCustomActionRegistrationId\').value=this.id\">' + prop + ':' + SP.ListTemplateType.prototype[prop] + '</div>');
+	        }
+	    }
+	},
+	GetTokens: function (InputHtml, Lisbox) {
+	    for (var i = 0; i < PnPData.GetTokensUserAction.length; i++) {
+	        $(Lisbox).append('<div id="' + PnPData.GetTokensUserAction[i].value + '" onclick=\" $(\'' + InputHtml + '\').val($(\'' + InputHtml + '\').val() + this.id + \' \'\)"><b>' + PnPData.GetTokensUserAction[i].value + '</b>:' + PnPData.GetTokensUserAction[i].desc + '</div>');
+	    }
+	},
+    GetProgId: function () {
+        for (var i = 0; i < PnPData.GetProgIds.length; i++) {
+            $("#PnPProgId").append('<div id="' + PnPData.GetProgIds[i] + '" onclick=\"document.getElementById(\'dlxCustomActionRegistrationId\').value=this.id\">' + PnPData.GetProgIds[i] + '</div>');
+        }
+    },
+    GetFileType: function () {
+        for (var i = 0; i < PnPData.GetFileTypes.length; i++) {
+            $("#PnPFileTypes").append('<div id="' + PnPData.GetFileTypes[i] + '" onclick=\"document.getElementById(\'dlxCustomActionRegistrationId\').value=document.getElementById(\'dlxCustomActionRegistrationId\').value+this.id+\', \'\">' + PnPData.GetFileTypes[i] + '</div>');
+        } 
+    }
+}
+window.PnPData = window.Core.ManageUserCustomAction.OfficeApp.SP.Data;
