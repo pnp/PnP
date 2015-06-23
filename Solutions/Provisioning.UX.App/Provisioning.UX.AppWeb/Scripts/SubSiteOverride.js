97cdbeebf2c2bada37b14d0568781ca314a910ab
frankmarasco <fmarasco@microsoft.com>
2015-06-23 06:54:13 -0500

    updated newsbweb and subsite overide

--- Solutions/Provisioning.UX.App/Provisioning.UX.AppWeb/Scripts/SubSiteOverride.js
+++ Solutions/Provisioning.UX.App/Provisioning.UX.AppWeb/Scripts/SubSiteOverride.js
@@ -1,23 +1,18 @@
-﻿
-if (document.readyState === "complete") {
-    //Already loaded!
-    SubSiteOverride_Inject(); //non MDS run
-}
-else {
-    //Add onload or DOMContentLoaded event listeners Mozilla, Opera and webkit nightlies currently support this event
-    if (document.addEventListener) {
-        // Use the handy event callback
-        document.addEventListener("DOMContentLoaded", function () { SubSiteOverride_Inject(); }, false);
-        // If IE event model is used
-    } else if (document.attachEvent) {
-        // ensure firing before onload,
-        // maybe late but safe also for iframes
-        document.attachEvent("onreadystatechange", function () { SubSiteOverride_Inject(); });
-    }
+﻿var jQuery = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.2.min.js";
+
+// Register script for MDS if possible
+// Is MDS enabled?
+if ("undefined" != typeof g_MinimalDownload && g_MinimalDownload && (window.location.pathname.toLowerCase()).endsWith("/_layouts/15/start.aspx") && "undefined" != typeof asyncDeltaManager) {
+    // Register script for MDS if possible
+    RegisterModuleInit("SubSiteOverride.js", JavaScript_Embed); //MDS registration
+    SubSiteOverride_Embed(); //non MDS run
+} else {
+    SubSiteOverride_Embed();
 }
 
+
 // Execution
-function SubSiteOverride_Inject() {
+function SubSiteOverride_Embed() {
 
     // Run injection only on specific pages
     if ((window.location.href.toLowerCase().indexOf("viewlsts.aspx") > -1 && window.location.href.toLowerCase().indexOf("_layouts/15") > -1)) {
@@ -43,6 +38,7 @@ function SubSiteOverride_ManageWebPage() {
         var link1 = document.getElementById('ctl00_PlaceHolderMain_MngSubwebToolBar_RptControls_newsite');
         var link2 = document.getElementById('ctl00_PlaceHolderMain_MngSubwebToolBar_RptControls_newsite_LinkImage');
         var link3 = document.getElementById('ctl00_PlaceHolderMain_MngSubwebToolBar_RptControls_newsite_LinkText');
+        var url = SubSiteSettings_Web_Url + encodeURIComponent(_spPageContextInfo.webAbsoluteUrl);
         if (link1 != undefined) {
             link1.href = url;
             link2.href = url;
