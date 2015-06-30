6b9b1639ddde0cdd1f46ac0b7171c764b2bc7b50
levalencia <luise.valencia@gmail.com>
2015-06-30 12:03:11 -0500

    Initial version

---
+++ Samples/AzureAD.RedisCacheUserProfile/AzureAD.RedisCacheUserProfile/AzureAD.RedisCacheUserProfile/Controllers/UserProfileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureAD.RedisCacheUserProfile.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        // GET: UserProfile
+        public ActionResult GetPropertiesForUser()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Models.UserProfile userProfile = Cache.UserProfile.GetUserProfile(ClaimsPrincipal.Current.Identities.First().Name);
+            stopWatch.Stop();
+            ViewData["EllapsedTime"] = stopWatch.ElapsedMilliseconds;
+            return PartialView(userProfile);
+        }
+    }
+}
