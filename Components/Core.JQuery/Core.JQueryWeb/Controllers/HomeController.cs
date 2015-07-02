0a96917f06b03a97eaab84d8b4896817344e18da
Patrick <patrick.rodgers@microsoft.com>
2015-07-02 13:46:07 -0400

    Created new project

---
+++ Components/Core.JQuery/Core.JQueryWeb/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Core.JQueryWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        [SharePointContextFilter]
+        public ActionResult Index()
+        {
+            User spUser = null;
+
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
+
+            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            {
+                if (clientContext != null)
+                {
+                    spUser = clientContext.Web.CurrentUser;
+
+                    clientContext.Load(spUser, user => user.Title);
+
+                    clientContext.ExecuteQuery();
+
+                    ViewBag.UserName = spUser.Title;
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
