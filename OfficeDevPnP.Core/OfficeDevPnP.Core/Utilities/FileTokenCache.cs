638174fe5c240916dd3ac275b7f975913ca5183a
Erwin van Hunen <erwin.van.hunen@outlook.com>
2015-07-15 17:14:33 +0200

    Implemented Azure AD Authentication

--- OfficeDevPnP.Core/OfficeDevPnP.Core/Utilities/FileTokenCache.cs
+++ OfficeDevPnP.Core/OfficeDevPnP.Core/Utilities/FileTokenCache.cs
@@ -1,21 +1,19 @@
-﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
-namespace OfficeDevPnP.PowerShell.Commands.Base
+namespace OfficeDevPnP.Core.Utilities
 {
-    class FileTokenCache : TokenCache
+    public class FileTokenCache : TokenCache
     {
         public string CacheFilePath;
         private static readonly object FileLock = new object();
-
-        // Initializes the cache against a local file.
-        // If the file is already present, it loads its content in the ADAL cache
+      
         public FileTokenCache(string filePath = @".\TokenCache.dat")
         {
             CacheFilePath = filePath;
@@ -31,15 +29,12 @@ namespace OfficeDevPnP.PowerShell.Commands.Base
             }
         }
 
-        // Empties the persistent store.
         public override void Clear()
         {
             base.Clear();
             File.Delete(CacheFilePath);
         }
 
-        // Triggered right before ADAL needs to access the cache.
-        // Reload the cache from the persistent store in case it changed since the last access.
         void BeforeAccessNotification(TokenCacheNotificationArgs args)
         {
             lock (FileLock)
@@ -52,20 +47,16 @@ namespace OfficeDevPnP.PowerShell.Commands.Base
             }
         }
 
-        // Triggered right after ADAL accessed the cache.
         void AfterAccessNotification(TokenCacheNotificationArgs args)
         {
-            // if the access operation resulted in a cache update
             if (this.HasStateChanged)
             {
                 lock (FileLock)
                 {
-                    // reflect changes in the persistent store
                     File.WriteAllBytes(CacheFilePath,
                         ProtectedData.Protect(this.Serialize(),
                                                 null,
                                                 DataProtectionScope.CurrentUser));
-                    // once the write operation took place, restore the HasStateChanged bit to false
                     this.HasStateChanged = false;
                 }
             }
