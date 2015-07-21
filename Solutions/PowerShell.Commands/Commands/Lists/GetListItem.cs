c1959facac1bef6a10a436c06d0f0796d189fdd9
Erwin van Hunen <erwin.van.hunen@outlook.com>
2015-07-21 20:50:30 +0200

    Adjusted help message and attribute desciption. Included Field parameter parsing when not query is specified.

--- Solutions/PowerShell.Commands/Commands/Lists/GetListItem.cs
+++ Solutions/PowerShell.Commands/Commands/Lists/GetListItem.cs
@@ -4,30 +4,32 @@ using System.Text;
 using Microsoft.SharePoint.Client;
 using OfficeDevPnP.PowerShell.CmdletHelpAttributes;
 using OfficeDevPnP.PowerShell.Commands.Base.PipeBinds;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace OfficeDevPnP.PowerShell.Commands.Lists
 {
     [Cmdlet(VerbsCommon.Get, "SPOListItem")]
     [CmdletHelp("Retrieves list items", Category = "Lists")]
     [CmdletExample(
-        Code = "PS:> Get-SPOListItem -List Tasks", 
-        Remarks = "Retrieves all list items from the tasks lists", 
+        Code = "PS:> Get-SPOListItem -List Tasks",
+        Remarks = "Retrieves all list items from the tasks lists",
         SortOrder = 1)]
     [CmdletExample(
-        Code = "PS:> Get-SPOListItem -List Tasks -Id 1", 
-        Remarks = "Retrieves the list item with ID 1 from from the tasks lists. This parameter is ignored if the Query parameter is specified.", 
+        Code = "PS:> Get-SPOListItem -List Tasks -Id 1",
+        Remarks = "Retrieves the list item with ID 1 from from the tasks lists. This parameter is ignored if the Query parameter is specified.",
         SortOrder = 2)]
     [CmdletExample(
-        Code = "PS:> Get-SPOListItem -List Tasks -UniqueId bd6c5b3b-d960-4ee7-a02c-85dc6cd78cc3", 
-        Remarks = "Retrieves the list item with unique id bd6c5b3b-d960-4ee7-a02c-85dc6cd78cc3 from from the tasks lists. This parameter is ignored if the Query parameter is specified.", 
+        Code = "PS:> Get-SPOListItem -List Tasks -UniqueId bd6c5b3b-d960-4ee7-a02c-85dc6cd78cc3",
+        Remarks = "Retrieves the list item with unique id bd6c5b3b-d960-4ee7-a02c-85dc6cd78cc3 from from the tasks lists. This parameter is ignored if the Query parameter is specified.",
         SortOrder = 3)]
     [CmdletExample(
-        Code = "PS:> Get-SPOListItem -List Tasks -Fields \"Title\",\"GUID\"", 
-        Remarks = "Retrieves all list items, but only includes the Title and GUID fields. This parameter is ignored if the Query parameter is specified.", 
+        Code = "PS:> Get-SPOListItem -List Tasks -Fields \"Title\",\"GUID\"",
+        Remarks = "Retrieves all list items, but only includes the values of the Title and GUID fields in the list item object. This parameter is ignored if the Query parameter is specified.",
         SortOrder = 4)]
     [CmdletExample(
-        Code = "PS:> Get-SPOListItem -List Tasks -Query \"<View><Query><Where><Eq><FieldRef Name='GUID'/><Value Type='Guid'>bd6c5b3b-d960-4ee7-a02c-85dc6cd78cc3</Value></Eq></Where></Query></View>\"", 
-        Remarks = "Retrieves all list items based on the CAML query specified.", 
+        Code = "PS:> Get-SPOListItem -List Tasks -Query \"<View><Query><Where><Eq><FieldRef Name='GUID'/><Value Type='Guid'>bd6c5b3b-d960-4ee7-a02c-85dc6cd78cc3</Value></Eq></Where></Query></View>\"",
+        Remarks = "Retrieves all list items based on the CAML query specified.",
         SortOrder = 5)]
     public class GetListItem : SPOWebCmdlet
     {
@@ -43,7 +45,7 @@ namespace OfficeDevPnP.PowerShell.Commands.Lists
         [Parameter(Mandatory = false, HelpMessage = "The CAML query to execute against the list")]
         public string Query;
 
-        [Parameter(Mandatory = false, HelpMessage = "The fields to retrieve. If not specified all fields will be loaded.")]
+        [Parameter(Mandatory = false, HelpMessage = "The fields to retrieve. If not specified all fields will be loaded in the returned list object.")]
         public string[] Fields;
 
         protected override void ExecuteCmdlet()
@@ -88,7 +90,7 @@ namespace OfficeDevPnP.PowerShell.Commands.Lists
             }
             else if (Query != null)
             {
-                CamlQuery query = new CamlQuery {ViewXml = Query};
+                CamlQuery query = new CamlQuery { ViewXml = Query };
                 var listItems = list.GetItems(query);
                 ClientContext.Load(listItems);
                 ClientContext.ExecuteQueryRetry();
@@ -96,7 +98,30 @@ namespace OfficeDevPnP.PowerShell.Commands.Lists
             }
             else
             {
-                var listItems = list.GetItems(CamlQuery.CreateAllItemsQuery());
+                var query = CamlQuery.CreateAllItemsQuery();
+                if (Fields != null)
+                {
+                    var queryElement = XElement.Parse(query.ViewXml);
+
+                    var viewFields = queryElement.Descendants("ViewFields").FirstOrDefault();
+                    if (viewFields != null)
+                    {
+                        viewFields.RemoveAll();
+                    } else
+                    {
+                        viewFields = new XElement("ViewFields");
+                        queryElement.Add(viewFields);
+                    }
+
+                    foreach (var field in Fields)
+                    {
+                        XElement viewField = new XElement("FieldRef");
+                        viewField.SetAttributeValue("Name", field);
+                        viewFields.Add(viewField);
+                    }
+                    query.ViewXml = queryElement.ToString();
+                }
+                var listItems = list.GetItems(query);
                 ClientContext.Load(listItems);
                 ClientContext.ExecuteQueryRetry();
                 WriteObject(listItems, true);
