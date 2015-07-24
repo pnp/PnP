8a7d7c0e85a886f1b5dc10ff9f7a992e2462ea6d
VincenttNL <vincent.wesker@live.com>
2015-07-24 11:53:41 +0200

    Added Remove-SPOWeb cmdlet. Removes subweb from web

---
+++ Solutions/PowerShell.Commands/Commands/Web/RemoveWeb.cs
@@ -0,0 +1,25 @@
+﻿using System.Management.Automation;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.PowerShell.CmdletHelpAttributes;
+
+namespace OfficeDevPnP.PowerShell.Commands
+{
+    [Cmdlet(VerbsCommon.Remove, "SPOWeb")]
+    [CmdletHelp("Removes a subweb in the current web", Category = "Webs")]
+    [CmdletExample(
+        Code = @"PS:> Remove-SPOWeb -Url projectA",
+        Remarks = "Remove a web",
+        SortOrder = 1)]
+
+    public class RemoveWeb : SPOWebCmdlet
+    {
+        [Parameter(Mandatory = true, HelpMessage = "The Url of the web")]
+        public string Url;
+
+        protected override void ExecuteCmdlet()
+        {
+            var web = SelectedWeb.DeleteWeb(Url);
+            ClientContext.ExecuteQueryRetry();
+        }
+    }
+}
