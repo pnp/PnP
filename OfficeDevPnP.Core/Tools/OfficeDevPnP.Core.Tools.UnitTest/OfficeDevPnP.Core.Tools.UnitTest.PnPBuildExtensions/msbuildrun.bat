f26a58df1315f8ff49f8b3477596532dde8208b9
Bert Jansen <bjansen@microsoft.com>
2015-07-31 09:06:23 +0200

    Added exit to script to ensure the cmd.exe session terminates when being run from a scheduled task using a service account

--- OfficeDevPnP.Core/Tools/OfficeDevPnP.Core.Tools.UnitTest/OfficeDevPnP.Core.Tools.UnitTest.PnPBuildExtensions/msbuildrun.bat
+++ OfficeDevPnP.Core/Tools/OfficeDevPnP.Core.Tools.UnitTest/OfficeDevPnP.Core.Tools.UnitTest.PnPBuildExtensions/msbuildrun.bat
@@ -4,3 +4,4 @@ c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe PnPCore.targets /prope
 c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe PnPCore.targets /property:PnPConfigurationToTest=OnlineCred /target:BuildAndUnitTestPnP
 c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe PnPCore.targets /property:PnPConfigurationToTest=OnlineAppOnly /target:BuildAndUnitTestPnP
 c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe PnPCore.targets /target:PushResultsToGitHub
+exit
