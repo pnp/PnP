2e77cf9ba3c12d1a94804efea619247323e7dd3c
VesaJuvonen <vesa.juvonen@hotmail.com>
2015-08-03 22:22:41 +0300

    Draft version of the governance timer jobs, waiting for polishing and fine tuning.

---
+++ Solutions/Governance.TimerJobs/Governance.TimerJobs.Policy/DefaultSitePolicyProvider.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using Governance.TimerJobs.Data;
+
+namespace Governance.TimerJobs.Policy
+{
+    public class DefaultSitePolicyProvider : ISitePolicyProvider
+    {
+        private readonly GovernancePlan extSharedTeamsPlan;
+        private readonly GovernancePlan extSharingOffTeamsPlan;
+
+        private List<ISitePolicy> AllSitePolicies;
+
+        public DefaultSitePolicyProvider()
+        {
+            StampingSites = false;
+            EnsureCustomization = false;
+            UpdateExistingCustomAction = false;
+
+            #region Site Policy
+
+            AllSitePolicies = new List<ISitePolicy>();
+
+            var lifeCyclePolicy = new LifeCyclePolicy()
+            {
+                Id = new Guid("{C62F3C53-61CA-4566-AA1C-34AA26D61FB9}"),
+                Name = "Check Site Lifecycle",
+                Description = "Site must follow the decommission/extending process once provisioned."
+            };
+            AllSitePolicies.Add(lifeCyclePolicy);
+
+            var administratorsPolicy = new AdministratorsPolicy
+            {
+                Id = new Guid("{A89D5DDA-8EF4-416A-93A2-73ABCEB5076D}"),
+                Name = "Check Site Collection Admins Count",
+                Description = "AdministratorsPolicy make sure a site collection have 2 administrators at least"
+            };
+            AllSitePolicies.Add(administratorsPolicy);
+
+            var hbiBroadAccessPolicy = new HbiBroadAccessPolicy
+            {
+                Id = new Guid("{950CF798-926B-4A1D-A867-D18D6E293C34}"),
+                Name = "Check Broad Access Group on HBI Sites",
+                Description =
+                    "HbiBroadAccessPolicy make sure there is no permission granted to predefined large groups at the site collection of sub sites level"
+            };
+            AllSitePolicies.Add(hbiBroadAccessPolicy);
+
+            var membershipReviewPolicy = new MembershipReviewPolicy
+            {
+                Id = new Guid("{1C4B31C9-B6DE-443A-89CD-9482E9ACD554}"),
+                Name = "Check External Users",
+                Description =
+                    "Membership review policy make sure a site collection's owner review its external users in a time period"
+            };
+            AllSitePolicies.Add(membershipReviewPolicy);
+
+            var siteMetadataPolicy = new SiteMetadataPolicy
+            {
+                Id = new Guid("{B2FF8BFA-27CD-4AD4-BBBA-C13FBF507C22}"),
+                Name = "Check If Missing Metadata",
+                Description = "Site Metadata Policy mark sure a site is not missing required metadata"
+            };
+            AllSitePolicies.Add(siteMetadataPolicy);
+
+            #endregion
+
+            #region Plan
+
+            extSharingOffTeamsPlan = new GovernancePlan
+            {
+                Id = new Guid("{1C19DA06-A357-4820-9DF9-BEECA39BB752}"),
+                Name = "External Sharing Disabled Team Site Governance Plan",
+                Description = "Governance Plan for all SharePoint Team Sites which disable External Sharing",
+                PolicyCollection = new ISitePolicy[]
+                {
+                    siteMetadataPolicy,
+                    administratorsPolicy,
+                    hbiBroadAccessPolicy,
+                    lifeCyclePolicy
+                }
+            };
+            extSharedTeamsPlan = new GovernancePlan
+            {
+                Id = new Guid("{1D4C0AB9-03F4-4BBB-9DBE-0CE15DBFB805}"),
+                Name = "Externally Shared Team Site Governance Plan",
+                Description = "Governance Plan for all Shared SharePoint Team Sites which enable External Sharing",
+                PolicyCollection = new ISitePolicy[]
+                {
+                    siteMetadataPolicy,
+                    administratorsPolicy,
+                    hbiBroadAccessPolicy,
+                    membershipReviewPolicy,
+                    lifeCyclePolicy
+                }
+            };
+
+            #endregion
+        }
+
+        public bool StampingSites { get; set; }
+        public bool EnsureCustomization { get; set; }
+        public bool UpdateExistingCustomAction { get; set; }
+        public bool IsShowAlert { get; set; }
+        public bool IsShowExternalSharing { get; set; }
+
+        public virtual GovernancePlan GetGovernancePlan(SiteInformation site)
+        {
+            // Add GetGovernnance Plan Logic Here
+            var plan = site.IsExternalSharingEnabled ? extSharedTeamsPlan : extSharingOffTeamsPlan;
+            return plan;
+        }
+
+        public IEnumerable<ISitePolicy> GetAllGovernancePolicy()
+        {
+            return AllSitePolicies;
+        }
+    }
+}
---
+++ Solutions/Governance.TimerJobs/Governance.TimerJobs/GovernancePreprocessJob.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Governance.TimerJobs.Data;
+using Governance.TimerJobs.Policy;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Framework.TimerJobs;
+
+namespace Governance.TimerJobs
+{
+    /// <summary>
+    /// GovernancePreprocessJob is designed to support some special site policies (like HbiBroadAccessPolicy) which requires a customizable site scope query (check all HBI webs) as well as a complex DB status updates process.
+    /// </summary>
+    public class GovernancePreprocessJob : DatabaseTimerJob
+    {
+        /// <summary>
+        /// The complex site policy attached with this GovernancePreprocessJob
+        /// </summary>
+        private ISitePolicy Policy
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Construct a new instance of GovernancePreprocessJob
+        /// </summary>
+        /// <param name="repository">The db repository</param>
+        /// <param name="url">The tenant URL</param>
+        /// <param name="policy">The complex site policy which requires preprocess</param>
+        public GovernancePreprocessJob(GovernanceDbRepository repository, string url, ISitePolicy policy)
+            : base("GovernancePreprocessJob", repository, url)
+        {
+            Policy = policy;
+        }
+
+        /// <summary>
+        /// Query DB for all root web or sub web records that matches the PreprocessPredictor criteria of the the attached site policy
+        /// </summary>
+        /// <param name="dbContext">The db context object</param>
+        /// <param name="webList">The web records list</param>
+        protected override void ResolveSitesFromDb(GovernanceDbContext dbContext, List<string> webList)
+        {
+            int maxPage;
+            int page = 1;
+            do
+            {
+                var webs = dbContext.GetAllWebs(
+                    page, PageSize, out maxPage, new[] { Policy.PreprocessPredictor });
+                foreach (var web in webs)
+                {
+                    webList.Add(web.Url);
+                }
+            }
+            while (page++ < maxPage);
+        }
+
+        /// <summary>
+        /// Run preprocess for the current site
+        /// </summary>
+        /// <param name="sender">The current timer job instance</param>
+        /// <param name="e">The timer job run event arguments</param>
+        protected override void TimerJobRunImpl(object sender, TimerJobRunEventArgs e)
+        {
+            DbRepository.UsingContext(dbContext => {
+                var web = dbContext.GetWeb(e.Url);
+                var site = dbContext.GetSite(web.SiteUrl);
+                Policy.Preprocess(site, web, e);
+                dbContext.SaveChanges();
+            });            
+        }
+    }
+}
