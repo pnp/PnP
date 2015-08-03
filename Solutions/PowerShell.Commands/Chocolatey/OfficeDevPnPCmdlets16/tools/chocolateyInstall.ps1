61cd3320a4fcb6fb8bbc8308e1585f9616c82c58
Erwin van Hunen <erwin.van.hunen@outlook.com>
2015-08-04 00:07:37 +0200

    added chocolatey packages

---
+++ Solutions/PowerShell.Commands/Chocolatey/OfficeDevPnPCmdlets16/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿$name = "officedevpnppowershellcommands16"
+$url = "https://github.com/OfficeDev/PnP/blob/master/Binaries/PowerShell.Commands/PnPPowerShellCommands16.msi?raw=true"
+Install-ChocolateyPackage $name 'msi' '/quiet' $url
