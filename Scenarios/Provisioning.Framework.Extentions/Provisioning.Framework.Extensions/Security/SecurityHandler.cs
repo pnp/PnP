ab9ad5489b5f76188d0f6d9a30cff1efa485ec64
ivan.vagunin <ivan.vagunin@gmail.com>
2015-08-10 06:57:42 -0700

    security extensions

---
+++ Scenarios/Provisioning.Framework.Extentions/Provisioning.Framework.Extensions/Security/SecurityHandler.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Linq;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Framework.Provisioning.Extensibility;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using Schema = Provisioning.Framework.Extensions.SecurityProvisionSchema;
+using Microsoft.SharePoint.Client.Utilities;
+using System.Text;
+
+namespace Provisioning.Framework.Extensions
+{
+    class SecurityHandler : IProvisioningExtensibilityProvider
+    {
+        public void ProcessRequest(ClientContext ctx, ProvisioningTemplate template, string configurationData)
+        {
+            var config = !string.IsNullOrEmpty(configurationData) ? XmlHelper.ReadXmlString<Schema.SecurityConfiguration>(configurationData) : null;
+            if (config != null)
+            {
+                foreach(var level in config.PermissionLevels) 
+                {
+                    ProvisionPermissionLevel(ctx, level);
+                }
+
+                foreach(var group in config.Groups) 
+                {
+                    ProvisionGroup(ctx, group);
+                }
+
+                foreach(var scope in config.PermissionScopes) 
+                {
+                    ProvisionListScope(ctx, scope);
+                }
+            }
+        }
+
+        private void ProvisionListScope(ClientContext ctx, Schema.ListPermissionScope scope)
+        {
+            var web = ctx.Web;
+            var targetList = web.Lists.GetByTitle(scope.Title);
+            ctx.Load(targetList, l => l.RootFolder, l => l.RootFolder.ServerRelativeUrl);
+            foreach (var permission in scope.Permissions)
+            {
+                var principal = ResolvePrincipal(ctx, permission.Principal);
+                targetList.BreakRoleInheritance(scope.CopyRoleAssignments, scope.ClearSubscopes);
+                AssignPermissionLevel(ctx, targetList, principal, permission.PermissionLevel);
+            }
+
+            if (scope.FolderScope != null && scope.FolderScope.Length > 0)
+            {
+                foreach (var folder in scope.FolderScope)
+                {
+                    if (folder.Permissions != null && folder.Permissions.Length > 0)
+                    {
+                        var folderUrl = GetListSubfolderUrl(targetList, folder.RelativeUrl);
+                        var targetFolder = web.GetFolderByServerRelativeUrl(folderUrl).ListItemAllFields;
+                        foreach (var permission in folder.Permissions)
+                        {
+                            var principal = ResolvePrincipal(ctx, permission.Principal);
+                            targetFolder.BreakRoleInheritance(folder.CopyRoleAssignments, folder.ClearSubscopes);
+                            AssignPermissionLevel(ctx, targetFolder, principal, permission.ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ProvisionPermissionLevel(ClientContext ctx, Schema.PermissionLevel permissionLevel)
+        {
+            if (!PermissionLevelExist(ctx, permissionLevel.Title))
+            {
+                var levelPermissions = new BasePermissions();
+                foreach (var permission in permissionLevel.Permissions)
+                {
+                    PermissionKind parsedPermission;
+                    if (Enum.TryParse(permission.ToString(), out parsedPermission))
+                    {
+                        levelPermissions.Set(parsedPermission);
+                    }
+                    else
+                    {
+                        throw new Exception(String.Format("Unknown permission name: {0}", permission));
+                    }
+                }
+                var creationInfo = new RoleDefinitionCreationInformation
+                {
+                    Name = permissionLevel.Title,
+                    Description = permissionLevel.Description,
+                    BasePermissions = levelPermissions
+                };
+                ctx.Web.RoleDefinitions.Add(creationInfo);
+                ctx.ExecuteQueryRetry();
+            }
+        }
+
+        private void ProvisionGroup(ClientContext ctx, Schema.Group group)
+        {
+            var web = ctx.Web;
+            var siteGroups = web.SiteGroups;
+            ctx.Load(siteGroups, sg => sg.Include(g => g.Title));
+            ctx.ExecuteQueryRetry();
+
+            Group targetGroup;
+            // check if group exists
+            if (siteGroups.Any(sg => sg.Title == group.Title))
+            {
+                targetGroup = siteGroups.First(sg => sg.Title == group.Title);
+            }
+            else
+            {
+                // create group
+                var newGroupInfo = new GroupCreationInformation
+                {
+                    Title = group.Title,
+                    Description = group.Description
+                };
+                targetGroup = web.SiteGroups.Add(newGroupInfo);
+            }
+            // assign permission level
+            AssignPermissionLevel(ctx, web, targetGroup, group.PermissionLevel);
+            
+            // add members
+            if (group.Members != null)
+            {
+                foreach (var user in group.Members)
+                {
+                    var spUser = web.EnsureUser(user);
+                    ctx.Load(spUser);
+                    targetGroup.Users.AddUser(spUser);
+                }
+                ctx.ExecuteQueryRetry();
+            }
+        }
+
+        private bool PermissionLevelExist(ClientContext ctx, string permissionLevelName)
+        {
+            bool res;
+            try
+            {
+                var level = ctx.Web.RoleDefinitions.GetByName(permissionLevelName);
+                ctx.Load(level);
+                ctx.ExecuteQueryRetry();
+                res = true;
+            }
+            catch (ServerException ex)
+            {
+                if (ex.ServerErrorCode == -2146232832)
+                {
+                    res = false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return res;
+        }
+
+        private static void AssignPermissionLevel(ClientContext ctx, SecurableObject target, Principal principal, string permissionLevel, bool reset = false)
+        {
+            RoleDefinition roleDefinition;
+            DefaultPermissionLevels permission;
+            // find OOB or custom RoleDefinition
+            if (Enum.TryParse(permissionLevel, out permission))
+            {
+                roleDefinition = ctx.Web.RoleDefinitions.GetById((int)permission);
+            }
+            else
+            {
+                roleDefinition = ctx.Web.RoleDefinitions.GetByName(permissionLevel);
+            }
+            var bindingCollection = new RoleDefinitionBindingCollection(ctx) { roleDefinition };
+            // cleanup existing assignments if needed
+            if (reset)
+            {
+                var assignment = target.RoleAssignments.GetByPrincipal(principal);
+                ctx.Load(assignment);
+                assignment.RoleDefinitionBindings.RemoveAll();
+                assignment.Update();
+            }
+            // assign permission level and save changes
+            target.RoleAssignments.Add(principal, bindingCollection);
+            ctx.ExecuteQueryRetry();
+        }
+
+        private static Principal ResolvePrincipal(ClientContext ctx, string principalName)
+        {
+            Principal principal = null;
+            var info = Utility.ResolvePrincipal(ctx, ctx.Web, principalName, PrincipalType.User | PrincipalType.SecurityGroup | PrincipalType.SharePointGroup, PrincipalSource.All, null, false);
+            ctx.ExecuteQueryRetry();
+            switch (info.Value.PrincipalType)
+            {
+                case PrincipalType.User:
+                    principal = ctx.Web.EnsureUser(info.Value.LoginName);
+                    break;
+                case PrincipalType.SharePointGroup:
+                case PrincipalType.SecurityGroup:
+                    principal = ctx.Web.SiteGroups.GetById(info.Value.PrincipalId);
+                    break;
+            }
+            ctx.Load(principal);
+            ctx.ExecuteQueryRetry();
+            return principal;
+        }
+
+        private static string GetListSubfolderUrl(List list, string relativeUrl)
+        {
+            var sb = new StringBuilder(list.RootFolder.ServerRelativeUrl);
+            sb.AppendFormat("/{0}", relativeUrl);
+            return sb.ToString();
+        }
+    }
+}
--- Scenarios/Provisioning.Framework.Extentions/readme.md
+++ Scenarios/Provisioning.Framework.Extentions/readme.md
@@ -14,12 +14,13 @@ None
 ### Solution ###
 Solution | Author(s)
 ---------|----------
-Provisioning.Framework.Extensions | Ivan Vagunin (Knowit Oy)
+Provisioning.Framework.Extensions | Ivan Vagunin (Knowit Oy), Alisher Abdurakhmanov (Datium Oy)
 
 ### Version history ###
 Version  | Date | Comments
 ---------| -----| --------
 1.0  | August 7th 2015 | Initial release
+1.1  | August 10th 2015 | Security extensions added
 
 ### Disclaimer ###
 **THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.**
@@ -29,7 +30,7 @@ Version  | Date | Comments
 
 # Navigation #
 Navigation\NavigationHandler implements extensibility provider for provisioning structural and taxonomy navigation
-Navigation\NavigationProvisionSchema.xsd - xml schema for extensibility provide, use compile_xsd.bat to generate C# classes based on schema
+Navigation\NavigationProvisionSchema.xsd - xml schema for extensibility provider, use compile_xsd.bat to generate C# classes based on schema
 
 Current version supports only top level navigation nodes. For structural navigation existing all nodes are deleted before provisioning new nodes
 
@@ -53,6 +54,65 @@ Taxonomy navigation xml snippet:
 				<pnpx:NavigationNode Title="Home" Url="/"/>
 			</pnpx:Navigation>
 		</pnpx:Navigations>
+	</pnpx:Configuration>
+</pnp:Provider>
+
+# Security #
+Security\SecurityHandler implements extensibility provider for security related objects
+Security\SecurityProvisionSchema.xsd - xml schema for extensibility provider, use compile_xsd.bat to generate C# classes based on schema
+
+Permission level xml snippet:
+<pnp:Provider Enabled="true" HandlerType="Provisioning.Framework.Extensions.SecurityHandler, Provisioning.Framework.Extensions">
+	<pnp:Configuration>
+		<pnpx:Security xmlns:pnpx="http://schemas.dev.office.com/PnP/2015/05/ProvisioningSchema/Extentions/Security">
+			<pnpx:PermissionLevels>
+				<pnpx:PermissionLevel Title="Restricted Read" Description="Can view pages and documents. No access to system pages">
+					<pnpx:Permissions>
+						<pnpx:BasePermission>ViewPages</pnpx:BasePermission>
+						<pnpx:BasePermission>Open</pnpx:BasePermission>
+						<pnpx:BasePermission>ViewListItems</pnpx:BasePermission>
+						<pnpx:BasePermission>OpenItems</pnpx:BasePermission>
+					</pnpx:Permissions>
+                </pnpx:PermissionLevel>
+            </pnpx:PermissionLevels>
+		</pnpx:Security>
+	</pnpx:Configuration>
+</pnp:Provider>
+
+Security group snippet:
+<pnp:Provider Enabled="true" HandlerType="Provisioning.Framework.Extensions.SecurityHandler, Provisioning.Framework.Extensions">
+	<pnp:Configuration>
+		<pnpx:Security xmlns:pnpx="http://schemas.dev.office.com/PnP/2015/05/ProvisioningSchema/Extentions/Security">
+			<pnpx:Groups>
+                <pnpx:Group Title="Finance Department" PermissionLevel="Restricted Read" Description="Members of this group have special permissions">
+					<pnpx:Members>
+						<pnpx:Member>user1@mytentancy.onmicrosoft.com</pnpx:Member>
+						<pnpx:Member>user2@mytentancy.onmicrosoft.com</pnpx:Member>
+					</pnpx:Members>
+                </pnpx:Group>
+            </pnpx:Groups>
+		</pnpx:Security>
+	</pnpx:Configuration>
+</pnp:Provider>
+
+List and folders level permissions:
+<pnp:Provider Enabled="true" HandlerType="Provisioning.Framework.Extensions.SecurityHandler, Provisioning.Framework.Extensions">
+	<pnp:Configuration>
+		<pnpx:Security xmlns:pnpx="http://schemas.dev.office.com/PnP/2015/05/ProvisioningSchema/Extentions/Security">
+			<pnpx:PermissionScopes>
+				<pnpx:ListScope Title="Documents" CopyRoleAssignments="false" ClearSubscopes="false">
+					<pnpx:Permissions>
+						<pnpx:Permission Principal="Site Owners" PermissionLevel="FullControl"/>
+						<pnpx:Permission Principal="Finance Department" PermissionLevel="Read"/>
+					</pnpx:Permissions>
+					<pnpx:FolderScope RelativeUrl="FinancialDocuments" CopyRoleAssignments="false">
+						<pnpx:Permissions>
+							<pnpx:Permission PermissionLevel="FullControl" Principal="Finance Department"/>
+						</pnpx:Permissions>
+					</pnpx:FolderScope>
+                </pnpx:ListScope>
+            </pnpx:PermissionScopes>
+		</pnpx:Security>
 	</pnpx:Configuration>
 </pnp:Provider>
 
