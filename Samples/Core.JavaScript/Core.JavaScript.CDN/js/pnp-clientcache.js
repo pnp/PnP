72f7c0811f350e2768848e6f46b6e659a2359975
Patrick <patrick.rodgers@microsoft.com>
2016-01-17 11:06:21 -0500

    Added support for client session storage ($pnp.session), updated core.getCurrentUserInfo to use session storage to avoid cross user conflicts on the same machine

--- Samples/Core.JavaScript/Core.JavaScript.CDN/js/pnp-clientcache.js
+++ Samples/Core.JavaScript/Core.JavaScript.CDN/js/pnp-clientcache.js
@@ -12,6 +12,18 @@
         }
     }
 
+    // after the modernizr test
+    function sessionTest() {
+        var str = 'test';
+        try {
+            sessionStorage.setItem(str, str);
+            sessionStorage.removeItem(str);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
     $.extend(window.officepnp, {
 
         // adds the client cache capability
@@ -78,7 +90,7 @@
 
                 if (o == null) {
                     o = getter();
-                    this.add(key, o);                    
+                    this.add(key, o);
                 }
 
                 return o;
@@ -91,9 +103,78 @@
                     expiration = $pnp.core.dateAdd(new Date(), 'minute', $pnp.settings.localStorageDefaultTimeoutMinutes);
                 }
 
-                return  JSON.stringify({
+                return JSON.stringify({
                     value: o,
                     expiration: expiration
+                });
+            }
+        },
+
+        // adds the client session capability
+        session: {
+
+            // determine if we have local storage once
+            enabled: sessionTest(),
+
+            add: function (/*string*/ key, /*object*/ value) {
+
+                if (this.enabled) {
+                    sessionStorage.setItem(key, this._createPersistable(value));
+                }
+            },
+
+            // gets an item from the cache, checking the expiration and removing the object if it is expired
+            get: function (/*string*/ key) {
+
+                if (!this.enabled) {
+                    return null;
+                }
+
+                var o = sessionStorage.getItem(key);
+
+                if (o == null) {
+                    return o;
+                }
+
+                var persistable = JSON.parse(o);
+
+                return persistable.value;
+            },
+
+            // removes an item from local storage by key
+            remove: function (/*string*/ key) {
+
+                if (this.enabled) {
+                    sessionStorage.removeItem(key);
+                }
+            },
+
+            // gets an item from the cache or adds it using the supplied getter function
+            getOrAdd: function (/*string*/ key, /*function*/ getter) {
+
+                if (!this.enabled) {
+                    return getter();
+                }
+
+                if (!$.isFunction(getter)) {
+                    throw 'Function expected for parameter "getter".';
+                }
+
+                var o = this.get(key);
+
+                if (o == null) {
+                    o = getter();
+                    this.add(key, o);
+                }
+
+                return o;
+            },
+
+            // creates the persisted object using the value and the expiration, setting the default expiration if none is applied
+            _createPersistable: function (/*object*/ o) {
+
+                return JSON.stringify({
+                    value: o
                 });
             }
         }
--- Samples/Core.JavaScript/Core.JavaScript.CDN/js/pnp-settings.js
+++ Samples/Core.JavaScript/Core.JavaScript.CDN/js/pnp-settings.js
@@ -4,7 +4,7 @@
         officepnp: {
             settings: {
                 // REST URL used to load the configuration stored in SharePoint. This is expecting a list with title "config" having two columns "Title" and "Value"
-                configLoadUrl: "https://{FQDN to site collection}/_api/lists/getbytitle('config')/items?$select=Title,Value",
+                configLoadUrl: "https://318studios.sharepoint.com/sites/dev/_api/lists/getbytitle('config')/items?$select=Title,Value",
                 // 0 = Verbose, 1 = Info, 2 = Warning, 3 = Error (messages below this level will not be logged)
                 activeLoggingLevel: 0,
                 // if you want to use the azure telemetry logging module enter your instrumentationKey below
