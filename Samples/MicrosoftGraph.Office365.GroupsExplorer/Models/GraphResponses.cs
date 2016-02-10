c669ae5d18ce736203ae31ab6ac579585f91ff41
Paul Schaeflein <paul@schaeflein.net>
2016-02-10 09:11:43 -0600

    Renamed project folder

---
+++ Samples/MicrosoftGraph.Office365.GroupsExplorer/Models/GraphResponses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace O365Groups.Models
+{
+	public class GetGroupsResponse
+	{
+		public Group[] value { get; set; }
+	}
+
+	public class GetConversationsResponse
+	{
+		public Conversation[] value { get; set; }
+	}
+
+	public class GetThreadsResponse
+	{
+		public ConversationThread[] value { get; set; }
+	}
+
+	public class GetPostsResponse
+	{
+		public Post[] value { get; set; }
+	}
+
+	public class GetEventsResponse
+	{
+		public Event[] value { get; set; }
+	}
+
+	public class GetFilesResponse
+	{
+		public DriveItem[] value { get; set; }
+	}
+}
