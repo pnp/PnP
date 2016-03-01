e7e75b6f050e94ca2c840040a9693f10103c076d
olemp <olemp@puzzlepart.com>
2016-03-01 11:26:24 +0100

    Initial structure for SharePoint. Provisioning will be merged from js-sites-core

---
+++ Libraries/Core.JavaScript/src/sharepoint/util.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+/// <reference path="..\..\typings\tsd.d.ts" />
+
+/**
+ * Retrieves the list ID of the current page from _spPageContextInfo
+ */
+export function getListId(): string {
+    return _spPageContextInfo.hasOwnProperty("pageListId") ? _spPageContextInfo.pageListId.substring(1,37) : "";
+}
