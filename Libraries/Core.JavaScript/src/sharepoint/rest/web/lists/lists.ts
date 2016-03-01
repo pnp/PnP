10469768963c4e58afbbabc55ef19379e0f1c479
olemp <olemp@puzzlepart.com>
2016-03-01 17:13:35 +0100

    Minor fix on Lists class (Web => Lists)

--- Libraries/Core.JavaScript/src/sharepoint/rest/web/lists/lists.ts
+++ Libraries/Core.JavaScript/src/sharepoint/rest/web/lists/lists.ts
@@ -1,6 +1,6 @@
 "use strict";
 
-/// <reference path="..\..\typings\main.d.ts" />
+/// <reference path="..\..\..\typings\main.d.ts" />
 
 /**
  * TODO
@@ -9,15 +9,6 @@ export class Lists {
     constructor() { }
 
     public query() {
-        var xmlhttp = new XMLHttpRequest();
-        var url = `${_spPageContextInfo.webAbsoluteUrl}/_api/web/lists`;
-        xmlhttp.setRequestHeader("Accept", "application/json;odata=verbose");
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                console.log(JSON.parse(xmlhttp.responseText));
-            }
-        };
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send();
+        
     }
 }
