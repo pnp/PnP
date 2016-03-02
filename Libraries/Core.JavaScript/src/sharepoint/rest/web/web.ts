66a75f27e76195e65ae4884fc81aa9a551e3250b
olemp <olemp@puzzlepart.com>
2016-03-02 16:48:47 +0100

    Another concat error ;)

--- Libraries/Core.JavaScript/src/sharepoint/rest/web/web.ts
+++ Libraries/Core.JavaScript/src/sharepoint/rest/web/web.ts
@@ -4,7 +4,7 @@ import { Queryable } from "../Queryable";
 import { Lists } from "./Lists/Lists";
 
 export class Web extends Queryable {
-    constructor(url: string) {
+    constructor(url: Array<string>) {
         super(url, "/web");
     }
     public lists() {
