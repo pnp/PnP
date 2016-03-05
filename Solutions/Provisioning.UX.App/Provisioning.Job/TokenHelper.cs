c1e3b569a3d84902f1bc1743479b43dea87381a3
brianmic <brianmic@live.com>
2016-03-05 00:08:55 -0500

    Provisioning Updates

--- Solutions/Provisioning.UX.App/Provisioning.Job/TokenHelper.cs
+++ Solutions/Provisioning.UX.App/Provisioning.Job/TokenHelper.cs
@@ -24,7 +24,7 @@ using AudienceUriValidationFailedException = Microsoft.IdentityModel.Tokens.Audi
 using SecurityTokenHandlerConfiguration = Microsoft.IdentityModel.Tokens.SecurityTokenHandlerConfiguration;
 using X509SigningCredentials = Microsoft.IdentityModel.SecurityTokenService.X509SigningCredentials;
 
-namespace Provisioning.Common
+namespace Provisioning.Job
 {
     public static class TokenHelper
     {
