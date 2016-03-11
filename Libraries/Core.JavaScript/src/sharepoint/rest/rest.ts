17368dc435cd2ec5f1cfdcff92348e2de8f880e4
olemp <olemp@puzzlepart.com>
2016-03-11 12:10:52 +0100

    Sending URL to web from Rest to Web. Queryable was 'hardcoded' to use web URL

--- Libraries/Core.JavaScript/src/sharepoint/rest/rest.ts
+++ Libraries/Core.JavaScript/src/sharepoint/rest/rest.ts
@@ -6,5 +6,5 @@ import { Web } from "./Web/Web";
  * Root of the SharePoint REST module
  */
 export class Rest {
-    public web = new Web(["/_api"]);
+    public web = new Web([_spPageContextInfo.webAbsoluteUrl, "/_api"]);
 }
