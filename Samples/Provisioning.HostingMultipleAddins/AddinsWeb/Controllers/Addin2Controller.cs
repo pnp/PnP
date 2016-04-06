2f1020f598c948cd6af45a8a5ede9fdb7c8d27cf
soniasuhag <soniasuhag@hotmail.com>
2016-04-06 16:14:02 +0530

    Added Provisioning.HostingMultipleAddins

---
+++ Samples/Provisioning.HostingMultipleAddins/AddinsWeb/Controllers/Addin2Controller.cs
@@ -0,0 +1,38 @@
+﻿using AddinsWeb;
+using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Addin1Web.Controllers
+{
+    public class Addin2Controller : Controller
+    {
+        // GET: Addin2
+        [SharePointContextFilter]
+        public ActionResult Index()
+        {
+            User spUser = null;
+
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
+
+            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            {
+                if (clientContext != null)
+                {
+                    spUser = clientContext.Web.CurrentUser;
+
+                    clientContext.Load(spUser, user => user.Title);
+
+                    clientContext.ExecuteQuery();
+
+                    ViewBag.UserName = spUser.Title;
+                }
+            }
+
+            return View();
+        }
+    }
+}
