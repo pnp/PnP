6283ea830746d64c8e37026da287fa5e488a3b22
Bert Jansen <bjansen@microsoft.com>
2016-04-11 15:44:37 +0200

    drop wrongly merged PR

--- Solutions/Governance.ChangeOwnership/Governance.ChangeOwnershipWeb/Scripts/csompeoplepickercontrol_resources.fi.js
+++
@@ -1,5 +0,0 @@
-﻿var resultsSingle = 'Näytetään {0} tulosta';
-var resultsTooMany = 'Näytetään {0} tulosta. Kokonaistulos {1} . <B>Käytä tarkempaa haku kriteeriä</B>';
-var resultsMany = 'Näytetään {0} tulosta';
-var resultsSearching = 'Etsitään...';
-var deleteUser = 'Poista henkilö tai ryhmä {0}';
