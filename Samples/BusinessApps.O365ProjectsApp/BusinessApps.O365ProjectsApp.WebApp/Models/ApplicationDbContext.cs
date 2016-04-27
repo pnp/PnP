c4f845e9148cf14bc15235f051da43457c317c48
Paolo Pialorsi <paolo@pialorsi.com>
2016-04-27 18:53:36 +0200

    BusinessApps.O365ProjectsApp first project release

---
+++ Samples/BusinessApps.O365ProjectsApp/BusinessApps.O365ProjectsApp.WebApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BusinessApps.O365ProjectsApp.WebApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserTokenCache> UserTokenCacheList { get; set; }
+    }
+
+    public class UserTokenCache
+    {
+        [Key]
+        public int UserTokenCacheId { get; set; }
+        public string webUserUniqueId { get; set; }
+        public byte[] cacheBits { get; set; }
+        public DateTime LastWrite { get; set; }
+    }
+}
---
+++ Samples/BusinessApps.O365ProjectsApp/BusinessApps.O365ProjectsApp.WebApp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace BusinessApps.O365ProjectsApp.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
