1a2a77300756fd14e0aecb767dc316f7086c6b8a
Paolo Pialorsi <paolo@pialorsi.com>
2016-06-29 10:15:22 +0200

    Updated book samples.

--- Samples/Office.Add-Ins/Outlook.AddInSample/Outlook.AddInSampleWeb/Scripts/_references.js
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="jquery-1.9.1.js" />
-/* Required to correctly initalize Office.js for intellisense */
-/// <reference path="_officeintellisense.js" />
-/* Use offline copy of Office.js for intellisense */
-// /// <reference path="office/1/outlook-win32.debug.js" />
-// /// <reference path="office/1/office.js" />
-/* Use online copy of Office.js for intellisense */
-/// <reference path="https://appsforoffice.microsoft.com/lib/1/hosted/outlook-win32.debug.js" />
-/// <reference path="https://appsforoffice.microsoft.com/lib/1/hosted/office.js" />
