d6fb4845325b32a6d401edd97a052cca28bc1227
Simon-Pierre Plante <sp.plante@gmail.com>
2016-09-05 22:43:46 -0400

    Added the Provisioning.PnPDeployer.Console folder within the samples. A small console application for deploying PnP XML templates and open xml packages through a userfriendly sequences configuration file.

---
+++ Samples/Provisioning.PnPDeployer.Console/PNP.Deployer.ExtensibilityProviders.SSOM/Classes/ExtensibilityProviders/SiteFieldsProvider.cs
@@ -0,0 +1,117 @@
+﻿using NLog;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using OfficeDevPnP.Core.Diagnostics;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Framework.Provisioning.Extensibility;
+using OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers;
+using OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Client;
+using PNP.Deployer.Common;
+
+
+
+// =======================================================
+/// <author>
+/// Simon-Pierre Plante (sp.plante@gmail.com)
+/// </author>
+// =======================================================
+namespace PNP.Deployer.ExtensibilityProviders.SSOM
+{
+    public class SiteFieldsProvider : IProvisioningExtensibilityHandler
+    {
+        #region Constants
+
+        private const string ERROR_GENERAL = "An error occured while executing the SiteFieldsProvider : {0}";
+
+        #endregion
+
+
+        #region Private Members
+
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        #endregion
+
+        
+        #region Interface Methods
+
+        public void Provision(ClientContext ctx, ProvisioningTemplate template, ProvisioningTemplateApplyingInformation applyingInformation, TokenParser tokenParser, PnPMonitoredScope scope, string configurationData)
+        {
+            logger.Info("Entering the SiteFields provider");
+
+            try
+            {
+                // ----------------------------------------------
+                // Deserializes the configuration data
+                // ----------------------------------------------
+                SiteFieldsConfigurationData configData = XmlUtility.DeserializeXml<SiteFieldsConfigurationData>(configurationData);
+
+                // ----------------------------------------------
+                // Loads the site collection and root web
+                // ----------------------------------------------
+                using (SPSite site = new SPSite(ctx.Url))
+                {
+                    using (SPWeb web = site.OpenWeb())
+                    {
+                        // ----------------------------------------------
+                        // Loops through the <Fields> nodes
+                        // ----------------------------------------------
+                        foreach (Field configField in configData.Fields)
+                        {
+                            SPField field = web.Fields.GetFieldByInternalName(configField.Name);
+
+                            // ----------------------------------------------
+                            // Configures the field's title resource
+                            // ----------------------------------------------
+                            ConfigureFieldTitleResource(field, configField);
+                        }
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception(String.Format(ERROR_GENERAL, e.Message), e.InnerException);
+            }
+        }
+
+
+        public ProvisioningTemplate Extract(ClientContext ctx, ProvisioningTemplate template, ProvisioningTemplateCreationInformation creationInformation, PnPMonitoredScope scope, string configurationData)
+        {
+            return template;
+        }
+
+
+        public IEnumerable<TokenDefinition> GetTokens(ClientContext ctx, ProvisioningTemplate template, string configurationData)
+        {
+            return new List<TokenDefinition>();
+        }
+
+        #endregion
+
+
+        #region Private Methods
+
+        // ===========================================================================================================
+        /// <summary>
+        /// Configures the TitleResource of the given <b>SPField</b> based on the specified <b>Field</b> config
+        /// </summary>
+        /// <param name="field">The <b>SPField</b> that needs to be configured</param>
+        /// <param name="configField">The <b>Field</b> config</param>
+        // ===========================================================================================================
+        private void ConfigureFieldTitleResource(SPField field, Field configField)
+        {
+            logger.Info("Configuring title resources for field '{0}'", field.InternalName);
+
+            foreach(TitleResource titleResource in configField.TitleResources)
+            {
+                field.TitleResource.SetValueForUICulture(new CultureInfo(titleResource.LCID), titleResource.Value);
+            }
+            field.Update();
+        }
+
+        #endregion
+    }
+}
---
+++ Samples/Provisioning.PnPDeployer.Console/PNP.Deployer/Classes/Helpers/FilesUtility.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+
+
+namespace PNP.Deployer
+{
+    // =======================================================
+    /// <author>
+    /// Simon-Pierre Plante (sp.plante@gmail.com)
+    /// </author>
+    // =======================================================
+    public static class FilesUtility
+    {
+        #region Private Methods
+
+        // ===========================================================================================================
+        /// <summary>
+        /// Copies recursively all files/folders from the specified source folder to the specified target folder
+        /// </summary>
+        /// <param name="source">The DirectoryInfo of the source folder</param>
+        /// <param name="target">The DirectoryInfo of the target folder</param>
+        // ===========================================================================================================
+        private static void CopyDirectoryRecursive(DirectoryInfo source, DirectoryInfo target)
+        {
+            // --------------------------------------------------
+            // Creates the destination folder if needed
+            // --------------------------------------------------
+            if (!target.Exists)
+                Directory.CreateDirectory(target.FullName);
+
+            // --------------------------------------------------
+            // Copies each file in the destination folder
+            // --------------------------------------------------
+            foreach(FileInfo fileInfo in source.GetFiles())
+            {
+                fileInfo.CopyTo(Path.Combine(target.FullName, fileInfo.Name), true);
+            }
+
+            // --------------------------------------------------
+            // Copies each folder in the destination folder
+            // --------------------------------------------------
+            foreach (DirectoryInfo sourceSubDirInfo in source.GetDirectories())
+            {
+                DirectoryInfo targetSubDirInfo = target.CreateSubdirectory(sourceSubDirInfo.Name);
+                CopyDirectoryRecursive(sourceSubDirInfo, targetSubDirInfo);
+            }
+        }
+
+        #endregion
+
+
+        #region Public Methods
+
+        // ===========================================================================================================
+        /// <summary>
+        /// Copies recursively all files/folders from the specified source folder to the specified target folder
+        /// </summary>
+        /// <param name="sourceDirectory">The source directory that needs to be copied</param>
+        /// <param name="destinationDirectory">The target directory </param>
+        // ===========================================================================================================
+        public static void CopyDirectory(string sourceDirectory, string destinationDirectory)
+        {
+            DirectoryInfo infoSourceDirectory = new DirectoryInfo(sourceDirectory);
+            DirectoryInfo infoDestinationDirectory = new DirectoryInfo(destinationDirectory);
+            CopyDirectoryRecursive(infoSourceDirectory, infoDestinationDirectory);
+        }
+
+        #endregion
+    }
+}
