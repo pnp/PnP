215ee59bbc213acf85eed612a06b3199f5c30644
Vesa Juvonen <vesa.juvonen@hotmail.com>
2016-09-07 21:25:12 +0300

    Updated sample to work with modern site contents page.

--- Samples/Provisioning.SubSiteCreationApp/Provisioning.SubSiteCreationAppWeb/Resources/CustomInjectedJS.js
+++ Samples/Provisioning.SubSiteCreationApp/Provisioning.SubSiteCreationAppWeb/Resources/CustomInjectedJS.js
@@ -1,34 +1,91 @@
-﻿// Register script for MDS if possible
-RegisterModuleInit("CustomInjectedJS.js", SubSiteOverride_Inject); //MDS registration
-SubSiteOverride_Inject(); //non MDS run
+﻿var jQueryScriptUrl = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.2.min.js";
 
-// Actual execution
-function SubSiteOverride_Inject() {
+// Is MDS enabled?
+if ("undefined" != typeof g_MinimalDownload && g_MinimalDownload && (window.location.pathname.toLowerCase()).endsWith("/_layouts/15/start.aspx") && "undefined" != typeof asyncDeltaManager) {
+    // Register script for MDS if possible
+    RegisterModuleInit("PnP-Partner-Pack-Overrides.js", JavaScript_Embed); //MDS registration
+    JavaScript_Embed(); //non MDS run
+} else {
+    JavaScript_Embed();
+}
 
-    // Run injection only for site content
-    if ((window.location.href.toLowerCase().indexOf("viewlsts.aspx") > -1 && window.location.href.toLowerCase().indexOf("_layouts/15") > -1))
-    {
-        _spBodyOnLoadFunctionNames.push('SubSiteOverride_OverrideLinkToAppUrl');
+function JavaScript_Embed() {
+
+    loadScript(jQueryScriptUrl, function () {
+        $(document).ready(function () {
+            // SP.SOD.executeOrDelayUntilScriptLoaded(function () { ReplaceLinks(); }, 'sp.js');
+            SP.SOD.executeFunc('sp.js', 'SP.ClientContext', replaceLinks);
+        });
+    });
+}
+
+function replaceLinks() {
+
+    // Customize the newsbweb.aspx page, as soon as possible
+    if (IsOnPage("newsbweb.aspx")) {
+        // Clean the current page as soon as possible
+        $("body").removeClass("ms-backgroundImage");
+        $("body").empty();
+    }
+
+    var ctx = SP.ClientContext.get_current();
+    var web = ctx.get_web();
+    ctx.load(web, 'Url');
+
+    ctx.executeQueryAsync(
+        function () { // on success
+
+            var currentScriptUrl = $('#PnPPartnerPackOverrides').attr('src');
+            addInSiteUrl = "https://localhost:44339";
+            var hostWebUrl = web.get_url();
+
+            // Customize the newsbweb.aspx page, as soon as possible
+            if (IsOnPage("newsbweb.aspx")) {
+                // Redirect to the custom "New Sub Site" page URL
+                location.replace(addInSiteUrl + "/pages/default.aspx?SPHostUrl=" + encodeURIComponent(hostWebUrl));
+            }
+
+            // Customize the viewlsts.aspx page
+            if (IsOnPage("viewlsts.aspx")) {
+                // Replace the "New Sub Site" link
+                $("#createnewsite").attr("href", addInSiteUrl + "/pages/default.aspx?SPHostUrl=" + encodeURIComponent(hostWebUrl));
+            }
+        },
+        function (s, e) { // on failure
+            console.error(e.get_message());
+        }
+    );
+}
+
+function IsOnPage(pageName) {
+    if (window.location.href.toLowerCase().indexOf(pageName.toLowerCase()) > -1) {
+        return true;
+    } else {
+        return false;
     }
 }
 
-// Actual link override. Checking the right URL from root site collection of the tenant/web application
-function SubSiteOverride_OverrideLinkToAppUrl() {
+function loadScript(url, callback) {
+    var head = document.getElementsByTagName("head")[0];
+    var script = document.createElement("script");
+    script.src = url;
 
-    //Update create new site link point to our custom page.
-    var link = document.getElementById('createnewsite');
-    var url = "https://localhost:44339/pages/default.aspx?SPHostUrl=" + encodeURIComponent(_spPageContextInfo.webAbsoluteUrl);
-    if (link != undefined) {
-        // Could be get from SPSite root web property bag - now hard coded for demo purposes
-        link.href = url;
-    }
+    // Attach handlers for all browsers
+    var done = false;
+    script.onload = script.onreadystatechange = function () {
+        if (!done && (!this.readyState
+					|| this.readyState == "loaded"
+					|| this.readyState == "complete")) {
+            done = true;
 
-}
+            // Continue your code
+            callback();
 
+            // Handle memory leak in IE
+            script.onload = script.onreadystatechange = null;
+            head.removeChild(script);
+        }
+    };
 
-if (typeof (Sys) != "undefined" && Boolean(Sys) && Boolean(Sys.Application)) {
-  Sys.Application.notifyScriptLoaded();
-}
-if (typeof (NotifyScriptLoadedAndExecuteWaitingJobs) == "function") {
-    NotifyScriptLoadedAndExecuteWaitingJobs("CustomInjectedJS.js");
+    head.appendChild(script);
 }
