60019dc193d6d9d64c5f772ec22d63206efa5f64
Charles Mullins <charlmu@hotmail.com>
2016-10-16 18:37:51 -0400

    Commit example including knockout, accordion, and REST

---
+++ Samples/SharePoint.RESTAPI/SharePoint.RESTAPI.SPApp/Scripts/accordion/datamodels/slideModel.js
@@ -0,0 +1,17 @@
+﻿AccordionContent.SlideModel = function () {
+    var AccordionContentSlide = function () {
+        var SlideItem = function (slideWhat, slideDescription, slideWho) {
+            this.slideWhat = slideWhat;
+            this.slideDescription = slideDescription;
+            this.slideWho = slideWho;
+        };
+
+        return {
+            SlideItem: SlideItem
+        }
+    }();
+
+    return {
+        AccordionContentSlide: AccordionContentSlide
+    }
+}();
---
+++ Samples/SharePoint.RESTAPI/SharePoint.RESTAPI.SPApp/Scripts/accordion/helpers/browserRequirements.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    var supportsMediaQuery = Modernizr.mediaqueries;
+    var supportsBeforeAfter = Modernizr.generatedcontent;
+    var supportsLocalStorage = Modernizr.localstorage;
+
+    if (!(supportsMediaQuery && supportsBeforeAfter && supportsLocalStorage)) {
+        var unsupportedDiv = $('#unsupported');
+        unsupportedDiv.show();
+        unsupportedDiv.click(function () {
+            unsupportedDiv.hide();
+        });
+    }
+});
