59f9fe68b6e2d054e8c28814955751dfa40fa00b
Simon-Pierre Plante <sp.plante@gmail.com>
2016-11-03 03:39:01 -0400

    Provisioning.pn p deployer.console (#1552)
    * Added a configurable parameter that allows the deployer to ignore folders in order to speed up the tokenizing process. Updated the token file XSD for a more consistent XML. Fixed a bug related to tokens loaded from the App.config file. Removed the project build events.
    
    * Update FieldsExtensibilityProvider.md
    
    * Update README.md
    
    * Added the custom Required attribute to the FieldsExtensibilityProvider serializable classes.
    
    * Added the ignored extensions functionnality
    
    Added the ignored extensions functionnality in order to be able to ignore specific file extensions for the tokenizing process. This makes sure that files such as images, videos, pnp packages etc aren't corrupted by the tokenizer opening them and saving them in a corrupted format.
    
    * Added the ignored extensions functionnality
    
    Added the ignored extensions functionality in order to be able to ignore specific file extensions for the tokenizing process. This makes sure that files such as images, videos, pnp packages etc aren't corrupted by the tokenizer opening them and saving them in a corrupted format.
    
    * Added the ignored extensions functionnality
    
    Added the ignored extensions functionnality in order to be able to ignore specific file extensions for the tokenizing process. This makes sure that files such as images, videos, pnp packages etc aren't corrupted by the tokenizer opening them and saving them in a corrupted format.
    
    * Removed the previously added ignored extensions parameter
    
    Removed the previously added ignored extensions parameter and replaced it by a more dynamic way of handling the problem.
    
    * Removed the previously added ignored extensions parameter
    
    Removed the previously added ignored extensions parameter and replaced it by a more dynamic way of handling the problem.
    
    * Removed the previously added ignored extensions parameter
    
    Removed the previously added ignored extensions parameter and replaced it by a more dynamic way of handling the problem.

--- Samples/Provisioning.PnPDeployer.Console/PNP.Deployer/Classes/Helpers/FilesUtility.cs
+++ Samples/Provisioning.PnPDeployer.Console/PNP.Deployer/Classes/Helpers/FilesUtility.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 
 namespace PNP.Deployer
@@ -18,30 +20,39 @@ namespace PNP.Deployer
         /// </summary>
         /// <param name="source">The DirectoryInfo of the source folder</param>
         /// <param name="target">The DirectoryInfo of the target folder</param>
+        /// <param name="ignoredFolders">An optional list of folder names which needs to be ignored</param>
         // ===========================================================================================================
-        private static void CopyDirectoryRecursive(DirectoryInfo source, DirectoryInfo target)
+        private static void CopyDirectoryRecursive(DirectoryInfo source, DirectoryInfo target, List<string> ignoredFolders = null)
         {
-            // --------------------------------------------------
-            // Creates the destination folder if needed
-            // --------------------------------------------------
-            if (!target.Exists)
-                Directory.CreateDirectory(target.FullName);
+            bool ignoreFolder = (ignoredFolders != null && ignoredFolders.FirstOrDefault(x => source.FullName.ToLower() == x.ToLower()) != null);
 
             // --------------------------------------------------
-            // Copies each file in the destination folder
+            // If the current folder name isn't ignored
             // --------------------------------------------------
-            foreach(FileInfo fileInfo in source.GetFiles())
+            if(!ignoreFolder)
             {
-                fileInfo.CopyTo(Path.Combine(target.FullName, fileInfo.Name), true);
-            }
+                // --------------------------------------------------
+                // Creates the destination folder if needed
+                // --------------------------------------------------
+                if (!target.Exists)
+                    Directory.CreateDirectory(target.FullName);
 
-            // --------------------------------------------------
-            // Copies each folder in the destination folder
-            // --------------------------------------------------
-            foreach (DirectoryInfo sourceSubDirInfo in source.GetDirectories())
-            {
-                DirectoryInfo targetSubDirInfo = target.CreateSubdirectory(sourceSubDirInfo.Name);
-                CopyDirectoryRecursive(sourceSubDirInfo, targetSubDirInfo);
+                // --------------------------------------------------
+                // Copies each file in the destination folder
+                // --------------------------------------------------
+                foreach (FileInfo fileInfo in source.GetFiles())
+                {
+                    fileInfo.CopyTo(Path.Combine(target.FullName, fileInfo.Name), true);
+                }
+
+                // --------------------------------------------------
+                // Copies each folder in the destination folder
+                // --------------------------------------------------
+                foreach (DirectoryInfo sourceSubDirInfo in source.GetDirectories())
+                {
+                    DirectoryInfo targetSubDirInfo = new DirectoryInfo(Path.Combine(target.FullName, sourceSubDirInfo.Name));
+                    CopyDirectoryRecursive(sourceSubDirInfo, targetSubDirInfo, ignoredFolders);
+                }
             }
         }
 
@@ -56,12 +67,13 @@ namespace PNP.Deployer
         /// </summary>
         /// <param name="sourceDirectory">The source directory that needs to be copied</param>
         /// <param name="destinationDirectory">The target directory </param>
+        /// <param name="ignoredFolders">An optional list of folder names which needs to be ignored</param>
         // ===========================================================================================================
-        public static void CopyDirectory(string sourceDirectory, string destinationDirectory)
+        public static void CopyDirectory(string sourceDirectory, string destinationDirectory, List<string> ignoredFolders = null)
         {
             DirectoryInfo infoSourceDirectory = new DirectoryInfo(sourceDirectory);
             DirectoryInfo infoDestinationDirectory = new DirectoryInfo(destinationDirectory);
-            CopyDirectoryRecursive(infoSourceDirectory, infoDestinationDirectory);
+            CopyDirectoryRecursive(infoSourceDirectory, infoDestinationDirectory, ignoredFolders);
         }
 
         #endregion
