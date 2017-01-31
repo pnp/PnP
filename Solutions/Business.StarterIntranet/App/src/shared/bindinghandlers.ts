560fbe5f3b44890bbb64490b9a5f426df6872666
Franck Cornu <franck.cornu@cgi.com>
2017-01-31 16:13:39 -0500

    Reset the folder

--- Solutions/Business.StarterIntranet/App/src/shared/bindinghandlers.ts
+++
@@ -1,9 +0,0 @@
-interface KnockoutBindingHandlers {
-    formatDateField: KnockoutBindingHandler;
-    stopBinding: KnockoutBindingHandler;
-    summarize: KnockoutBindingHandler;
-    localizedTermLabel: KnockoutBindingHandler;
-    getNewsSearchUrl: KnockoutBindingHandler;
-    getDocumentsSearchUrl: KnockoutBindingHandler;
-    inputFocus: KnockoutBindingHandler;
-}
