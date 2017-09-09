0b1a1bfa0a92527bce68214d56dd50946993f501
Franck Cornu <franck.cornu@outlook.com>
2017-09-09 10:41:45 -0400

    PnP Starter Intranet 2.0 - New folder (Business.SharePointIntranet) (#1697)
    * Removed previous code
    
    * Added the code for the v2.0 of the PnP Starter Intranet Solution
    
    * Updated README
    
    * Fix button alignement
    
    * PnP Starter Intranet v2.0 release
    
    * Deleted old folder "Business.StarterIntranet" and created a new one for the version 2.0.0
    
    * Reverted back to the original folder name

--- Solutions/Business.StarterIntranet/app/webpack.config.js
+++ Solutions/Business.StarterIntranet/app/webpack.config.js
@@ -1,6 +1,5 @@
 var webpack = require('webpack');
 const path = require('path');
-const validate = require('webpack-validator');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 // CSS files output
@@ -11,26 +10,32 @@ var layoutsCssEditExtractTextPlugin = new ExtractTextPlugin("layouts-edit.css");
 var portalCssExtractTextPlugin = new ExtractTextPlugin("portal.css");
 var bootstrapCssExtractTextPlugin = new ExtractTextPlugin("bootstrap-iso.css");
 
-// Wrap the whole Webpack config with a validator to avoid typos mistakes
-module.exports = validate({
-    
+const config = {
+
     entry: {
         app: "./main.ts", // The main entry point for the application.
 
         // Note that 'es6-promise' and 'whatwg-fetch' are necessary to get pnp work in IE
         // More info here https://github.com/OfficeDev/PnP-JS-Core/wiki/Install-and-Use
         vendor: [
-                "jquery",
-                "knockout",
-                "knockout-mapping",
-                "office-ui-fabric/dist/css/fabric.css",
-                "office-ui-fabric/dist/css/fabric.components.css",
-                "bootstrap/dist/js/bootstrap.js",
-                "es6-promise",
-                "moment",
-                "whatwg-fetch"]
+            "jquery",
+            "jquery-ui",
+            "knockout",
+            "knockout-mapping",
+            "bootstrap/dist/js/bootstrap.min.js",
+            "whatwg-fetch",
+            "es6-promise",
+            "lodash",
+            "react",
+            "react-dom",
+            "sp-pnp-js",
+            "office-ui-fabric-react/lib/Panel",
+            "office-ui-fabric-react/lib/Link",
+            "office-ui-fabric-react/dist/css/fabric.min.css",
+            "flickity/dist/flickity.min.css"
+        ]
     },
-    
+
     output: {
         filename: "js/app.js",
         path: path.join(__dirname, "dist"),
@@ -39,7 +44,11 @@ module.exports = validate({
         // We need this to be able to apply Knockout JS bindings manually for SharePoint display templates (the 'ko' variable is not exposed in the global context)
         library: ['Intranet']
     },
-    
+
+    node: {
+        fs: "empty" // For the iCal feature (an error is raised otherwise)
+    },
+
     // Context for entry point
     context: path.join(__dirname, "src"), 
 
@@ -48,90 +57,195 @@ module.exports = validate({
     // Be careful, source maps are sometimes not recognized correctly by Google Chrome (use Google Chrome Canary if you encounter some troubles)
     devtool: "cheap-module-source-map",
     
-    resolve: {    
-               
-        extensions: ["", ".webpack.js", ".web.js", ".js",".ts"],         
-    },
-    
-    module: {
+    resolve: {                   
+        extensions: [".webpack.js", ".web.js", ".js",".ts",".tsx"],
 
-        loaders: [
+        alias: {
+            // Resolve the jQueryUi plugin manually
+            'jquery-ui': 'jquery-ui-dist/jquery-ui.min.js',
+        },            
+    },
+
+    module: {
+        rules: [
+            {
+                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "source-map-loader",
+                exclude: [
+                    /node_modules/,
+                ] 
+            },
             {
                 // We use the text loader to get the raw HTML markup for a Knockout component template file
                 test: /\.html$/,
-                loader: "text"
+                use: 'text-loader'
             },
             {
                 // Ouput images files directly into the 'dist' folder without modifications. They will be used by the CSS stylesheets.
                 test: /\.(jpg|png|gif|ico)$/,
-                loader: 'file?name=./img/[name].[ext]',
+                use: [
+                    {
+                        loader: 'file-loader',
+                        query : {
+                            name: "./img/[name].[ext]"
+                        }
+                    }
+                ],
+            },
+            {
+                // Loader for TypeScript files (.ts)
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        query : {
+                            silent: true // To be able to run the webpack-bundle-size-analyzer without errors
+                        }
+                    }
+                ],
+
+                
+            },
+            {
+                // CSS loader for the Office UI fabric styles
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                // Custom fonts (Myriad Pro)
+                test: /\.woff?$|\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        query : {
+                            name: "./fonts/[name].[ext]"
+                        }
+                    }
+                ],
+               
             },
             {
                 // Sass Loader. Note that we use the resolve-url loader to insert relative path to images into style sheets
                 test: /\.scss$/,
-                loader: portalCssExtractTextPlugin.extract('style', 'css!resolve-url!sass?sourceMap'),
+                use: portalCssExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                            },
+                            {
+                                loader: 'resolve-url-loader'
+                            },
+                            {
+                                loader: "sass-loader",
+                                options: {
+                                    sourceMap: true
+                                }
+                            }
+                        ],
+                }),
                 exclude: /(layouts|layouts-edit)\.scss$/
             },
             {
                 // Page layouts styles (display and edit) are bundled separately. The loading is controlled by the page layout itself. 
                 test: /layouts\.scss$/,
-                loader: layoutsCssExtractTextPlugin.extract('style', 'css!sass?sourceMap')
+                use: layoutsCssExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                            },
+                            {
+                                loader: "sass-loader",
+                                options: {
+                                    sourceMap: true
+                                }
+                            }
+                        ],
+                }),
             },  
             {
                 test: /layouts-edit\.scss$/,
-                loader: layoutsCssEditExtractTextPlugin.extract('style', 'css!sass?sourceMap')
-            },        
+                use: layoutsCssEditExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                            },
+                            {
+                                loader: "sass-loader",
+                                options: {
+                                    sourceMap: true
+                                }
+                            }
+                        ],
+                }),
+            },  
             {
                 // Isolate the Bootstrap CSS to avoid conflicts with the SharePoint default CSS
                 // More info here: https://formden.com/blog/isolate-bootstrap
                 // We used a customized CSS version of Bootstrap (http://getbootstrap.com/customize/) because we don't need the full package
                 test: /\.less$/,
-                loader: bootstrapCssExtractTextPlugin.extract('style', 'css?sourceMap!string-replace?{multiple:[{search:"\.bootstrap-iso body",replace:".bootstrap-iso"},{ search:"\.bootstrap-iso html",replace:".bootstrap-iso" }]}!less'),
-            },
-            { 
-                // CSS loader for the Office UI fabric styles
-                test: /\.css$/,
-                loader: "style!css"
-            },
-            {  
-                // Loader for TypeScript files (.ts)
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                loader: 'ts-loader' 
-            },  
-            {   
-                // Used for resources (en-EN & fr-FR)
-                test: /\.json$/,
-                loader: 'json-loader' 
-            }  
-        ],
-
-        preLoaders: [
-            
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
-                 test: /\.js$/, 
-                 loader: "source-map-loader" 
-            }
-        ],
+                use: bootstrapCssExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            },
+                            {
+                                loader: "string-replace-loader",
+                                query: {
+                                    multiple: [
+                                        { search: '\.bootstrap-iso body', replace: '.bootstrap-iso' },
+                                        { search: '\.bootstrap-iso html', replace: 'bootstrap-iso' }
+                                    ]
+                                }
+                            },
+                            {
+                                loader: 'less-loader'
+                            },                            
+                        ],
+                }),                
+            }, 
+        ]
     },
-        
-    plugins: [
-                // Every time webpack encounters $, jQuery, window.jQuery or ko, it will replace it by the correct library dependency
-                // Especially useful for the default bootstrap.min.js file
-                new webpack.ProvidePlugin({
-                    $: 'jquery',
-                    jQuery: 'jquery',
-                    "window.jQuery" : 'jquery',
-                    ko : 'knockout'
-                }),
-                                
-                portalCssExtractTextPlugin,
-                bootstrapCssExtractTextPlugin,
-                layoutsCssExtractTextPlugin,
-                layoutsCssEditExtractTextPlugin,
 
-                // Split the application into chunks
-                new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"js/vendor.js")
-            ]
-});
+    plugins: [
+
+        // Every time webpack encounters $, jQuery, window.jQuery or ko, it will replace it by the correct library dependency
+        // Especially useful for the default bootstrap.min.js file
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            "window.jQuery" : 'jquery',
+            ko : 'knockout',
+        }),
+
+        portalCssExtractTextPlugin,
+        bootstrapCssExtractTextPlugin,
+        layoutsCssExtractTextPlugin,
+        layoutsCssEditExtractTextPlugin,
+
+        // Split the application into chunks
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'js/vendor.js',
+        }),
+
+        // Load only the needed locales to reduce the size of the bundle
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en|fr/),
+
+        // To resolve the dynamic require in the builder.js from in the ical-toolkit module
+        new webpack.ContextReplacementPlugin(/ical-toolkit/, /..\/timezones\/database\/america-montreal\.json/)
+    ]
+};
+
+module.exports = config;
