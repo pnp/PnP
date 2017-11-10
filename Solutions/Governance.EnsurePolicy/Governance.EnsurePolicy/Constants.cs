f202e10f4526fb104027ad3d9f3c2a4c41078806
Bert Jansen <bjansen@microsoft.com>
2017-11-10 11:05:51 +0100

    New solution: Governance.EnsurePolicy

---
+++ Solutions/Governance.EnsurePolicy/Governance.EnsurePolicy/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Governance.EnsurePolicy
+{
+    public static class Constants
+    {
+        // App config settings
+        internal const string AppSettings_AppId = "AppId";
+        internal const string AppSettings_AzureTenant = "AzureTenant";
+        internal const string AppSettings_PfxCertificate = "PfxCertificate";
+        internal const string AppSettings_PfxCertificatePassword = "PfxCertificatePassword";
+        internal const string AppSettings_TenantAdmin = "TenantAdmin";
+        internal const string AppSettings_ExcludeOD4BSites = "ExcludeOD4BSites";
+        internal const string AppSettings_NumberOfThreads = "NumberOfThreads";
+        internal const string AppSettings_SiteFilters = "SiteFilters";
+    }
+}
---
+++ Solutions/Governance.EnsurePolicy/Governance.EnsurePolicy/PolicyJob.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers;
+using OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml;
+using OfficeDevPnP.Core.Framework.TimerJobs;
+using System;
+
+namespace Governance.EnsurePolicy
+{
+    public class PolicyJob: TimerJob
+    {
+
+        public string ProvisioningTemplateToApply { get; set; }
+
+        public PolicyJob(): base("PolicyJob", "1.0")
+        {
+            TimerJobRun += PolicyJob_TimerJobRun;
+        }
+
+        private void PolicyJob_TimerJobRun(object sender, TimerJobRunEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine($"Processing site {e.Url}");
+
+                ProvisioningTemplateApplyingInformation ptai = new ProvisioningTemplateApplyingInformation()
+                {
+                    HandlersToProcess = Handlers.SiteSecurity,
+                };
+
+                XMLTemplateProvider provider = new XMLFileSystemTemplateProvider(@".", "");
+                ProvisioningTemplate sourceTemplate = provider.GetTemplate(ProvisioningTemplateToApply);
+
+                e.WebClientContext.Web.ApplyProvisioningTemplate(sourceTemplate, ptai);
+            }
+            catch(Exception ex)
+            {
+                // Catch exceptions to avoid ending the program run since we're typically processing multiple site collections
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
