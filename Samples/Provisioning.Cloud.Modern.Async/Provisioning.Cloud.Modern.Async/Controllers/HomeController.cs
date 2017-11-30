c6a441347949a9ff9d91c3e77e2fae0dcf5cfbe1
paolo@pialorsi.com <paolo@pialorsi.com>
2017-12-01 00:05:30 +0100

    Renamed projects, fixed webjob, removed site classification

---
+++ Samples/Provisioning.Cloud.Modern.Async/Provisioning.Cloud.Modern.Async/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using Provisioning.Cloud.Modern.Async.Components;
+using Provisioning.Cloud.Modern.Async.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Provisioning.Cloud.Modern.Async.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Just fire the OAuth access token request
+            var token = MicrosoftGraphHelper.GetAccessTokenForCurrentUser();
+
+            var model = new IndexViewModel();
+            if (System.Security.Claims.ClaimsPrincipal.Current != null && System.Security.Claims.ClaimsPrincipal.Current.Identity != null && System.Security.Claims.ClaimsPrincipal.Current.Identity.IsAuthenticated)
+            {
+                model.CurrentUserPrincipalName = System.Security.Claims.ClaimsPrincipal.Current.Identity.Name;
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IndexViewModel model)
+        {
+            AntiForgery.Validate();
+            if (ModelState.IsValid)
+            {
+                // Set the current SPO Admin Site URL
+                model.SPORootSiteUrl = ConfigurationManager.AppSettings["SPORootSiteUrl"];
+
+                // Set the current user's access token
+                model.UserAccessToken = MicrosoftGraphHelper.GetAccessTokenForCurrentUser(model.SPORootSiteUrl);
+
+                // Get the JSON site creation information
+                String modernSiteCreation = JsonConvert.SerializeObject(model);
+
+                String targetQueue = String.Empty;
+
+                // Determine the target asynchronous creation technique
+                switch (model.AsyncTech)
+                {
+                    case AsynchronousTechnique.AzureFunction:
+                        targetQueue = ConfigurationManager.AppSettings["AzureFunctionQueue"];
+                        break;
+                    case AsynchronousTechnique.AzureWebJob:
+                    default:
+                        targetQueue = ConfigurationManager.AppSettings["AzureWebJobQueue"];
+                        break;
+                }
+
+                // Get the storage account for Azure Storage Queue
+                CloudStorageAccount storageAccount =
+                    CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageQueue"].ConnectionString);
+
+                // Get queue ... and create if it does not exist
+                CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+                CloudQueue queue = queueClient.GetQueueReference(targetQueue);
+                queue.CreateIfNotExists();
+
+                // Add entry to queue
+                queue.AddMessage(new CloudQueueMessage(modernSiteCreation));
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Samples/Provisioning.Cloud.Modern.Async/Provisioning.Cloud.Modern.Async/Models/IndexViewModel.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Provisioning.Cloud.Modern.Async.Models
+{
+    public class IndexViewModel
+    {
+        public String CurrentUserPrincipalName { get; set; }
+
+        [Required(ErrorMessage = "Please select a \"modern\" site type")]
+        [DisplayName("Site type")]
+        [UIHint("SiteType")]
+        public SiteType SiteType { get; set; }
+
+        [Required(ErrorMessage = "Please provide  a title for the \"modern\" site")]
+        [DisplayName("Site Title")]
+        public String SiteTitle { get; set; }
+
+        [Required(ErrorMessage = "Please provide an alias for the \"modern\" site")]
+        [DisplayName("Site Alias")]
+        public String SiteAlias { get; set; }
+
+        [DisplayName("Site Description")]
+        public String SiteDescription { get; set; }
+
+        [Required(ErrorMessage = "Please choose if the \"modern\" site will be Public or Private")]
+        [DisplayName("Privacy Settings")]
+        [UIHint("PrivacySettings")]
+        public Boolean IsPublic { get; set; }
+
+        [JsonIgnore]
+        public List<String> Classifications { get; set; }
+
+        [DisplayName("Site Creation Technique")]
+        [UIHint("AsynchronousTechnique")]
+        [JsonIgnore]
+        public AsynchronousTechnique AsyncTech { get; set; }
+
+        [DisplayName("PnP Provisioning Template")]
+        [UIHint("PnPTemplate")]
+        public String PnPTemplate { get; set; }
+
+        public String UserAccessToken { get; set; }
+
+        public String SPORootSiteUrl { get; set; }
+    }
+
+    /// <summary>
+    /// Defines the available "modern" site options
+    /// </summary>
+    public enum SiteType
+    {
+        /// <summary>
+        /// "modern" team site
+        /// </summary>
+        [Display(Name = "Team Site")]
+        TeamSite,
+        /// <summary>
+        /// "modern" communication site
+        /// </summary>
+        [Display(Name = "Communication Site")]
+        CommunicationSite,
+    }
+
+    /// <summary>
+    /// Defines the two async site creation options
+    /// </summary>
+    public enum AsynchronousTechnique
+    {
+        /// <summary>
+        /// "modern" team site
+        /// </summary>
+        [Display(Name = "Azure Function")]
+        AzureFunction,
+        /// <summary>
+        /// "modern" communication site
+        /// </summary>
+        [Display(Name = "Azure WebJob")]
+        AzureWebJob,
+    }
+}
