703f125f2bd4562d2a19270445715d250e044b9b
Bert Jansen <bjansen@microsoft.com>
2018-02-28 11:15:30 +0100

    New sample shows how to discover a multi-geo setup using the CSOM API

---
+++ Samples/MultiGeo.TenantInformationCollectionCSOM/MultiGeo.TenantInformationCollectionCSOM/MultiGeoManager.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Online.SharePoint.TenantAdministration;
+using Microsoft.SharePoint.Client;
+using System.Collections.Generic;
+
+namespace GeoTenantInformationCollection
+{
+    /// <summary>
+    /// Multi-Geo helper class
+    /// </summary>
+    public class MultiGeoManager
+    {
+        private List<GeoProperties> geosCache = null;
+        private ClientContext clientContext = null;
+
+        /// <summary>
+        /// Constructs the multi geo manager
+        /// </summary>
+        /// <param name="clientContext">ClientContext for the tenant</param>
+        public MultiGeoManager(ClientContext clientContext)
+        {
+            this.clientContext = clientContext;
+        }
+
+        /// <summary>
+        /// Return the geo locations from the tenant linked to the Azure AD hosting the defined Azure AD application
+        /// </summary>
+        /// <returns>List of geo locations in this tenant</returns>
+        public List<GeoProperties> GetTenantGeoLocations()
+        {
+            // Return data from cache...geos are fairly stable :-)
+            if (this.geosCache != null)
+            {
+                return this.geosCache;
+            }
+
+            List<GeoProperties> geoList = new List<GeoProperties>();
+
+            Tenant tenant = new Tenant(this.clientContext);
+
+            var tenantInstances = tenant.GetTenantInstances();
+            this.clientContext.Load(tenantInstances);
+            this.clientContext.ExecuteQuery();
+
+            foreach(var instance in tenantInstances)
+            {
+                geoList.Add(new GeoProperties()
+                {
+                    GeoLocation = instance.DataLocation,
+                    RootSiteUrl = instance.RootSiteUrl,
+                    MySiteHostUrl = instance.MySiteHostUrl,
+                    TenantAdminUrl = instance.TenantAdminUrl,
+                });
+            }
+
+            // cache data as geos are fairly stable
+            this.geosCache = geoList;
+
+            return geoList;
+        }
+    }
+}
---
+++ Samples/MultiGeo.TenantInformationCollectionCSOM/MultiGeo.TenantInformationCollectionCSOM/Program.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Security;
+
+namespace GeoTenantInformationCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string tenantAdminUrl = "https://contoso-admin.sharepoint.com";
+                string userName = "admin@contoso.onmicrosoft.com";
+
+                using (var clientContext = new ClientContext(tenantAdminUrl))
+                {
+                    clientContext.Credentials = new SharePointOnlineCredentials(userName, GetSecureString($"Password for {userName}"));
+
+                    // Check the readme file to learn how to register an application in azure ad and replace these values
+                    MultiGeoManager multiGeoManager = new MultiGeoManager(clientContext);
+                    var geos = multiGeoManager.GetTenantGeoLocations();
+                    foreach (var geo in geos)
+                    {
+                        Console.WriteLine($"{geo.GeoLocation} - {geo.RootSiteUrl} - {geo.TenantAdminUrl}");
+                    }
+
+                    Console.WriteLine("Press a key to continue...");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Multi-geo exception: ${ex.ToString()}");
+            }
+
+        }
+
+        #region Helper methods
+        private static SecureString GetSecureString(string label)
+        {
+            SecureString sStrPwd = new SecureString();
+            try
+            {
+                Console.Write(String.Format("{0}: ", label));
+
+                for (ConsoleKeyInfo keyInfo = Console.ReadKey(true); keyInfo.Key != ConsoleKey.Enter; keyInfo = Console.ReadKey(true))
+                {
+                    if (keyInfo.Key == ConsoleKey.Backspace)
+                    {
+                        if (sStrPwd.Length > 0)
+                        {
+                            sStrPwd.RemoveAt(sStrPwd.Length - 1);
+                            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                            Console.Write(" ");
+                            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        }
+                    }
+                    else if (keyInfo.Key != ConsoleKey.Enter)
+                    {
+                        Console.Write("*");
+                        sStrPwd.AppendChar(keyInfo.KeyChar);
+                    }
+
+                }
+                Console.WriteLine("");
+            }
+            catch (Exception e)
+            {
+                sStrPwd = null;
+                Console.WriteLine(e.Message);
+            }
+
+            return sStrPwd;
+        }
+        #endregion
+
+    }
+}
