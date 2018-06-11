196fb0a9671024348362e2a0be3375de89936647
Mike Morrison <mike-morr@users.noreply.github.com>
2018-06-11 02:28:45 -0600

    Added Sample Provisioning.CreateSite.Mvc (#1786)
    Adds MVC sample for provisioning site collections and subsites

---
+++ Samples/Provisioning.CreateSite.Mvc/Provisioning.CreateSite.MvcWeb/Models/NewWebProperties.cs
@@ -0,0 +1,45 @@
+﻿using SP = Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Provisioning.CreateSite.MvcWeb.Models
+{
+    public class NewWebProperties
+    {
+        [Required]
+        [Remote("ValidateWeb", "Home")]
+        public string Url { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [Display(Name = "Site Owner")]
+
+        public string SelectedWebTemplate { get; set; }
+
+        [Display(Name = "Template")]
+        public List<SelectListItem> WebTemplate { get; set; }
+
+        [HiddenInput]
+        public string SPHostUrl { get; set; }
+
+        public NewWebProperties()
+        {
+
+        }
+
+        public NewWebProperties(SP.WebTemplateCollection webTemplates)
+        {
+            WebTemplate = new List<SelectListItem>();
+
+            foreach (var template in webTemplates)
+            {
+                WebTemplate.Add(new SelectListItem { Value = template.Name, Text = template.Title });
+            }
+        }
+    }
+}
